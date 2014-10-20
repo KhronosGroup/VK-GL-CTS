7e01a414adc237914552ab970ab3c8c81a8ff799
Pyry Haulos <phaulos@google.com>
2014-10-20 11:09:56 -0700

    Improvements to standalone Android build scripts
     * If multiple connected devices are detected, install.py prompts to
    select one by default if no additional arguments are given.
    
     * Add -a option to install.py that installs package to all connected
    devices.
    
     * Use ninja on Linux / OS X, if installed, for faster builds (apt-get
    install ninja-build to get the awesomeness).
    
     * If make is used, pass in -j{CPUs} based on number of cores detected.
    
     * Prefer adb in path to avoid adb version mismatch in certain
    environments, for example when doing Android OS builds.
    
     * Clean up libs/ dir to avoid stale versions for archs that are not
    being built.
    
     * Do not unnecessarily force re-linking of libtestercore.so.
    
     * Optimize asset cleanup by performing that only for the build that is
    used for the assets.
    
     * Auto-detect ANDROID_NDK_HOST_OS.
    
    Bug: 18329517
    Change-Id: I44e1b0acb5e6bcafeff7df30147002f216a76deb
    (cherry picked from commit 89e0405021b883114cca9e748166c17bd7ebcc98)

--- android/scripts/build.py
+++ android/scripts/build.py
@@ -8,6 +8,8 @@ import argparse
 
 import common
 
+BASE_LIBS_DIR = os.path.join(common.ANDROID_DIR, "package", "libs")
+
 def getStoreKeyPasswords (filename):
 	f			= open(filename)
 	storepass	= None
@@ -28,24 +30,15 @@ def getNativeBuildDir (nativeLib, buildType):
 	deqpDir = os.path.normpath(os.path.join(common.ANDROID_DIR, ".."))
 	return os.path.normpath(os.path.join(deqpDir, "android", "build", "%s-%d-%s" % (buildType.lower(), nativeLib.apiVersion, nativeLib.abiVersion)))
 
+def getAssetsDir (nativeLib, buildType):
+	return os.path.join(getNativeBuildDir(nativeLib, buildType), "assets")
+
 def buildNative (nativeLib, buildType):
 	deqpDir		= os.path.normpath(os.path.join(common.ANDROID_DIR, ".."))
 	buildDir	= getNativeBuildDir(nativeLib, buildType)
-	assetsDir	= os.path.join(buildDir, "assets")
-	libsDir		= os.path.join(common.ANDROID_DIR, "package", "libs", nativeLib.abiVersion)
+	libsDir		= os.path.join(BASE_LIBS_DIR, nativeLib.abiVersion)
 	srcLibFile	= os.path.join(buildDir, "libtestercore.so")
 	dstLibFile	= os.path.join(libsDir, "lib%s.so" % nativeLib.libName)
-
-	# Remove old lib files if such exist
-	if os.path.exists(srcLibFile):
-		os.unlink(srcLibFile)
-
-	if os.path.exists(dstLibFile):
-		os.unlink(dstLibFile)
-
-	# Remove assets directory so that we don't collect unnecessary cruft to the APK
-	if os.path.exists(assetsDir):
-		shutil.rmtree(assetsDir)
 
 	# Make build directory if necessary
 	if not os.path.exists(buildDir):
@@ -65,7 +58,7 @@ def buildNative (nativeLib, buildType):
 			])
 
 	os.chdir(buildDir)
-	common.execute(common.BUILD_CMD)
+	common.execArgs(['cmake', '--build', '.'] + common.EXTRA_BUILD_ARGS)
 
 	if not os.path.exists(libsDir):
 		os.makedirs(libsDir)
@@ -85,48 +78,69 @@ def buildNative (nativeLib, buildType):
 		# Make sure there is no gdbserver if build is not debug build
 		os.unlink(os.path.join(libsDir, "gdbserver"))
 
-def copyAssets (nativeLib, buildType):
-	srcDir = os.path.join(getNativeBuildDir(nativeLib, buildType), "assets")
-	dstDir = os.path.join(common.ANDROID_DIR, "package", "assets")
-
-	if os.path.exists(dstDir):
-		shutil.rmtree(dstDir)
-
-	if os.path.exists(srcDir):
-		shutil.copytree(srcDir, dstDir)
-
-def fileContains (filename, str):
-	f = open(filename, 'rb')
-	data = f.read()
-	f.close()
-
-	return data.find(str) >= 0
-
 def buildApp (isRelease):
 	appDir	= os.path.join(common.ANDROID_DIR, "package")
 
 	# Set up app
 	os.chdir(appDir)
-	common.execute("%s update project --name dEQP --path . --target %s" % (common.shellquote(common.ANDROID_BIN), common.ANDROID_JAVA_API))
+	common.execArgs([
+			common.ANDROID_BIN,
+			'update', 'project',
+			'--name', 'dEQP',
+			'--path', '.',
+			'--target', str(common.ANDROID_JAVA_API),
+		])
 
 	# Build
-	common.execute("%s %s" % (common.shellquote(common.ANT_BIN), "release" if isRelease else "debug"))
+	common.execArgs([common.ANT_BIN, "release" if isRelease else "debug"])
 
 def signApp (keystore, keyname, storepass, keypass):
 	os.chdir(os.path.join(common.ANDROID_DIR, "package"))
-	common.execute("%s -keystore %s -storepass %s -keypass %s -sigfile CERT -digestalg SHA1 -sigalg MD5withRSA -signedjar bin/dEQP-unaligned.apk bin/dEQP-release-unsigned.apk %s" % (common.shellquote(common.JARSIGNER_BIN), common.shellquote(keystore), storepass, keypass, keyname))
-	common.execute("%s -f 4 bin/dEQP-unaligned.apk bin/dEQP-release.apk" % (common.shellquote(common.ZIPALIGN_BIN)))
+	common.execArgs([
+			common.JARSIGNER_BIN,
+			'-keystore', keystore,
+			'-storepass', storepass,
+			'-keypass', keypass,
+			'-sigfile', 'CERT',
+			'-digestalg', 'SHA1',
+			'-sigalg', 'MD5withRSA',
+			'-signedjar', 'bin/dEQP-unaligned.apk',
+			'bin/dEQP-release-unsigned.apk',
+			keyname
+		])
+	common.execArgs([
+			common.ZIPALIGN_BIN,
+			'-f', '4',
+			'bin/dEQP-unaligned.apk',
+			'bin/dEQP-release.apk'
+		])
 
 def build (isRelease=False, nativeBuildType="Release"):
 	curDir = os.getcwd()
 
 	try:
+		assetsSrcDir	= getAssetsDir(common.NATIVE_LIBS[0], nativeBuildType)
+		assetsDstDir	= os.path.join(common.ANDROID_DIR, "package", "assets")
+
+		# Remove assets from the first build dir where we copy assets from
+		# to avoid collecting cruft there.
+		if os.path.exists(assetsSrcDir):
+			shutil.rmtree(assetsSrcDir)
+		if os.path.exists(assetsDstDir):
+			shutil.rmtree(assetsDstDir)
+
+		# Remove old libs dir to avoid collecting out-of-date versions
+		# of libs for ABIs not built this time.
+		if os.path.exists(BASE_LIBS_DIR):
+			shutil.rmtree(BASE_LIBS_DIR)
+
 		# Build native code
 		for lib in common.NATIVE_LIBS:
 			buildNative(lib, nativeBuildType)
 
-		# Copy assets from first build dir
-		copyAssets(common.NATIVE_LIBS[0], nativeBuildType)
+		# Copy assets
+		if os.path.exists(assetsSrcDir):
+			shutil.copytree(assetsSrcDir, assetsDstDir)
 
 		# Build java code and .apk
 		buildApp(isRelease)
--- android/scripts/install.py
+++ android/scripts/install.py
@@ -7,29 +7,55 @@ import string
 
 import common
 
-def install (extraArgs = ""):
+def install (extraArgs = []):
 	curDir = os.getcwd()
 	try:
 		os.chdir(common.ANDROID_DIR)
 
-		adbCmd = common.shellquote(common.ADB_BIN)
-		if len(extraArgs) > 0:
-			adbCmd += " %s" % extraArgs
-
 		print "Removing old dEQP Package..."
-		common.execute("%s uninstall com.drawelements.deqp" % adbCmd)
+		common.execArgs([common.ADB_BIN] + extraArgs + [
+				'uninstall',
+				'com.drawelements.deqp'
+			])
 		print ""
 
 		print "Installing dEQP Package..."
-		common.execute("%s install -r package/bin/dEQP-debug.apk" % adbCmd)
+		common.execArgs([common.ADB_BIN] + extraArgs + [
+				'install',
+				'-r',
+				'package/bin/dEQP-debug.apk'
+			])
 		print ""
 
 	finally:
 		# Restore working dir
 		os.chdir(curDir)
-		
+
+def installToDevice (device):
+	print "Installing to %s (%s)..." % (device.serial, device.model)
+	install(['-s', device.serial])
+
+def installToAllDevices ():
+	devices = common.getDevices(common.ADB_BIN)
+	for device in devices:
+		installToDevice(device)
+
 if __name__ == "__main__":
 	if len(sys.argv) > 1:
-		install(string.join(sys.argv[1:], " "))
+		if sys.argv[1] == '-a':
+			installToAllDevices()
+		else:
+			install(sys.argv[1:])
 	else:
-		install()
+		devices = common.getDevices(common.ADB_BIN)
+		if len(devices) == 0:
+			common.die('No devices connected')
+		elif len(devices) == 1:
+			installToDevice(devices[0])
+		else:
+			print "More than one device connected:"
+			for i in range(0, len(devices)):
+				print "%3d: %16s %s" % ((i+1), devices[i].serial, devices[i].model)
+
+			deviceNdx = int(raw_input("Choose device (1-%d): " % len(devices)))
+			installToDevice(devices[deviceNdx-1])
