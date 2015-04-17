dac378b7552f48aa929bc9f290927a9331d4851d
Pyry Haulos <phaulos@google.com>
2015-04-17 15:57:51 -0700

    Update libpng reference to 1.6.17
    Change-Id: Idef27a35ca598c1df95f1c38392aa0450322392a

--- external/fetch_sources.py
+++ external/fetch_sources.py
@@ -93,9 +93,9 @@ PACKAGES = [
 				  "zlib-1.2.8.tar.gz",
 				  "36658cb768a54c1d4dec43c3116c27ed893e88b02ecfcb44f2166f9c0b7f2a0d",
 				  "zlib"),
-	SourcePackage("ftp://ftp.simplesystems.org/pub/libpng/png/src/libpng16/libpng-1.6.16.tar.gz",
-				  "libpng-1.6.14.tar.gz",
-				  "02f96b6bad5a381d36d7ba7a5d9be3b06f7fe6c274da00707509c23592a073ad",
+	SourcePackage("ftp://ftp.simplesystems.org/pub/libpng/png/src/libpng16/libpng-1.6.17.tar.gz",
+				  "libpng-1.6.17.tar.gz",
+				  "a18233c99e1dc59a256180e6871d9305a42e91b3f98799b3ceb98e87e9ec5e31",
 				  "libpng",
 				  postExtract = postExtractLibpng),
 ]
