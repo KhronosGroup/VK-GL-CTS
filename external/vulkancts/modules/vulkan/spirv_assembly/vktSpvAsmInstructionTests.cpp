c2b3c3a35e60816d57d72ed30b98a98a10bd3d23
Andrew Woloszyn <awoloszyn@google.com>
2016-01-15 13:21:23 -0500

    Fixed constants spirv_assembly.instruction.graphics.opphi.out_of_order
    Fixes #209

--- external/vulkancts/modules/vulkan/spirv_assembly/vktSpvAsmInstructionTests.cpp
+++ external/vulkancts/modules/vulkan/spirv_assembly/vktSpvAsmInstructionTests.cpp
@@ -6297,7 +6297,7 @@ tcu::TestCaseGroup* createOpPhiTests(tcu::TestContext& testCtx)
 	//     float operand;
 	//     switch (i) {
 	//       case 0: operand = .2; break;
-	//       case 1: operand = .6; break;
+	//       case 1: operand = .5; break;
 	//       case 2: operand = .4; break;
 	//       case 3: operand = .0; break;
 	//       default: break; // unreachable
@@ -6359,10 +6359,10 @@ tcu::TestCaseGroup* createOpPhiTests(tcu::TestContext& testCtx)
 
 	getHalfColorsFullAlpha(inputColors);
 
-	outputColors1[0]		= RGBA(178, 180, 229, 255);
-	outputColors1[1]		= RGBA(178, 153, 102, 255);
-	outputColors1[2]		= RGBA(51,  180, 102, 255);
-	outputColors1[3]		= RGBA(51,  153, 229, 255);
+	outputColors1[0]		= RGBA(178, 255, 229, 255);
+	outputColors1[1]		= RGBA(178, 127, 102, 255);
+	outputColors1[2]		= RGBA(51,  255, 102, 255);
+	outputColors1[3]		= RGBA(51,  127, 229, 255);
 
 	createTestsForAllStages("out_of_order", inputColors, outputColors1, fragments1, group.get());
 
