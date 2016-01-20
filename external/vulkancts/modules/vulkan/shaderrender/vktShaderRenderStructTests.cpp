533b2651cc2f6ac8da907fcb1b2932ec53576e3d
pflisiko <pawel.flisikowski@intel.com>
2016-01-20 13:56:53 +0100

    fixed test case dEQP-VK.glsl.struct.uniform.dynamic_loop_nested_struct_array_* - changed binding number for 'uniform buffer11' to 11

--- external/vulkancts/modules/vulkan/shaderrender/vktShaderRenderStructTests.cpp
+++ external/vulkancts/modules/vulkan/shaderrender/vktShaderRenderStructTests.cpp
@@ -1895,7 +1895,7 @@ void UniformStructTests::init (void)
 		<< "	T				b[3];"
 		<< "	int				c;"
 		<< "};"
-		<< "layout (std140, set = 0, binding = 10) uniform buffer11 { S s[2]; };"
+		<< "layout (std140, set = 0, binding = 11) uniform buffer11 { S s[2]; };"
 		<< ""
 		<< "void main (void)"
 		<< "{"
