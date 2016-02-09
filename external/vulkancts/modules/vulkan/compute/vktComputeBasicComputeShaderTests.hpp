014bc2d4145274f44aba801b49285f5593d84767
Pyry Haulos <phaulos@google.com>
2016-02-09 12:49:00 -0800

    Remove confidentiality clause from Vulkan CTS license

--- external/vulkancts/modules/vulkan/compute/vktComputeBasicComputeShaderTests.hpp
+++ external/vulkancts/modules/vulkan/compute/vktComputeBasicComputeShaderTests.hpp
@@ -4,7 +4,7 @@
  * Vulkan Conformance Tests
  * ------------------------
  *
- * Copyright (c) 2015 Mobica Ltd.
+ * Copyright (c) 2016 The Khronos Group Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and/or associated documentation files (the
@@ -16,10 +16,6 @@
  *
  * The above copyright notice(s) and this permission notice shall be included
  * in all copies or substantial portions of the Materials.
- *
- * The Materials are Confidential Information as defined by the
- * Khronos Membership Agreement until designated non-confidential by Khronos,
- * at which point this condition clause shall be removed.
  *
  * THE MATERIALS ARE PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
  * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
