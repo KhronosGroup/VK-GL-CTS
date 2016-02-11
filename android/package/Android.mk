0f019f48fd99e89798a2e9db64bf85ab3e1392ad
Mika Isojärvi <misojarvi@google.com>
2016-02-11 13:59:46 -0800

    Add Vulkan assets to apk when building for Android CTS.
    Change-Id: Ib8c757b3e01444ef5e112dadc0c2ff835bbbd46f

--- android/package/Android.mk
+++ android/package/Android.mk
@@ -28,7 +28,11 @@ LOCAL_COMPATIBILITY_SUITE := cts_v2
 LOCAL_SRC_FILES := $(call all-java-files-under,src)
 LOCAL_JNI_SHARED_LIBRARIES := libdeqp
 
-LOCAL_ASSET_DIR := $(LOCAL_PATH)/../../data
+LOCAL_ASSET_DIR := \
+	$(LOCAL_PATH)/../../data \
+	$(LOCAL_PATH)/../../external/vulkancts/data \
+	$(LOCAL_PATH)/../../../../prebuilts/deqp/spirv
+
 LOCAL_PACKAGE_NAME := com.drawelements.deqp
 LOCAL_MULTILIB := both
 
