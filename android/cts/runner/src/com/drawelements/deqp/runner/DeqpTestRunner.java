ab7320715201dd492d71b4cd264115ea6e378467
Aaron Holden <aaronholden@google.com>
2016-06-27 18:57:25 -0700

    Copy collect-tests-only option in DeqpTestRunner
    Tests should be collected, not run, for all shards of DeqpTestRunner
    during plan 'collect-tests-only'
    
    Change-Id: Icaaa90209c00bcc733d409a11c0f6067af8e0ce8

--- android/cts/runner/src/com/drawelements/deqp/runner/DeqpTestRunner.java
+++ android/cts/runner/src/com/drawelements/deqp/runner/DeqpTestRunner.java
@@ -2087,6 +2087,7 @@ public class DeqpTestRunner implements IBuildReceiver, IDeviceTest,
         destination.mExcludeFilters = new ArrayList<>(source.mExcludeFilters);
         destination.mAbi = source.mAbi;
         destination.mLogData = source.mLogData;
+        destination.mCollectTestsOnly = source.mCollectTestsOnly;
     }
 
     /**
