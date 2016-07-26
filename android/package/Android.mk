62875f6b9f26d8c717e4a827a8c23dd3f882f657
Pyry Haulos <phaulos@google.com>
2016-07-26 16:26:54 -0700

    DO NOT MERGE revert CTS integration build changes
    Reverts CTS integration makefiles to match marshmallow-cts-dev
    
    Change-Id: I2b99024cee587bc823003df5aef0b555c265e381

--- android/package/Android.mk
+++ android/package/Android.mk
@@ -16,14 +16,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-# don't include this package in any target ??????
-LOCAL_MODULE_TAGS := optional
-# and when built explicitly put it in the data partition
-LOCAL_MODULE_PATH := $(TARGET_OUT_DATA_APPS)
-
 LOCAL_MODULE_TAGS := tests
-
-LOCAL_COMPATIBILITY_SUITE := cts
 
 LOCAL_SRC_FILES := $(call all-java-files-under,src)
 LOCAL_JNI_SHARED_LIBRARIES := libdeqp
