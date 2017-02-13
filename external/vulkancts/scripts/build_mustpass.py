6edea8fc58f0082c0d083bf9ac54b9e2cd2d85c8
Pyry Haulos <phaulos@google.com>
2017-02-13 11:16:33 -0800

    Start new mustpass for Vulkan CTS 1.0.3
    Vulkan CTS 1.0.2 mustpass is now archived under mustpass/1.0.2 and new
    mustpass can be found under mustpass/1.0.3.
    
    Components: Vulkan
    
    Change-Id: I7c073dc7436a434558ba1710e01988377a8d686c

--- external/vulkancts/scripts/build_mustpass.py
+++ external/vulkancts/scripts/build_mustpass.py
@@ -68,6 +68,14 @@ VULKAN_1_0_1_PKG	= Package(module = VULKAN_MODULE, configurations = [
 # 1.0.2
 
 VULKAN_1_0_2_PKG	= Package(module = VULKAN_MODULE, configurations = [
+			# Master
+			Configuration(name		= "default",
+						  filters	= [include("master.txt")]),
+	])
+
+# 1.0.3
+
+VULKAN_1_0_3_PKG	= Package(module = VULKAN_MODULE, configurations = [
 		  # Master
 		  Configuration(name		= "default",
 						filters		= [include("master.txt"),
@@ -80,6 +88,7 @@ MUSTPASS_LISTS		= [
 		  Mustpass(project = PROJECT,	version = "1.0.0",	packages = [VULKAN_1_0_0_PKG]),
 		  Mustpass(project = PROJECT,	version = "1.0.1",	packages = [VULKAN_1_0_1_PKG]),
 		  Mustpass(project = PROJECT,	version = "1.0.2",	packages = [VULKAN_1_0_2_PKG]),
+		  Mustpass(project = PROJECT,	version = "1.0.3",	packages = [VULKAN_1_0_3_PKG]),
 	]
 
 if __name__ == "__main__":
