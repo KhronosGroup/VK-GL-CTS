2f2af613bf266f8f6052976111c59241df154049
Alexander Galazin <alexander.galazin@arm.com>
2017-03-10 20:19:09 +0100

    Fix build after CTS_ARB_sparse_texture_clamp tests
    GL_EXT_direct_state_access was not added to the script
    generating inline files. This caused sanity checks failures.
    
    Component: Framework
    
    Affects: inline file generation
    
    Change-Id: I0a4c1e8dc6076f3a939f0adf474d9ae58ef38dba

--- scripts/opengl/src_util.py
+++ scripts/opengl/src_util.py
@@ -60,6 +60,7 @@ EXTENSIONS			= [
 	'GL_EXT_texture_type_2_10_10_10_REV',
 	'GL_EXT_copy_image',
 	'GL_EXT_depth_bounds_test',
+	'GL_EXT_direct_state_access',
 	'GL_EXT_draw_buffers_indexed',
 	'GL_EXT_draw_elements_base_vertex',
 	'GL_EXT_direct_state_access',
