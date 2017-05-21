12b099d59111b60518c581364fd026b6cfadf7d1
Boris Zanin <boris.zanin@mobica.com>
2017-05-21 22:21:49 +0200

    VK_KHR_dedicated_allocation: Extend pipeline.image
    Update tests:
     * dEQP-VK.pipeline.image.*
    
    Rename tests (add .suballocation subgroup):
     * dEQP-VK.pipeline.image.suballocation.*
    
    Add tests:
     * dEQP-VK.pipeline.image.dedicated_allocation.*
    
    Components: Vulkan
    
    VK-GL-CTS issue: 153
    
    Change-Id: I0b25c08b830a44956dd0c26da39124364dbabb89

--- external/vulkancts/modules/vulkan/pipeline/vktPipelineImageSamplingInstance.cpp
+++ external/vulkancts/modules/vulkan/pipeline/vktPipelineImageSamplingInstance.cpp
@@ -46,6 +46,65 @@ using de::UniquePtr;
 
 namespace
 {
+de::MovePtr<Allocation> allocateBuffer (const InstanceInterface&	vki,
+										const DeviceInterface&		vkd,
+										const VkPhysicalDevice&		physDevice,
+										const VkDevice				device,
+										const VkBuffer&				buffer,
+										const MemoryRequirement		requirement,
+										Allocator&					allocator,
+										AllocationKind				allocationKind)
+{
+	switch (allocationKind)
+	{
+		case ALLOCATION_KIND_SUBALLOCATED:
+		{
+			const VkMemoryRequirements	memoryRequirements	= getBufferMemoryRequirements(vkd, device, buffer);
+
+			return allocator.allocate(memoryRequirements, requirement);
+		}
+
+		case ALLOCATION_KIND_DEDICATED:
+		{
+			return allocateDedicated(vki, vkd, physDevice, device, buffer, requirement);
+		}
+
+		default:
+		{
+			TCU_THROW(InternalError, "Invalid allocation kind");
+		}
+	}
+}
+
+de::MovePtr<Allocation> allocateImage (const InstanceInterface&		vki,
+									   const DeviceInterface&		vkd,
+									   const VkPhysicalDevice&		physDevice,
+									   const VkDevice				device,
+									   const VkImage&				image,
+									   const MemoryRequirement		requirement,
+									   Allocator&					allocator,
+									   AllocationKind				allocationKind)
+{
+	switch (allocationKind)
+	{
+		case ALLOCATION_KIND_SUBALLOCATED:
+		{
+			const VkMemoryRequirements	memoryRequirements	= getImageMemoryRequirements(vkd, device, image);
+
+			return allocator.allocate(memoryRequirements, requirement);
+		}
+
+		case ALLOCATION_KIND_DEDICATED:
+		{
+			return allocateDedicated(vki, vkd, physDevice, device, image, requirement);
+		}
+
+		default:
+		{
+			TCU_THROW(InternalError, "Invalid allocation kind");
+		}
+	}
+}
 
 static VkImageType getCompatibleImageType (VkImageViewType viewType)
 {
@@ -135,8 +194,10 @@ ImageSamplingInstance::ImageSamplingInstance (Context&							context,
 											  float								samplerLod,
 											  const std::vector<Vertex4Tex4>&	vertices,
 											  VkDescriptorType					samplingType,
-											  int								imageCount)
+											  int								imageCount,
+											  AllocationKind					allocationKind)
 	: vkt::TestInstance		(context)
+	, m_allocationKind		(allocationKind)
 	, m_samplingType		(samplingType)
 	, m_imageViewType		(imageViewType)
 	, m_imageFormat			(imageFormat)
@@ -151,7 +212,9 @@ ImageSamplingInstance::ImageSamplingInstance (Context&							context,
 	, m_colorFormat			(VK_FORMAT_R8G8B8A8_UNORM)
 	, m_vertices			(vertices)
 {
+	const InstanceInterface&	vki						= context.getInstanceInterface();
 	const DeviceInterface&		vk						= context.getDeviceInterface();
+	const VkPhysicalDevice		physDevice				= context.getPhysicalDevice();
 	const VkDevice				vkDevice				= context.getDevice();
 	const VkQueue				queue					= context.getUniversalQueue();
 	const deUint32				queueFamilyIndex		= context.getUniversalQueueFamilyIndex();
@@ -203,6 +266,14 @@ ImageSamplingInstance::ImageSamplingInstance (Context&							context,
 	if (imageViewType == VK_IMAGE_VIEW_TYPE_CUBE_ARRAY && !context.getDeviceFeatures().imageCubeArray)
 		TCU_THROW(NotSupportedError, "imageCubeArray feature is not supported");
 
+	if (m_allocationKind == ALLOCATION_KIND_DEDICATED)
+	{
+		const std::string extensionName("VK_KHR_dedicated_allocation");
+
+		if (!de::contains(context.getDeviceExtensions().begin(), context.getDeviceExtensions().end(), extensionName))
+			TCU_THROW(NotSupportedError, std::string(extensionName + " is not supported").c_str());
+	}
+
 	// Create texture images, views and samplers
 	{
 		VkImageCreateFlags			imageFlags			= 0u;
@@ -246,7 +317,7 @@ ImageSamplingInstance::ImageSamplingInstance (Context&							context,
 		for (int imgNdx = 0; imgNdx < m_imageCount; ++imgNdx)
 		{
 			m_images[imgNdx] = SharedImagePtr(new UniqueImage(createImage(vk, vkDevice, &imageParams)));
-			m_imageAllocs[imgNdx] = SharedAllocPtr(new UniqueAlloc(memAlloc.allocate(getImageMemoryRequirements(vk, vkDevice, **m_images[imgNdx]), MemoryRequirement::Any)));
+			m_imageAllocs[imgNdx] = SharedAllocPtr(new UniqueAlloc(allocateImage(vki, vk, physDevice, vkDevice, **m_images[imgNdx], MemoryRequirement::Any, memAlloc, m_allocationKind)));
 			VK_CHECK(vk.bindImageMemory(vkDevice, **m_images[imgNdx], (*m_imageAllocs[imgNdx])->getMemory(), (*m_imageAllocs[imgNdx])->getOffset()));
 
 			// Upload texture data
@@ -351,7 +422,7 @@ ImageSamplingInstance::ImageSamplingInstance (Context&							context,
 		for (int imgNdx = 0; imgNdx < m_imageCount; ++imgNdx)
 		{
 			m_colorImages[imgNdx] = SharedImagePtr(new UniqueImage(createImage(vk, vkDevice, &colorImageParams)));
-			m_colorImageAllocs[imgNdx] = SharedAllocPtr(new UniqueAlloc(memAlloc.allocate(getImageMemoryRequirements(vk, vkDevice, **m_colorImages[imgNdx]), MemoryRequirement::Any)));
+			m_colorImageAllocs[imgNdx] = SharedAllocPtr(new UniqueAlloc(allocateImage(vki, vk, physDevice, vkDevice, **m_colorImages[imgNdx], MemoryRequirement::Any, memAlloc, m_allocationKind)));
 			VK_CHECK(vk.bindImageMemory(vkDevice, **m_colorImages[imgNdx], (*m_colorImageAllocs[imgNdx])->getMemory(), (*m_colorImageAllocs[imgNdx])->getOffset()));
 
 			const VkImageViewCreateInfo colorAttachmentViewParams =
@@ -685,8 +756,7 @@ ImageSamplingInstance::ImageSamplingInstance (Context&							context,
 		DE_ASSERT(vertexBufferSize > 0);
 
 		m_vertexBuffer		= createBuffer(vk, vkDevice, &vertexBufferParams);
-		m_vertexBufferAlloc	= memAlloc.allocate(getBufferMemoryRequirements(vk, vkDevice, *m_vertexBuffer), MemoryRequirement::HostVisible);
-
+		m_vertexBufferAlloc = allocateBuffer(vki, vk, physDevice, vkDevice, *m_vertexBuffer, MemoryRequirement::HostVisible, memAlloc, m_allocationKind);
 		VK_CHECK(vk.bindBufferMemory(vkDevice, *m_vertexBuffer, m_vertexBufferAlloc->getMemory(), m_vertexBufferAlloc->getOffset()));
 
 		// Load vertices into vertex buffer
