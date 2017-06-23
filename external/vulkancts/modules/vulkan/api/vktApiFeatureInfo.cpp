92971d53e00d8acd99ffb199e0d2d807700ac3a9
Alexander Galazin <alexander.galazin@arm.com>
2017-06-24 00:50:32 +0200

    Add VK_KHR_dedicated_allocation to allowed extensions
    Components: Vulkan
    VK-GL-CTS issue: 153
    
    Change-Id: Ia4a785bc31c4790d73375f01fbeb34d5936f7c80

--- external/vulkancts/modules/vulkan/api/vktApiFeatureInfo.cpp
+++ external/vulkancts/modules/vulkan/api/vktApiFeatureInfo.cpp
@@ -692,6 +692,7 @@ void checkDeviceExtensions (tcu::ResultCollector& results, const vector<string>&
 		"VK_KHR_external_fence_fd",
 		"VK_KHR_external_fence_win32",
 		"VK_KHR_win32_keyed_mutex",
+		"VK_KHR_dedicated_allocation",
 	};
 
 	checkKhrExtensions(results, extensions, DE_LENGTH_OF_ARRAY(s_allowedDeviceKhrExtensions), s_allowedDeviceKhrExtensions);
