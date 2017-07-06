0309f91ba399b3d5b8ca0fe02bdc4c67202eadd9
Alexander Galazin <alexander.galazin@arm.com>
2017-07-06 09:35:17 +0200

    Update fetch_sources.py to use public repos
    VK-GL-CTS issue: 556
    
    Components: Framework
    
    Change-Id: Ie603395ef669a1b45bc1d9df9c8d5fac042c9fc6

--- external/fetch_sources.py
+++ external/fetch_sources.py
@@ -241,9 +241,9 @@ PACKAGES = [
 		"libpng",
 		postExtract = postExtractLibpng),
 	GitRepo(
-		"https://gitlab.khronos.org/spirv/spirv-tools.git",
-		"git@gitlab.khronos.org:spirv/spirv-tools.git",
-		"5ef1440f3ec5a5fc6e5cf368383c4b1ae323f0f8",
+		"https://github.com/KhronosGroup/SPIRV-Tools.git",
+		None,
+		"0b0454c42c6b6f6746434bd5c78c5c70f65d9c51",
 		"spirv-tools"),
 	GitRepo(
 		"https://github.com/KhronosGroup/glslang.git",
@@ -251,9 +251,9 @@ PACKAGES = [
 		"a5c5fb61180e8703ca85f36d618f98e16dc317e2",
 		"glslang"),
 	GitRepo(
-		"https://gitlab.khronos.org/spirv/SPIRV-Headers.git",
-		"git@gitlab.khronos.org:spirv/SPIRV-Headers.git",
-		"de4c26af39f243b0c2176f9e15089f5968d52394",
+		"https://github.com/KhronosGroup/SPIRV-Headers.git",
+		None,
+		"2bf02308656f97898c5f7e433712f21737c61e4e",
 		"spirv-headers"),
 ]
 
