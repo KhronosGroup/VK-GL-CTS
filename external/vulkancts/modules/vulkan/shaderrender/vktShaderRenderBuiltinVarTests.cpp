153979d342b54aa04cb2038e158ff3e3c742d8b8
Samuel Iglesias Gonsálvez <siglesias@igalia.com>
2017-09-04 09:16:30 +0200

    Fix bug when setting descriptor set layouts
    Affects:
    
    dEQP-VK.glsl.builtin_var.fragdepth.*
    dEQP-VK.glsl.builtin_var.fragcoord_msaa.*
    
    Fixes crashes added by 595b5f763a8f6ef1c68bfd133a55ded9c25ac101 (CL#1017).
    
    Components: Vulkan
    VK-GL-CTS issue: 659
    
    Change-Id: I923637fa2f7a4b0916515155afa44fcf64221209

--- external/vulkancts/modules/vulkan/shaderrender/vktShaderRenderBuiltinVarTests.cpp
+++ external/vulkancts/modules/vulkan/shaderrender/vktShaderRenderBuiltinVarTests.cpp
@@ -955,8 +955,8 @@ TestStatus BuiltinFragDepthCaseInstance::iterate (void)
 
 		drawState.numSamples				= m_samples;
 		drawState.sampleShadingEnable		= true;
-		vulkanProgram.descriptorSetLayout	= descriptorSetLayout;
-		vulkanProgram.descriptorSet			= descriptorSet;
+		vulkanProgram.descriptorSetLayout	= *descriptorSetLayout;
+		vulkanProgram.descriptorSet			= *descriptorSet;
 
 		VulkanDrawContext		vulkanDrawContext(m_context, drawState, drawCallData, vulkanProgram);
 		vulkanDrawContext.draw();
@@ -1498,8 +1498,8 @@ TestStatus BuiltinFragCoordMsaaCaseInstance::iterate (void)
 
 		drawState.numSamples				= m_sampleCount;
 		drawState.sampleShadingEnable		= true;
-		vulkanProgram.descriptorSetLayout	= descriptorSetLayout;
-		vulkanProgram.descriptorSet			= descriptorSet;
+		vulkanProgram.descriptorSetLayout	= *descriptorSetLayout;
+		vulkanProgram.descriptorSet			= *descriptorSet;
 
 		VulkanDrawContext	vulkanDrawContext(m_context, drawState, drawCallData, vulkanProgram);
 		vulkanDrawContext.draw();
