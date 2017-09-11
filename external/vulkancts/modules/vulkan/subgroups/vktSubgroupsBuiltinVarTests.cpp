4e3b4214a201bd02f2e4868f464222ca6ede8dc0
Samuel Iglesias Gonsálvez <siglesias@igalia.com>
2017-09-11 12:41:56 +0200

    Check if the driver supports subgroup operations
    Affects: dEQP-VK.subgroups.*
    
    Components: Vulkan
    
    VK-GL-CTS issue: 683
    
    Change-Id: I85936d808459fab9a260bff372c7a5bbe7cc67fc
    (cherry picked from commit 9cbd59a275ea092c6e1ffed249d974054224c081)

--- external/vulkancts/modules/vulkan/subgroups/vktSubgroupsBuiltinVarTests.cpp
+++ external/vulkancts/modules/vulkan/subgroups/vktSubgroupsBuiltinVarTests.cpp
@@ -541,6 +541,9 @@ void initPrograms(SourceCollections& programCollection, CaseDefinition caseDef)
 
 tcu::TestStatus test(Context& context, const CaseDefinition caseDef)
 {
+	if (!subgroups::isSubgroupSupported(context))
+		TCU_THROW(NotSupportedError, "Subgroup operations are not supported");
+
 	if (!areSubgroupOperationsSupportedForStage(
 				context, caseDef.shaderStage))
 	{
