35e2f65c4061c2f49fca5f6b3c64cb8f003f6168
Paavo Pessi <paavo.pessi@siru.fi>
2017-10-30 15:25:49 +0200

    Test component qualifier in shaders
    Tests were added to verify that various combinations of component
    qualifiers can be used with different data types (vec2, vec3, vec4,
    ivec2 and ivec4) in following scenarios:
    
    - in vertex shader input
    - in fragment shader output
    - when passing data from vertex shader to fragment shader
    
    New tests:
    
    dEQP-VK.glsl.440.linkage.varying.component.*
    
    Components: Vulkan
    
    VK-GL-CTS issue: 741
    
    Change-Id: Ie18c420b9d3ba8bd082b98f4618d7fc595b8baba

--- framework/opengl/gluShaderLibrary.cpp
+++ framework/opengl/gluShaderLibrary.cpp
@@ -335,6 +335,8 @@ private:
 		TOKEN_GROUP,
 		TOKEN_CASE,
 		TOKEN_END,
+		TOKEN_OUTPUT_COLOR,
+		TOKEN_FORMAT,
 		TOKEN_VALUES,
 		TOKEN_BOTH,
 		TOKEN_VERTEX,
@@ -414,6 +416,7 @@ private:
 	deUint32					parseShaderStageList		(void);
 	void						parseRequirement			(CaseRequirement& valueBlock);
 	void						parseExpectResult			(ExpectResult& expectResult);
+	void						parseFormat					(DataType& format);
 	void						parseGLSLVersion			(glu::GLSLVersion& version);
 	void						parsePipelineProgram		(ProgramSpecification& program);
 	void						parseShaderCase				(vector<tcu::TestNode*>& shaderNodeList);
@@ -612,6 +615,8 @@ void ShaderParser::advanceToken (void)
 			{ "group",						TOKEN_GROUP						},
 			{ "case",						TOKEN_CASE						},
 			{ "end",						TOKEN_END						},
+			{ "output_color",				TOKEN_OUTPUT_COLOR				},
+			{ "format",						TOKEN_FORMAT					},
 			{ "values",						TOKEN_VALUES					},
 			{ "both",						TOKEN_BOTH						},
 			{ "vertex",						TOKEN_VERTEX					},
@@ -1245,6 +1250,12 @@ void ShaderParser::parseExpectResult (ExpectResult& expectResult)
 	advanceToken();
 }
 
+void ShaderParser::parseFormat (DataType& format)
+{
+	format = mapDataTypeToken(m_curToken);
+	advanceToken();
+}
+
 void ShaderParser::parseGLSLVersion (glu::GLSLVersion& version)
 {
 	int			versionNum		= 0;
@@ -1353,6 +1364,8 @@ void ShaderParser::parseShaderCase (vector<tcu::TestNode*>& shaderNodeList)
 	// Setup case.
 	GLSLVersion						version			= DEFAULT_GLSL_VERSION;
 	ExpectResult					expectResult	= EXPECT_PASS;
+	OutputType						outputType		= OUTPUT_RESULT;
+	DataType						format			= TYPE_LAST;
 	string							description;
 	string							bothSource;
 	vector<string>					vertexSources;
@@ -1380,6 +1393,12 @@ void ShaderParser::parseShaderCase (vector<tcu::TestNode*>& shaderNodeList)
 		{
 			advanceToken();
 			parseExpectResult(expectResult);
+		}
+		else if (m_curToken == TOKEN_OUTPUT_COLOR)
+		{
+			outputType = OUTPUT_COLOR;
+			advanceToken();
+			parseFormat(format);
 		}
 		else if (m_curToken == TOKEN_VALUES)
 		{
@@ -1516,6 +1535,8 @@ void ShaderParser::parseShaderCase (vector<tcu::TestNode*>& shaderNodeList)
 		ShaderCaseSpecification	spec;
 		spec.caseType				= CASETYPE_COMPLETE;
 		spec.expectResult			= expectResult;
+		spec.outputType				= outputType;
+		spec.outputFormat			= format;
 		spec.targetVersion			= version;
 		spec.fullGLSLES100Required	= fullGLSLES100Required;
 		spec.requiredCaps			= requiredCaps;
