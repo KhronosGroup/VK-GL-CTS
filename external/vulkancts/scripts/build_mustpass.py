9d091ad9163c2d6e71ec6650d70399e1114de06a
Alexander Galazin <alexander.galazin@arm.com>
2018-01-19 11:37:36 +0100

    Remove 1.0.4 mustpass
    Components: Vulkan
    
    Change-Id: I75fc3abc5019333c737f5ee4c1f0690931311ec0

--- external/vulkancts/scripts/build_mustpass.py
+++ external/vulkancts/scripts/build_mustpass.py
@@ -84,22 +84,6 @@ VULKAN_1_0_3_PKG	= Package(module = VULKAN_MODULE, configurations = [
 						filters		= [include("master.txt")]),
 	 ])
 
-# 1.0.4
-
-VULKAN_1_0_4_PKG	= Package(module = VULKAN_MODULE, configurations = [
-		  # Master
-		  Configuration(name		= "default",
-						filters		= [include("master.txt"),
-									   exclude("test-issues.txt"),
-									   exclude("excluded-tests.txt"),
-									   exclude("android-tests.txt"),
-									   exclude("waivers.txt")]),
-		  Configuration(name		= "default-no-waivers",
-						filters		= [include("master.txt"),
-									   exclude("test-issues.txt"),
-									   exclude("excluded-tests.txt"),
-									   exclude("android-tests.txt")]),
-	 ])
 # 1.1.0
 
 VULKAN_1_1_0_PKG	= Package(module = VULKAN_MODULE, configurations = [
@@ -135,7 +119,6 @@ MUSTPASS_LISTS		= [
 		  Mustpass(project = PROJECT,	version = "1.0.1",	packages = [VULKAN_1_0_1_PKG]),
 		  Mustpass(project = PROJECT,	version = "1.0.2",	packages = [VULKAN_1_0_2_PKG]),
 		  Mustpass(project = PROJECT,	version = "1.0.3",	packages = [VULKAN_1_0_3_PKG]),
-		  Mustpass(project = PROJECT,	version = "1.0.4",	packages = [VULKAN_1_0_4_PKG]),
 		  Mustpass(project = PROJECT,	version = "1.1.0",	packages = [VULKAN_1_1_0_PKG]),
 		  Mustpass(project = PROJECT,	version = "1.1.1",	packages = [VULKAN_1_1_1_PKG]),
 	]
