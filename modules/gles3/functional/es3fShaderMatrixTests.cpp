71d3798e9b80ab0d85d798f4a6a36df95bf2ec20
Jamie Madill <jmadill@chromium.org>
2018-01-26 12:55:20 -0500

    Work around VC++ code-gen bug in shader matrix tests.
    This code-gen bug was found in crbug.com/759402, and was causing test
    runtime failures when run. Work around it by disabling optimization
    for the specific compiler version.
    
    Components: AOSP
    Affects:
      dEQP-GLES2.functional.shaders.matrix.*
      dEQP-GLES3.functional.shaders.matrix.*

--- modules/gles3/functional/es3fShaderMatrixTests.cpp
+++ modules/gles3/functional/es3fShaderMatrixTests.cpp
@@ -1957,3 +1957,9 @@ void ShaderMatrixTests::init (void)
 } // Functional
 } // gles3
 } // deqp
+
+#if defined(_MSC_VER) && _MSC_FULL_VER == 191125507
+// Work around crbug.com/759402 which is a code-gen bug in VC++ 2017, version
+// 15.3.2.
+#pragma optimize("", off)
+#endif
