KhronosGroup/VK-GL-CTS

commit 774de0ae8a05a8109576b5a1cea57fc472c6013a

Added shader cache to speed up runtime
Added shader cache which hashes incoming shader sources and saves the
resulting binaries on disk, avoiding unneccessary recompilation.

On testing it was found that the conformance suite calls shader
compilation about 500k times while only about 30k shaders are unique.
When all shaders are prebuilt on disk, complete runtime is reduced by
about 30%.

The shader cache also affects vk-build-programs. If shaders already
exist and shader validation is not enabled, vk-build-programs runs about
95% faster.

The timings above are based on debug builds on a desktop PC. Release
builds get less benefit, but this may be helpful on mobile platforms.

The shader cache does not affect targets which do not include spir-v
tools.

Affects:

dEQP-VK.*

Components: Framework, Vulkan

VK-GL-CTS issue: 899

Change-Id: I9b1ea915b3c9d94be85adb17161c12f453175d78

framework/qphelper/gen_release_info.py
@@ -41,16 +41,23 @@ def getHead (gitDir):
 									  "rev-parse", "HEAD"])
 	return commit.decode().strip()
 
-def makeReleaseInfo (name, id):
+def makeReleaseInfo (name, id, glslname, spirvtoolsname, spirvheadersname):
 	return """
 /* WARNING: auto-generated file, use {genFileName} to modify */
 
-#define DEQP_RELEASE_NAME	"{releaseName}"
-#define DEQP_RELEASE_ID		{releaseId}
+#define DEQP_RELEASE_NAME				"{releaseName}"
+#define DEQP_RELEASE_ID					{releaseId}
+#define DEQP_RELEASE_GLSL_NAME			"{glslReleaseName}"
+#define DEQP_RELEASE_SPIRV_TOOLS_NAME	"{spirvToolsReleaseName}"
+#define DEQP_RELEASE_SPIRV_HEADERS_NAME	"{spirvHeadersReleaseName}"
+
 """[1:].format(
-		genFileName	= os.path.basename(__file__),
-		releaseName	= name,
-		releaseId	= id)
+		genFileName				= os.path.basename(__file__),
+		releaseName				= name,
+		releaseId				= id,
+		glslReleaseName			= glslname,
+		spirvToolsReleaseName	= spirvtoolsname,
+		spirvHeadersReleaseName	= spirvheadersname)
 
 def parseArgs ():
 	parser = argparse.ArgumentParser(description="Generate release info for build")
@@ -58,6 +65,9 @@ def parseArgs ():
 	parser.add_argument('--id', dest='releaseId', default=None, help="Release ID (must be C integer literal)")
 	parser.add_argument('--git', dest='git', action='store_true', default=False, help="Development build, use git HEAD to identify")
 	parser.add_argument('--git-dir', dest='gitDir', default=None, help="Use specific git dir for extracting info")
+	parser.add_argument('--glsl-git-dir', dest='glslGitDir', default=None, help="Use specific git dir for extracting glsl version info")
+	parser.add_argument('--glsl-spirvtools-dir', dest='spirvToolsGitDir', default=None, help="Use specific git dir for extracting spirv tools version info")
+	parser.add_argument('--glsl-spirvheaders-dir', dest='spirvHeadersGitDir', default=None, help="Use specific git dir for extracting spirv headers version info")
 	parser.add_argument('--out', dest='out', default=None, help="Output file")
 
 	args = parser.parse_args()
@@ -75,22 +85,37 @@ def parseArgs ():
 	return args
 
 if __name__ == "__main__":
-	curDir			= os.path.dirname(__file__)
-	defaultGitDir	= os.path.normpath(os.path.join(curDir, "..", "..", ".git"))
-	defaultDstFile	= os.path.join(curDir, "qpReleaseInfo.inl")
+	curDir						= os.path.dirname(__file__)
+	defaultGitDir				= os.path.normpath(os.path.join(curDir, "..", "..", ".git"))
+	defaultGlslGitDir			= os.path.normpath(os.path.join(curDir, "..", "..", "external", "glslang", "src", ".git"))
+	defaultSpirvToolsGitDir		= os.path.normpath(os.path.join(curDir, "..", "..", "external", "spirv-tools", "src", ".git"))
+	defaultSpirvHeadersGitDir	= os.path.normpath(os.path.join(curDir, "..", "..", "external", "spirv-headers", "src", ".git"))
+	defaultDstFile				= os.path.join(curDir, "qpReleaseInfo.inl")
 
 	args = parseArgs()
 
 	if args.git:
-		gitDir		= args.gitDir if args.gitDir != None else defaultGitDir
-		head		= getHead(gitDir)
-		releaseName	= "git-%s" % head
-		releaseId	= "0x%s" % head[0:8]
+		gitDir				= args.gitDir				if args.gitDir				!= None else defaultGitDir
+		glslGitDir			= args.glslGitDir			if args.glslGitDir			!= None else defaultGlslGitDir
+		spirvToolsGitDir	= args.spirvToolsGitDir		if args.spirvToolsGitDir	!= None else defaultSpirvToolsGitDir
+		spirvHeadersGitDir	= args.spirvHeadersGitDir	if args.spirvHeadersGitDir	!= None else defaultSpirvHeadersGitDir
+		head				= getHead(gitDir)
+		glslHead			= getHead(glslGitDir)
+		spirvToolsHead		= getHead(spirvToolsGitDir)
+		spirvHeadersHead	= getHead(spirvHeadersGitDir)
+		releaseName			= "git-%s" % head
+		releaseId			= "0x%s" % head[0:8]
+		glslName			= "git-%s" % glslHead
+		spirvToolsName		= "git-%s" % spirvToolsHead
+		spirvHeadersName	= "git-%s" % spirvHeadersHead
 	else:
-		releaseName	= args.releaseName
-		releaseId	= args.releaseId
+		releaseName			= args.releaseName
+		releaseId			= args.releaseId
+		glslName			= args.releaseName
+		spirvToolsName		= args.releaseName
+		spirvHeadersName	= args.releaseName
 
-	releaseInfo	= makeReleaseInfo(releaseName, releaseId)
+	releaseInfo	= makeReleaseInfo(releaseName, releaseId, glslName, spirvToolsName, spirvHeadersName)
 	dstFile		= args.out if args.out != None else defaultDstFile
 
 	writeFile(dstFile, releaseInfo)

external/vulkancts/framework/vulkan/vkPrograms.cpp
@@ -21,20 +21,29 @@
  * \brief Program utilities.
  *//*--------------------------------------------------------------------*/
 
+#include "qpInfo.h"
+
 #include "vkPrograms.hpp"
 #include "vkShaderToSpirV.hpp"
 #include "vkSpirVAsm.hpp"
 #include "vkRefUtil.hpp"
 
+#include "deMutex.hpp"
+#include "deFilePath.hpp"
 #include "deArrayUtil.hpp"
 #include "deMemory.h"
 #include "deInt32.h"
+
+#include "tcuCommandLine.hpp"
+
+#include <map>
 
 namespace vk
 {
 
 using std::string;
 using std::vector;
+using std::map;
 
 #if defined(DE_DEBUG) && defined(DEQP_HAVE_SPIRV_TOOLS)
 #	define VALIDATE_BINARIES	true
@@ -112,78 +121,330 @@ void validateCompiledBinary(const vector<deUint32>& binary, glu::ShaderProgramIn
 	}
 }
 
-ProgramBinary* buildProgram (const GlslSource& program, glu::ShaderProgramInfo* buildInfo)
+
+#if defined(DEQP_HAVE_SPIRV_TOOLS)
+
+de::Mutex							cacheFileMutex;
+map<deUint32, vector<deUint32> >	cacheFileIndex;
+
+std::string intToString (deUint32 integer)
+{
+	std::stringstream temp_sstream;
+
+	temp_sstream << integer;
+
+	return temp_sstream.str();
+}
+
+// Parse chunked shader cache file for hashes and offsets
+void shaderCacheParse (const char* shaderCacheFile)
+{
+	FILE* file = fopen(shaderCacheFile, "rb");
+	int count = 0;
+	if (file)
+	{
+		deUint32 chunksize	= 0;
+		deUint32 hash		= 0;
+		deUint32 offset		= 0;
+		bool ok				= true;
+		while (ok)
+		{
+			offset = (deUint32)ftell(file);
+			if (ok) ok = fread(&chunksize, 1, 4, file)				== 4;
+			if (ok) ok = fread(&hash, 1, 4, file)					== 4;
+			if (ok) cacheFileIndex[hash].push_back(offset);
+			if (ok) ok = fseek(file, offset + chunksize, SEEK_SET)	== 0;
+			count++;
+		}
+		fclose(file);
+	}
+}
+
+vk::ProgramBinary* shadercacheLoad (const std::string& shaderstring, const char* shaderCacheFilename)
+{
+	deUint32		hash		= deStringHash(shaderstring.c_str());
+	deInt32			format;
+	deInt32			length;
+	deInt32			sourcelength;
+	deUint32		i;
+	deUint32		temp;
+	deUint8*		bin			= 0;
+	char*			source		= 0;
+	bool			ok			= true;
+	cacheFileMutex.lock();
+	if (cacheFileIndex.empty())
+		shaderCacheParse(shaderCacheFilename);
+
+	if (cacheFileIndex.count(hash) == 0)
+	{
+		cacheFileMutex.unlock();
+		return 0;
+	}
+	FILE*			file		= fopen(shaderCacheFilename, "rb");
+	ok				= file											!= 0;
+
+	for (i = 0; i < cacheFileIndex[hash].size(); i++)
+	{
+		if (ok) ok = fseek(file, cacheFileIndex[hash][i], SEEK_SET)	== 0;
+		if (ok) ok = fread(&temp, 1, 4, file)						== 4; // Chunk size (skip)
+		if (ok) ok = fread(&temp, 1, 4, file)						== 4; // Stored hash
+		if (ok) ok = temp											== hash; // Double check
+		if (ok) ok = fread(&format, 1, 4, file)						== 4;
+		if (ok) ok = fread(&length, 1, 4, file)						== 4;
+		if (ok) ok = length											> 0; // sanity check
+		if (ok) bin = new deUint8[length];
+		if (ok) ok = fread(bin, 1, length, file)					== (size_t)length;
+		if (ok) ok = fread(&sourcelength, 1, 4, file)				== 4;
+		if (ok && sourcelength > 0)
+		{
+			source = new char[sourcelength + 1];
+			ok = fread(source, 1, sourcelength, file)				== (size_t)sourcelength;
+			source[sourcelength] = 0;
+		}
+		if (!ok || shaderstring != std::string(source))
+		{
+			// Mismatch, but may still exist in cache if there were hash collisions
+			delete[] source;
+			delete[] bin;
+		}
+		else
+		{
+			delete[] source;
+			if (file) fclose(file);
+			cacheFileMutex.unlock();
+			return new vk::ProgramBinary((vk::ProgramFormat)format, length, bin);
+		}
+	}
+	if (file) fclose(file);
+	cacheFileMutex.unlock();
+	return 0;
+}
+
+void shadercacheSave (const vk::ProgramBinary* binary, const std::string& shaderstring, const char* shaderCacheFilename)
+{
+	if (binary == 0)
+		return;
+	deUint32			hash		= deStringHash(shaderstring.c_str());
+	deInt32				format		= binary->getFormat();
+	deUint32			length		= (deUint32)binary->getSize();
+	deUint32			chunksize;
+	deUint32			offset;
+	const deUint8*		bin			= binary->getBinary();
+	const de::FilePath	filePath	(shaderCacheFilename);
+
+	cacheFileMutex.lock();
+
+	if (!de::FilePath(filePath.getDirName()).exists())
+		de::createDirectoryAndParents(filePath.getDirName().c_str());
+
+	FILE*				file		= fopen(shaderCacheFilename, "ab");
+	if (!file)
+	{
+		cacheFileMutex.unlock();
+		return;
+	}
+	// Append mode starts writing from the end of the file,
+	// but unless we do a seek, ftell returns 0.
+	fseek(file, 0, SEEK_END);
+	offset		= (deUint32)ftell(file);
+	chunksize	= 4 + 4 + 4 + 4 + length + 4 + (deUint32)shaderstring.length();
+	fwrite(&chunksize, 1, 4, file);
+	fwrite(&hash, 1, 4, file);
+	fwrite(&format, 1, 4, file);
+	fwrite(&length, 1, 4, file);
+	fwrite(bin, 1, length, file);
+	length = (deUint32)shaderstring.length();
+	fwrite(&length, 1, 4, file);
+	fwrite(shaderstring.c_str(), 1, length, file);
+	fclose(file);
+	cacheFileIndex[hash].push_back(offset);
+
+	cacheFileMutex.unlock();
+}
+
+// Insert any information that may affect compilation into the shader string.
+void getCompileEnvironment (std::string& shaderstring)
+{
+	shaderstring += "GLSL:";
+	shaderstring += qpGetReleaseGlslName();
+	shaderstring += "\nSpir-v Tools:";
+	shaderstring += qpGetReleaseSpirvToolsName();
+	shaderstring += "\nSpir-v Headers:";
+	shaderstring += qpGetReleaseSpirvHeadersName();
+	shaderstring += "\n";
+}
+
+// Insert compilation options into the shader string.
+void getBuildOptions (std::string& shaderstring, const ShaderBuildOptions& buildOptions)
+{
+	shaderstring += "Target Spir-V ";
+	shaderstring += getSpirvVersionName(buildOptions.targetVersion);
+	shaderstring += "\n";
+	if (buildOptions.flags & ShaderBuildOptions::FLAG_ALLOW_RELAXED_OFFSETS)
+		shaderstring += "Flag:Allow relaxed offsets\n";
+	if (buildOptions.flags & ShaderBuildOptions::FLAG_USE_STORAGE_BUFFER_STORAGE_CLASS)
+		shaderstring += "Flag:Use storage buffer storage class\n";
+}
+
+ProgramBinary* buildProgram (const GlslSource& program, glu::ShaderProgramInfo* buildInfo, const tcu::CommandLine& commandLine)
 {
 	const SpirvVersion	spirvVersion	= program.buildOptions.targetVersion;
 	const bool			validateBinary	= VALIDATE_BINARIES;
 	vector<deUint32>	binary;
+	std::string			shaderstring;
+	vk::ProgramBinary*	res				= 0;
 
+	if (commandLine.isShadercacheEnabled())
 	{
-		vector<deUint32> nonStrippedBinary;
+		getCompileEnvironment(shaderstring);
+		getBuildOptions(shaderstring, program.buildOptions);
 
-		if (!compileGlslToSpirV(program, &nonStrippedBinary, buildInfo))
-			TCU_THROW(InternalError, "Compiling GLSL to SPIR-V failed");
+		for (int i = 0; i < glu::SHADERTYPE_LAST; i++)
+			for (std::vector<std::string>::const_iterator it = program.sources[i].begin(); it != program.sources[i].end(); ++it)
+				if (it->length() > 0)
+				{
+					shaderstring += glu::getShaderTypeName((glu::ShaderType)i);
+					shaderstring += *it;
+				}
 
-		TCU_CHECK_INTERNAL(!nonStrippedBinary.empty());
-		stripSpirVDebugInfo(nonStrippedBinary.size(), &nonStrippedBinary[0], &binary);
-		TCU_CHECK_INTERNAL(!binary.empty());
+		res = shadercacheLoad(shaderstring, commandLine.getShaderCacheFilename());
 	}
 
-	if (validateBinary)
-		validateCompiledBinary(binary, buildInfo, spirvVersion);
+	if (!res)
+	{
+		{
+			vector<deUint32> nonStrippedBinary;
 
-	return createProgramBinaryFromSpirV(binary);
+			if (!compileGlslToSpirV(program, &nonStrippedBinary, buildInfo))
+				TCU_THROW(InternalError, "Compiling GLSL to SPIR-V failed");
+
+			TCU_CHECK_INTERNAL(!nonStrippedBinary.empty());
+			stripSpirVDebugInfo(nonStrippedBinary.size(), &nonStrippedBinary[0], &binary);
+			TCU_CHECK_INTERNAL(!binary.empty());
+		}
+
+		if (validateBinary)
+			validateCompiledBinary(binary, buildInfo, spirvVersion);
+
+		res = createProgramBinaryFromSpirV(binary);
+		if (commandLine.isShadercacheEnabled())
+			shadercacheSave(res, shaderstring, commandLine.getShaderCacheFilename());
+	}
+	return res;
 }
 
-ProgramBinary* buildProgram (const HlslSource& program, glu::ShaderProgramInfo* buildInfo)
+ProgramBinary* buildProgram (const HlslSource& program, glu::ShaderProgramInfo* buildInfo, const tcu::CommandLine& commandLine)
 {
 	const SpirvVersion	spirvVersion	= program.buildOptions.targetVersion;
 	const bool			validateBinary	= VALIDATE_BINARIES;
 	vector<deUint32>	binary;
+	std::string			shaderstring;
+	vk::ProgramBinary*	res				= 0;
 
+	if (commandLine.isShadercacheEnabled())
 	{
-		vector<deUint32> nonStrippedBinary;
+		getCompileEnvironment(shaderstring);
+		getBuildOptions(shaderstring, program.buildOptions);
 
-		if (!compileHlslToSpirV(program, &nonStrippedBinary, buildInfo))
-			TCU_THROW(InternalError, "Compiling HLSL to SPIR-V failed");
+		for (int i = 0; i < glu::SHADERTYPE_LAST; i++)
+			for (std::vector<std::string>::const_iterator it = program.sources[i].begin(); it != program.sources[i].end(); ++it)
+				if (it->length() > 0)
+				{
+					shaderstring += glu::getShaderTypeName((glu::ShaderType)i);
+					shaderstring += *it;
+				}
 
-		TCU_CHECK_INTERNAL(!nonStrippedBinary.empty());
-		stripSpirVDebugInfo(nonStrippedBinary.size(), &nonStrippedBinary[0], &binary);
-		TCU_CHECK_INTERNAL(!binary.empty());
+		res = shadercacheLoad(shaderstring, commandLine.getShaderCacheFilename());
 	}
 
-	if (validateBinary)
-		validateCompiledBinary(binary, buildInfo, spirvVersion);
+	if (!res)
+	{
+		{
+			vector<deUint32> nonStrippedBinary;
 
-	return createProgramBinaryFromSpirV(binary);
+			if (!compileHlslToSpirV(program, &nonStrippedBinary, buildInfo))
+				TCU_THROW(InternalError, "Compiling HLSL to SPIR-V failed");
+
+			TCU_CHECK_INTERNAL(!nonStrippedBinary.empty());
+			stripSpirVDebugInfo(nonStrippedBinary.size(), &nonStrippedBinary[0], &binary);
+			TCU_CHECK_INTERNAL(!binary.empty());
+		}
+
+		if (validateBinary)
+			validateCompiledBinary(binary, buildInfo, spirvVersion);
+
+		res = createProgramBinaryFromSpirV(binary);
+		if (commandLine.isShadercacheEnabled())
+			shadercacheSave(res, shaderstring, commandLine.getShaderCacheFilename());
+	}
+	return res;
 }
 
-ProgramBinary* assembleProgram (const SpirVAsmSource& program, SpirVProgramInfo* buildInfo)
+ProgramBinary* assembleProgram (const SpirVAsmSource& program, SpirVProgramInfo* buildInfo, const tcu::CommandLine& commandLine)
 {
 	const SpirvVersion	spirvVersion		= program.buildOptions.targetVersion;
 	const bool			validateBinary		= VALIDATE_BINARIES;
 	vector<deUint32>	binary;
+	vk::ProgramBinary*	res					= 0;
+	std::string			shaderstring;
 
-	if (!assembleSpirV(&program, &binary, buildInfo, spirvVersion))
-		TCU_THROW(InternalError, "Failed to assemble SPIR-V");
-
-	if (validateBinary)
+	if (commandLine.isShadercacheEnabled())
 	{
-		std::ostringstream	validationLog;
+		getCompileEnvironment(shaderstring);
+		shaderstring += "Target Spir-V ";
+		shaderstring += getSpirvVersionName(spirvVersion);
+		shaderstring += "\n";
 
-		if (!validateSpirV(binary.size(), &binary[0], &validationLog, spirvVersion))
-		{
-			buildInfo->compileOk	 = false;
-			buildInfo->infoLog		+= "\n" + validationLog.str();
+		shaderstring += program.source;
 
-			TCU_THROW(InternalError, "Validation failed for assembled SPIR-V binary");
-		}
+		res = shadercacheLoad(shaderstring, commandLine.getShaderCacheFilename());
 	}
 
-	return createProgramBinaryFromSpirV(binary);
+	if (!res)
+	{
+
+		if (!assembleSpirV(&program, &binary, buildInfo, spirvVersion))
+			TCU_THROW(InternalError, "Failed to assemble SPIR-V");
+
+		if (validateBinary)
+		{
+			std::ostringstream	validationLog;
+
+			if (!validateSpirV(binary.size(), &binary[0], &validationLog, spirvVersion))
+			{
+				buildInfo->compileOk = false;
+				buildInfo->infoLog += "\n" + validationLog.str();
+
+				TCU_THROW(InternalError, "Validation failed for assembled SPIR-V binary");
+			}
+		}
+
+		res = createProgramBinaryFromSpirV(binary);
+		if (commandLine.isShadercacheEnabled())
+			shadercacheSave(res, shaderstring, commandLine.getShaderCacheFilename());
+	}
+	return res;
 }
 
-void disassembleProgram (const ProgramBinary& program, std::ostream* dst, SpirvVersion spirvVersion)
+#else // !DEQP_HAVE_SPIRV_TOOLS
+
+ProgramBinary* buildProgram (const GlslSource&, glu::ShaderProgramInfo*, const tcu::CommandLine&)
+{
+	TCU_THROW(NotSupportedError, "GLSL to SPIR-V compilation not supported (DEQP_HAVE_GLSLANG not defined)");
+}
+
+ProgramBinary* buildProgram (const HlslSource&, glu::ShaderProgramInfo*, const tcu::CommandLine&)
+{
+	TCU_THROW(NotSupportedError, "HLSL to SPIR-V compilation not supported (DEQP_HAVE_GLSLANG not defined)");
+}
+
+ProgramBinary* assembleProgram (const SpirVAsmSource&, SpirVProgramInfo*, const tcu::CommandLine&)
+{
+	TCU_THROW(NotSupportedError, "SPIR-V assembly not supported (DEQP_HAVE_SPIRV_TOOLS not defined)");
+}
+#endif
+
+ void disassembleProgram (const ProgramBinary& program, std::ostream* dst, SpirvVersion spirvVersion)
 {
 	if (program.getFormat() == PROGRAM_FORMAT_SPIRV)
 	{

external/vulkancts/framework/vulkan/vkPrograms.hpp
@@ -207,9 +207,9 @@ struct SourceCollections
 
 typedef ProgramCollection<ProgramBinary, BinaryBuildOptions>	BinaryCollection;
 
-ProgramBinary*			buildProgram		(const GlslSource& program, glu::ShaderProgramInfo* buildInfo);
-ProgramBinary*			buildProgram		(const HlslSource& program, glu::ShaderProgramInfo* buildInfo);
-ProgramBinary*			assembleProgram		(const vk::SpirVAsmSource& program, SpirVProgramInfo* buildInfo);
+ProgramBinary*			buildProgram		(const GlslSource& program, glu::ShaderProgramInfo* buildInfo, const tcu::CommandLine& commandLine);
+ProgramBinary*			buildProgram		(const HlslSource& program, glu::ShaderProgramInfo* buildInfo, const tcu::CommandLine& commandLine);
+ProgramBinary*			assembleProgram		(const vk::SpirVAsmSource& program, SpirVProgramInfo* buildInfo, const tcu::CommandLine& commandLine);
 void					disassembleProgram	(const ProgramBinary& program, std::ostream* dst, SpirvVersion spirvVersion);
 bool					validateProgram		(const ProgramBinary& program, std::ostream* dst, SpirvVersion spirvVersion);
 

external/vulkancts/modules/vulkan/vktBuildPrograms.cpp
@@ -246,11 +246,17 @@ class BuildHighLevelShaderTask : public Task
 public:
 
 	BuildHighLevelShaderTask (const Source& source, Program* program)
-		: m_source	(source)
-		, m_program	(program)
+		: m_source		(source)
+		, m_program		(program)
+		, m_commandLine	(0)
 	{}
 
 	BuildHighLevelShaderTask (void) : m_program(DE_NULL) {}
+
+	void setCommandline (const tcu::CommandLine &commandLine)
+	{
+		m_commandLine = &commandLine;
+	}
 
 	void execute (void)
 	{
@@ -259,8 +265,8 @@ public:
 		try
 		{
 			DE_ASSERT(m_source.buildOptions.targetVersion < vk::SPIRV_VERSION_LAST);
-
-			m_program->binary		= ProgramBinarySp(vk::buildProgram(m_source, &buildInfo));
+			DE_ASSERT(m_commandLine != DE_NULL);
+			m_program->binary		= ProgramBinarySp(vk::buildProgram(m_source, &buildInfo, *m_commandLine));
 			m_program->buildStatus	= Program::STATUS_PASSED;
 		}
 		catch (const tcu::Exception&)
@@ -275,8 +281,9 @@ public:
 	}
 
 private:
-	Source		m_source;
-	Program*	m_program;
+	Source					m_source;
+	Program*				m_program;
+	const tcu::CommandLine*	m_commandLine;
 };
 
 void writeBuildLogs (const vk::SpirVProgramInfo& buildInfo, std::ostream& dst)
@@ -293,11 +300,17 @@ class BuildSpirVAsmTask : public Task
 {
 public:
 	BuildSpirVAsmTask (const vk::SpirVAsmSource& source, Program* program)
-		: m_source	(source)
-		, m_program	(program)
+		: m_source		(source)
+		, m_program		(program)
+		, m_commandLine	(0)
 	{}
 
 	BuildSpirVAsmTask (void) : m_program(DE_NULL) {}
+
+	void setCommandline (const tcu::CommandLine &commandLine)
+	{
+		m_commandLine = &commandLine;
+	}
 
 	void execute (void)
 	{
@@ -306,8 +319,8 @@ public:
 		try
 		{
 			DE_ASSERT(m_source.buildOptions.targetVersion < vk::SPIRV_VERSION_LAST);
-
-			m_program->binary		= ProgramBinarySp(vk::assembleProgram(m_source, &buildInfo));
+			DE_ASSERT(m_commandLine != DE_NULL);
+			m_program->binary		= ProgramBinarySp(vk::assembleProgram(m_source, &buildInfo, *m_commandLine));
 			m_program->buildStatus	= Program::STATUS_PASSED;
 		}
 		catch (const tcu::Exception&)
@@ -322,8 +335,9 @@ public:
 	}
 
 private:
-	vk::SpirVAsmSource	m_source;
-	Program*			m_program;
+	vk::SpirVAsmSource		m_source;
+	Program*				m_program;
+	const tcu::CommandLine*	m_commandLine;
 };
 
 class ValidateBinaryTask : public Task
@@ -437,6 +451,7 @@ BuildStats buildPrograms (tcu::TestContext&			testCtx,
 
 						programs.pushBack(Program(vk::ProgramIdentifier(casePath, progIter.getName()), progIter.getProgram().buildOptions.targetVersion));
 						buildGlslTasks.pushBack(BuildHighLevelShaderTask<vk::GlslSource>(progIter.getProgram(), &programs.back()));
+						buildGlslTasks.back().setCommandline(testCtx.getCommandLine());
 						executor.submit(&buildGlslTasks.back());
 					}
 
@@ -450,6 +465,7 @@ BuildStats buildPrograms (tcu::TestContext&			testCtx,
 
 						programs.pushBack(Program(vk::ProgramIdentifier(casePath, progIter.getName()), progIter.getProgram().buildOptions.targetVersion));
 						buildHlslTasks.pushBack(BuildHighLevelShaderTask<vk::HlslSource>(progIter.getProgram(), &programs.back()));
+						buildHlslTasks.back().setCommandline(testCtx.getCommandLine());
 						executor.submit(&buildHlslTasks.back());
 					}
 
@@ -463,6 +479,7 @@ BuildStats buildPrograms (tcu::TestContext&			testCtx,
 
 						programs.pushBack(Program(vk::ProgramIdentifier(casePath, progIter.getName()), progIter.getProgram().buildOptions.targetVersion));
 						buildSpirvAsmTasks.pushBack(BuildSpirVAsmTask(progIter.getProgram(), &programs.back()));
+						buildSpirvAsmTasks.back().setCommandline(testCtx.getCommandLine());
 						executor.submit(&buildSpirvAsmTasks.back());
 					}
 				}
@@ -535,10 +552,18 @@ BuildStats buildPrograms (tcu::TestContext&			testCtx,
 namespace opt
 {
 
-DE_DECLARE_COMMAND_LINE_OPT(DstPath,		std::string);
-DE_DECLARE_COMMAND_LINE_OPT(Cases,			std::string);
-DE_DECLARE_COMMAND_LINE_OPT(Validate,		bool);
-DE_DECLARE_COMMAND_LINE_OPT(VulkanVersion,	deUint32);
+DE_DECLARE_COMMAND_LINE_OPT(DstPath,				std::string);
+DE_DECLARE_COMMAND_LINE_OPT(Cases,					std::string);
+DE_DECLARE_COMMAND_LINE_OPT(Validate,				bool);
+DE_DECLARE_COMMAND_LINE_OPT(VulkanVersion,			deUint32);
+DE_DECLARE_COMMAND_LINE_OPT(ShaderCache,			bool);
+DE_DECLARE_COMMAND_LINE_OPT(ShaderCacheFilename,	std::string);
+
+static const de::cmdline::NamedValue<bool> s_enableNames[] =
+{
+	{ "enable",		true },
+	{ "disable",	false }
+};
 
 void registerOptions (de::cmdline::Parser& parser)
 {
@@ -556,7 +581,9 @@ void registerOptions (de::cmdline::Parser& parser)
 	parser << Option<opt::DstPath>				("d", "dst-path",				"Destination path",	"out")
 		   << Option<opt::Cases>				("n", "deqp-case",				"Case path filter (works as in test binaries)")
 		   << Option<opt::Validate>				("v", "validate-spv",			"Validate generated SPIR-V binaries")
-		   << Option<opt::VulkanVersion>		("t", "target-vulkan-version",	"Target Vulkan version", s_vulkanVersion, "1.1");
+		   << Option<opt::VulkanVersion>		("t", "target-vulkan-version",	"Target Vulkan version", s_vulkanVersion, "1.1")
+		   << Option<opt::ShaderCache>			("s", "shadercache",			"Enable or disable shader cache", s_enableNames, "disable")
+		   << Option<opt::ShaderCacheFilename>	("r", "shadercache-filename",	"Write shader cache to given file", "shadercache.bin");
 }
 
 } // opt
@@ -585,6 +612,21 @@ int main (int argc, const char* argv[])
 		{
 			deqpArgv.push_back("--deqp-case");
 			deqpArgv.push_back(cmdLine.getOption<opt::Cases>().c_str());
+		}
+
+		if (cmdLine.hasOption<opt::ShaderCacheFilename>())
+		{
+			deqpArgv.push_back("--deqp-shadercache-filename");
+			deqpArgv.push_back(cmdLine.getOption<opt::ShaderCacheFilename>().c_str());
+		}
+
+		if (cmdLine.hasOption<opt::ShaderCache>())
+		{
+			deqpArgv.push_back("--deqp-shadercache");
+			if (cmdLine.getOption<opt::ShaderCache>())
+				deqpArgv.push_back("enable");
+			else
+				deqpArgv.push_back("disable");
 		}
 
 		if (!deqpCmdLine.parse((int)deqpArgv.size(), &deqpArgv[0]))

commit 290f855035bea79e834b2f2747000405beb777c3

allow building from git without glsl/spirv tools
Other parts of the build system support this just fine.

Fixes: 774de0ae8a0 ("Added shader cache to speed up runtime")

framework/qphelper/gen_release_info.py
@@ -96,21 +96,26 @@ if __name__ == "__main__":
 
 	if args.git:
 		gitDir				= args.gitDir				if args.gitDir				!= None else defaultGitDir
-		glslGitDir			= args.glslGitDir			if args.glslGitDir			!= None else defaultGlslGitDir
-		spirvToolsGitDir	= args.spirvToolsGitDir		if args.spirvToolsGitDir	!= None else defaultSpirvToolsGitDir
-		spirvHeadersGitDir	= args.spirvHeadersGitDir	if args.spirvHeadersGitDir	!= None else defaultSpirvHeadersGitDir
 		head				= getHead(gitDir)
-		glslHead			= getHead(glslGitDir)
-		spirvToolsHead		= getHead(spirvToolsGitDir)
-		spirvHeadersHead	= getHead(spirvHeadersGitDir)
 		releaseName			= "git-%s" % head
 		releaseId			= "0x%s" % head[0:8]
-		glslName			= "git-%s" % glslHead
-		spirvToolsName		= "git-%s" % spirvToolsHead
-		spirvHeadersName	= "git-%s" % spirvHeadersHead
 	else:
 		releaseName			= args.releaseName
 		releaseId			= args.releaseId
+
+	if args.glslGitDir or os.path.exists(defaultGlslGitDir):
+		glslGitDir			= args.glslGitDir			if args.glslGitDir			!= None else defaultGlslGitDir
+		spirvToolsGitDir	= args.spirvToolsGitDir		if args.spirvToolsGitDir	!= None else defaultSpirvToolsGitDir
+		spirvHeadersGitDir	= args.spirvHeadersGitDir	if args.spirvHeadersGitDir	!= None else defaultSpirvHeadersGitDir
+		glslHead			= getHead(glslGitDir)
+		spirvToolsHead		= getHead(spirvToolsGitDir)
+		spirvHeadersHead	= getHead(spirvHeadersGitDir)
+		glslName			= "git-%s" % glslHead
+		spirvToolsName		= "git-%s" % spirvToolsHead
+		spirvHeadersName	= "git-%s" % spirvHeadersHead
+	elif args.git:
+		glslName = spirvToolsName = spirvHeadersName = "N/A"
+	else:
 		glslName			= args.releaseName
 		spirvToolsName		= args.releaseName
 		spirvHeadersName	= args.releaseName