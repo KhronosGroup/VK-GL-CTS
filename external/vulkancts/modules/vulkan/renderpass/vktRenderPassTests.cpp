86121ffa3d91a791f4091ee12df4f6381eb4b4ce
Chris Forbes <chrisforbes@google.com>
2018-05-18 15:50:39 -0700

    Add missing self-dependency in color->input self-dep tests
    Change-Id: Ibf400c91bbc59abb49795078804e35e50c59c7db
    VK-GL-CTS: 1181
    Components: Vulkan
    Affects: dEQP-VK.renderpass.*

--- external/vulkancts/modules/vulkan/renderpass/vktRenderPassTests.cpp
+++ external/vulkancts/modules/vulkan/renderpass/vktRenderPassTests.cpp
@@ -5848,6 +5848,13 @@ void addFormatTests (tcu::TestCaseGroup* group, AllocationKind allocationKind)
 																vk::VK_ACCESS_INPUT_ATTACHMENT_READ_BIT,
 																vk::VK_DEPENDENCY_BY_REGION_BIT));
 
+								deps.push_back(SubpassDependency(1, 1,
+																vk::VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT,
+																vk::VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT,
+																vk::VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT,
+																vk::VK_ACCESS_INPUT_ATTACHMENT_READ_BIT,
+																vk::VK_DEPENDENCY_BY_REGION_BIT));
+
 								if (useInputAspect)
 								{
 									const VkInputAttachmentAspectReference inputAspect =
@@ -6094,6 +6101,13 @@ void addFormatTests (tcu::TestCaseGroup* group, AllocationKind allocationKind)
 																vk::VK_PIPELINE_STAGE_EARLY_FRAGMENT_TESTS_BIT | vk::VK_PIPELINE_STAGE_LATE_FRAGMENT_TESTS_BIT,
 																vk::VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT,
 
+																vk::VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT,
+																vk::VK_ACCESS_INPUT_ATTACHMENT_READ_BIT,
+																vk::VK_DEPENDENCY_BY_REGION_BIT));
+
+								deps.push_back(SubpassDependency(1, 1,
+																vk::VK_PIPELINE_STAGE_EARLY_FRAGMENT_TESTS_BIT | vk::VK_PIPELINE_STAGE_LATE_FRAGMENT_TESTS_BIT,
+																vk::VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT,
 																vk::VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT,
 																vk::VK_ACCESS_INPUT_ATTACHMENT_READ_BIT,
 																vk::VK_DEPENDENCY_BY_REGION_BIT));
