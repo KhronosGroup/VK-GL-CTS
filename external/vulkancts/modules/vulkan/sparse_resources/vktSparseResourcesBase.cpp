59f96193ba30d27f821db10ca3b33cbd08be5b0c
Alexander Galazin <alexander.galazin@arm.com>
2019-03-22 10:06:18 +0100

    Fix uninitialized variable reported by gcc 8.3
    VK-GL-CTS issue: 1656
    
    Components: Vulkan
    
    Affects: dEQP-VK.sparse_resources.*
    
    Change-Id: I298c795b05c0c204f04970da7a923f797c4478b8
    (cherry picked from commit 1dadbfc46128054db7679bb662eba16b20a241c1)

--- external/vulkancts/modules/vulkan/sparse_resources/vktSparseResourcesBase.cpp
+++ external/vulkancts/modules/vulkan/sparse_resources/vktSparseResourcesBase.cpp
@@ -139,7 +139,7 @@ void SparseResourcesBaseInstance::createDeviceSupportingQueues(const QueueRequir
 
 			for (deUint32 queueNdx = 0; queueNdx < queuesPerFamilyCount; ++queueNdx)
 			{
-				Queue queue;
+				Queue queue				= {DE_NULL, 0, 0};
 				queue.queueFamilyIndex	= queueFamilyIndex;
 				queue.queueIndex		= queueNdx;
 
