ea11a8cdb5005d8bcda6051594cfcd33c7163102
Paul Thomson <paulthomson@google.com>
2019-07-01 15:17:58 +0100

    dEQP-VK.graphicsfuzz.early-return-and-barrier
    Components: Vulkan
    
    New Tests:
    
    dEQP-VK.graphicsfuzz.early-return-and-barrier
    
    Change-Id: I72818ac38bb0552f286a70b82f23907ada8a7059

--- external/vulkancts/modules/vulkan/amber/vktAmberGraphicsFuzzTests.cpp
+++ external/vulkancts/modules/vulkan/amber/vktAmberGraphicsFuzzTests.cpp
@@ -50,6 +50,7 @@ void createAmberTests (tcu::TestCaseGroup* group)
 		{	"control-flow-switch.amber",					"control-flow-switch",					"A fragment shader with somewhat complex control flow and a switch"						},
 		{	"dead-barriers-in-loops.amber",					"dead-barriers-in-loops",				"A compute shader with dead barriers"													},
 		{	"dead-struct-init.amber",						"dead-struct-init",						"A fragment shader that uses struct initializers"										},
+		{	"early-return-and-barrier.amber",				"early-return-and-barrier",				"A compute shader with an early return and a barrier"									},
 		{	"fragcoord-control-flow.amber",					"fragcoord-control-flow",				"A fragment shader that uses FragCoord and somewhat complex control flow"				},
 		{	"fragcoord-control-flow-2.amber",				"fragcoord-control-flow-2",				"A fragment shader that uses FragCoord and somewhat complex control flow"				},
 		{	"if-and-switch.amber",							"if-and-switch",						"A fragment shader with a switch and some data flow"									},
