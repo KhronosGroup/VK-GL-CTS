KhronosGroup/VK-GL-CTS

commit 7b7791a40fa263fef71a5dcb39154fb935ddd322

Record command line arguments in .qpa file
Command line parameters are written to .qpa file in sessionInfo section.

Components: Framework

VK-GL-CTS issue: 1666

Change-Id: If8fb1ce85408141362f046c318016039e06e228a

framework/common/tcuTestLog.cpp
@@ -180,8 +180,8 @@ TestLog& SampleBuilder::operator<< (const TestLog::EndSampleToken&)
 
 // TestLog
 
-TestLog::TestLog (const char* fileName, deUint32 flags)
-	: m_log(qpTestLog_createFileLog(fileName, flags))
+TestLog::TestLog (const char* fileName, int argc, char** argv, deUint32 flags)
+	: m_log(qpTestLog_createFileLog(fileName, argc, argv, flags))
 {
 	if (!m_log)
 		throw ResourceError(std::string("Failed to open test log file '") + fileName + "'");

framework/common/tcuTestLog.hpp
@@ -102,7 +102,7 @@ public:
 	typedef LogNumber<float>		Float;
 	typedef LogNumber<deInt64>		Integer;
 
-	explicit			TestLog					(const char* fileName, deUint32 flags = 0);
+	explicit			TestLog					(const char* fileName, int argc = 0, char** argv = DE_NULL, deUint32 flags = 0);
 						~TestLog				(void);
 
 	MessageBuilder		operator<<				(const BeginMessageToken&);

executor/xeBatchResult.hpp
@@ -42,6 +42,7 @@ public:
 	std::string			releaseName;
 	std::string			releaseId;
 	std::string			targetName;
+	std::string			qpaCommandLineParameters;
 
 	// Produced by Candy.
 	std::string			candyTargetName;

executor/xeTestLogParser.cpp
@@ -103,6 +103,8 @@ void TestLogParser::parse (const deUint8* bytes, size_t numBytes)
 					m_sessionInfo.resultName = value;
 				else if (deStringEqual(attribute, "timestamp"))
 					m_sessionInfo.timestamp = value;
+				else if (deStringEqual(attribute, "commandLineParameters"))
+					m_sessionInfo.qpaCommandLineParameters = value;
 
 				// \todo [2012-06-09 pyry] What to do with unknown/duplicate attributes? Currently just ignored.
 				break;

framework/platform/tcuMain.cpp
@@ -45,7 +45,7 @@ int main (int argc, char** argv)
 	{
 		tcu::CommandLine				cmdLine		(argc, argv);
 		tcu::DirArchive					archive		(".");
-		tcu::TestLog					log			(cmdLine.getLogFileName(), cmdLine.getLogFlags());
+		tcu::TestLog					log			(cmdLine.getLogFileName(), argc-1, argv+1, cmdLine.getLogFlags());
 		de::UniquePtr<tcu::Platform>	platform	(createPlatform());
 		de::UniquePtr<tcu::App>			app			(new tcu::App(*platform, archive, log, cmdLine));
 

framework/qphelper/qpTestLog.c
@@ -289,7 +289,7 @@ DE_INLINE void doubleToString (double value, char* buf, size_t bufSize)
 	deSprintf(buf, bufSize, "%f", value);
 }
 
-static deBool beginSession (qpTestLog* log)
+static deBool beginSession (qpTestLog* log, int argc, char** argv)
 {
 	DE_ASSERT(log && !log->isSessionOpen);
 
@@ -297,6 +297,14 @@ static deBool beginSession (qpTestLog* log)
 	fprintf(log->outputFile, "#sessionInfo releaseName %s\n", qpGetReleaseName());
 	fprintf(log->outputFile, "#sessionInfo releaseId 0x%08x\n", qpGetReleaseId());
 	fprintf(log->outputFile, "#sessionInfo targetName \"%s\"\n", qpGetTargetName());
+	fprintf(log->outputFile, "#sessionInfo commandLineParameters \"");
+	for (int i = 0; i < argc && argv != NULL; ++i)
+	{
+		fprintf(log->outputFile, "%s", argv[i]);
+		if (i < argc-1)
+			fprintf(log->outputFile, " ");
+	}
+	fprintf(log->outputFile, "\"\n");
 
     /* Write out #beginSession. */
 	fprintf(log->outputFile, "#beginSession\n");
@@ -328,7 +336,7 @@ static deBool endSession (qpTestLog* log)
  * \param fileName Name of the file where to put logs
  * \return qpTestLog instance, or DE_NULL if cannot create file
  *//*--------------------------------------------------------------------*/
-qpTestLog* qpTestLog_createFileLog (const char* fileName, deUint32 flags)
+qpTestLog* qpTestLog_createFileLog (const char* fileName, int argc, char** argv, deUint32 flags)
 {
 	qpTestLog* log = (qpTestLog*)deCalloc(sizeof(qpTestLog));
 	if (!log)
@@ -371,7 +379,7 @@ qpTestLog* qpTestLog_createFileLog (const char* fileName, deUint32 flags)
 		return DE_NULL;
 	}
 
-	beginSession(log);
+	beginSession(log, argc, argv);
 
 	return log;
 }

framework/qphelper/qpTestLog.h
@@ -188,7 +188,7 @@ typedef struct qpEglConfigInfo_s
 } qpEglConfigInfo;
 
 
-qpTestLog*		qpTestLog_createFileLog			(const char* fileName, deUint32 flags);
+qpTestLog*		qpTestLog_createFileLog			(const char* fileName, int argc, char** argv, deUint32 flags);
 void			qpTestLog_destroy				(qpTestLog* log);
 
 deBool			qpTestLog_startCase				(qpTestLog* log, const char* testCasePath, qpTestCaseType testCaseType);

commit a62bc26741eb7156d06798b433c4231da22f12bd

Fix command line parameters processing of TestLog
Change 7b7791a40f for issue 1666 has added functionality
to TestLog by adding extra parameters to the constructor.
The change, however, has patchied only one of the uses of the
class, breaking the other 3 uses of it. This has caused the
log flags to be wrong, and the new functionality introduced
by 7b7791a40f to not work, as expected.

This change fixes the rest of the locations and is adding one
more constructor for the TestLog class targeted for use by Android.

Components: Framework

VK-GL-CTS issue: 1887

Change-Id: I8d52d40ee5dffb120b8a4945b0f865c8f3e7b7dd

external/openglcts/modules/runner/glcTestRunner.cpp
@@ -48,7 +48,7 @@ class RunSession
 public:
 	RunSession(tcu::Platform& platform, tcu::Archive& archive, const int numArgs, const char* const* args)
 		: m_cmdLine(numArgs, args)
-		, m_log(m_cmdLine.getLogFileName(), m_cmdLine.getLogFlags())
+		, m_log(m_cmdLine.getLogFileName(), (numArgs - 1), (char**)(args + 1), m_cmdLine.getLogFlags())
 		, m_app(platform, archive, m_log, m_cmdLine)
 	{
 	}

external/vulkancts/modules/vulkan/vktBuildPrograms.cpp
@@ -667,7 +667,7 @@ int main (int argc, const char* argv[])
 	try
 	{
 		tcu::DirArchive			archive					(".");
-		tcu::TestLog			log						(deqpCmdLine.getLogFileName(), deqpCmdLine.getLogFlags());
+		tcu::TestLog			log					(deqpCmdLine.getLogFileName(), (argc - 1), (char **)(argv + 1), deqpCmdLine.getLogFlags());
 		tcu::Platform			platform;
 		tcu::TestContext		testCtx					(platform, archive, log, deqpCmdLine, DE_NULL);
 		vk::SpirvVersion		baselineSpirvVersion	= vk::getBaselineSpirvVersion(cmdLine.getOption<opt::VulkanVersion>());

framework/common/tcuTestLog.cpp
@@ -21,6 +21,7 @@
  * \brief Test Log C++ Wrapper.
  *//*--------------------------------------------------------------------*/
 
+#include "deCommandLine.h"
 #include "tcuTestLog.hpp"
 #include "tcuTextureUtil.hpp"
 #include "tcuSurface.hpp"
@@ -183,6 +184,20 @@ TestLog& SampleBuilder::operator<< (const TestLog::EndSampleToken&)
 TestLog::TestLog (const char* fileName, int argc, char** argv, deUint32 flags)
 	: m_log(qpTestLog_createFileLog(fileName, argc, argv, flags))
 {
+	if (!m_log)
+		throw ResourceError(std::string("Failed to open test log file '") + fileName + "'");
+}
+
+TestLog::TestLog (const char* fileName, const std::string& cmdLine, deUint32 flags)
+{
+
+	deCommandLine* parsedCmdLine = deCommandLine_parse(cmdLine.c_str());
+	if (!parsedCmdLine)
+		throw std::bad_alloc();
+
+	m_log = qpTestLog_createFileLog(fileName, parsedCmdLine->numArgs, parsedCmdLine->args, flags);
+	deCommandLine_destroy(parsedCmdLine);
+
 	if (!m_log)
 		throw ResourceError(std::string("Failed to open test log file '") + fileName + "'");
 }

framework/common/tcuTestLog.hpp
@@ -103,6 +103,7 @@ public:
 	typedef LogNumber<deInt64>		Integer;
 
 	explicit			TestLog					(const char* fileName, int argc = 0, char** argv = DE_NULL, deUint32 flags = 0);
+	explicit			TestLog					(const char* fileName, const std::string& cmdLine, deUint32 flags = 0);
 						~TestLog				(void);
 
 	MessageBuilder		operator<<				(const BeginMessageToken&);

framework/platform/android/tcuAndroidTestActivity.cpp
@@ -38,12 +38,12 @@ namespace Android
 
 // TestThread
 
-TestThread::TestThread (NativeActivity& activity, const CommandLine& cmdLine)
+TestThread::TestThread (NativeActivity& activity, const std::string& cmdLineString, const CommandLine& cmdLine)
 	: RenderThread	(activity)
 	, m_cmdLine		(cmdLine)
 	, m_platform	(activity)
 	, m_archive		(activity.getNativeActivity()->assetManager)
-	, m_log			(m_cmdLine.getLogFileName(), m_cmdLine.getLogFlags())
+	, m_log			(m_cmdLine.getLogFileName(), cmdLineString, m_cmdLine.getLogFlags())
 	, m_app			(m_platform, m_archive, m_log, m_cmdLine)
 	, m_finished	(false)
 {
@@ -87,7 +87,7 @@ bool TestThread::render (void)
 TestActivity::TestActivity (ANativeActivity* activity)
 	: RenderActivity	(activity)
 	, m_cmdLine			(getIntentStringExtra(activity, "cmdLine"))
-	, m_testThread		(*this, m_cmdLine)
+	, m_testThread		(*this, getIntentStringExtra(activity, "cmdLine"), m_cmdLine)
 	, m_started			(false)
 {
 	// Set initial orientation.

framework/platform/android/tcuAndroidTestActivity.hpp
@@ -39,7 +39,7 @@ namespace Android
 class TestThread : public RenderThread
 {
 public:
-							TestThread					(NativeActivity& activity, const CommandLine& cmdLine);
+							TestThread					(NativeActivity& activity, const std::string& cmdLineString, const CommandLine& cmdLine);
 							~TestThread					(void);
 
 	void					run							(void);