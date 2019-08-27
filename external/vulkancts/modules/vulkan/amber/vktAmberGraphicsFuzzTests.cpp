0176fc2b22b9f0f4f1d4a01b9031f816115ecaae
Paul Thomson <paulthomson@google.com>
2019-08-27 10:57:30 +0100

    dEQP-VK.graphicsfuzz.discard-continue-return
    Components: Vulkan
    
    New Tests:
    
    dEQP-VK.graphicsfuzz.discard-continue-return
    
    Change-Id: I8645d5ccd5fa0259db46b7f38e0c5041bcf9b6c8

--- external/vulkancts/modules/vulkan/amber/vktAmberGraphicsFuzzTests.cpp
+++ external/vulkancts/modules/vulkan/amber/vktAmberGraphicsFuzzTests.cpp
@@ -53,6 +53,7 @@ void createAmberTests (tcu::TestCaseGroup* group)
 		{	"control-flow-switch.amber",					"control-flow-switch",					"A fragment shader with somewhat complex control flow and a switch"						},
 		{	"dead-barriers-in-loops.amber",					"dead-barriers-in-loops",				"A compute shader with dead barriers"													},
 		{	"dead-struct-init.amber",						"dead-struct-init",						"A fragment shader that uses struct initializers"										},
+		{	"discard-continue-return.amber",				"discard-continue-return",				"A fragment shader with a discard, continue, and return"								},
 		{	"do-while-loop-in-conditionals.amber",			"do-while-loop-in-conditionals",		"A fragment shader with do-while loop in conditional nest"								},
 		{	"early-return-and-barrier.amber",				"early-return-and-barrier",				"A compute shader with an early return and a barrier"									},
 		{	"for-condition-always-false.amber",				"for-condition-always-false",			"A fragment shader that uses a for loop with condition always false"					},
