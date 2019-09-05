f1aeca0287fbed4d1bf4f95d616eb8340243a7cd
Alexander Galazin <alexander.galazin@arm.com>
2019-09-05 11:12:31 +0200

    Uncoment SPV_ENV_VULKAN_1_2 case
    Components: Vulkan
    
    Change-Id: Ie8305971f82c355266021a6487893ebf291a6103

--- external/vulkancts/framework/vulkan/vkSpirVAsm.cpp
+++ external/vulkancts/framework/vulkan/vkSpirVAsm.cpp
@@ -49,7 +49,7 @@ static spv_target_env getSpirvToolsEnvForValidatorOptions(SpirvValidatorOptions 
 	{
 		case VK_MAKE_VERSION(1, 0, 0): return SPV_ENV_VULKAN_1_0;
 		case VK_MAKE_VERSION(1, 1, 0): return allow_1_4 ? SPV_ENV_VULKAN_1_1_SPIRV_1_4 : SPV_ENV_VULKAN_1_1;
-		//case VK_MAKE_VERSION(1, 2, 0): return SPV_ENV_VULKAN_1_2; // TODO: uncommment when SPV_ENV_VULKAN_1_2 is available
+		case VK_MAKE_VERSION(1, 2, 0): return SPV_ENV_VULKAN_1_2;
 		default:
 			break;
 	}
