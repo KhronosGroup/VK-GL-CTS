a94173b6e0245f22263758c438ba8ed63f1ca575
Alexander Galazin <alexander.galazin@arm.com>
2019-09-06 14:25:45 +0200

    Whitelist Vulkan CTS 1.2.0.0
    Affects: dEQP-VK.api.driver_properties.properties
    
    Components: Vulkan
    
    Change-Id: I6f3f4d2dd96d2c8540187cb55ffc888db3bb647e

--- external/vulkancts/modules/vulkan/api/vktApiDriverPropertiesTests.cpp
+++ external/vulkancts/modules/vulkan/api/vktApiDriverPropertiesTests.cpp
@@ -65,6 +65,7 @@ static const deUint32 knownDriverIds[] =
 
 static const VkConformanceVersionKHR knownConformanceVersions[] =
 {
+	makeConformanceVersion(1, 2, 0, 0),
 	makeConformanceVersion(1, 1, 5, 0),
 	makeConformanceVersion(1, 1, 4, 3),
 	makeConformanceVersion(1, 1, 4, 2),
