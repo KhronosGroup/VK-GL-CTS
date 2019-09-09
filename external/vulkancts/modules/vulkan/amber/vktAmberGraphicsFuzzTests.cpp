2b5f75c980d63c01261ce1433079d2efc82f3f9f
Paul Thomson <paulthomson@google.com>
2019-09-09 11:36:58 +0100

    dEQP-VK.graphicsfuzz.max-mix-conditional-discard
    Components: Vulkan
    
    New Tests:
    
    dEQP-VK.graphicsfuzz.max-mix-conditional-discard
    
    Change-Id: Ie9ada9c6e7a71a4ad09ad6f09f0ee8264e02d9c1

--- external/vulkancts/modules/vulkan/amber/vktAmberGraphicsFuzzTests.cpp
+++ external/vulkancts/modules/vulkan/amber/vktAmberGraphicsFuzzTests.cpp
@@ -62,6 +62,7 @@ void createAmberTests (tcu::TestCaseGroup* group)
 		{	"mat-array-deep-control-flow.amber",			"mat-array-deep-control-flow",			"A fragment shader that uses an array of matrices and has deep control flow"			},
 		{	"mat-array-distance.amber",						"mat-array-distance",					"A fragment shader that uses an array of matrices and distance"							},
 		{	"matrices-and-return-in-loop.amber",			"matrices-and-return-in-loop",			"A fragment shader with matrices and a return in a loop"								},
+		{	"max-mix-conditional-discard.amber",			"max-mix-conditional-discard",			"A fragment shader with an expression used in two discard guards"						},
 		{	"nested-ifs-and-return-in-for-loop.amber",		"nested-ifs-and-return-in-for-loop",	"A fragment shader with return in nest of ifs, inside loop"								},
 		{	"pow-vec4.amber",								"pow-vec4",								"A fragment shader that uses pow"														},
 		{	"return-in-loop-in-function.amber",				"return-in-loop-in-function",			"A fragment shader with early return from loop in function"								},
