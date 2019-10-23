d0ee43aaa980dab94ed247e467999864373868d8
Ricardo Garcia <rgarcia@igalia.com>
2019-10-23 11:46:15 +0200

    Add buffer reference comparison tests
    This commit adds tests to convert buffer references to both uint64_t and
    uvec2 and compare them against zero and against other references.
    
    In addition, new test variants also check cross conversions, i.e., when
    a reference is loaded as a uint64_t and later converted to uvec2 or vice
    versa.
    
    New tests:
    dEQP-VK.binding_model.buffer_device_address.*.convertchecku64.*
    dEQP-VK.binding_model.buffer_device_address.*.convertcheckuv2.*
    dEQP-VK.binding_model.buffer_device_address.*.crossconvertu2p.*
    dEQP-VK.binding_model.buffer_device_address.*.crossconvertp2u.*
    
    Components: Vulkan
    VK-GL-CTS issue: 1968
    
    Change-Id: I4ae5b66473953097a52f169e5b8c425647eedb2c

--- external/vulkancts/modules/vulkan/binding_model/vktBindingBufferDeviceAddressTests.cpp
+++ external/vulkancts/modules/vulkan/binding_model/vktBindingBufferDeviceAddressTests.cpp
@@ -93,8 +93,12 @@ typedef enum
 typedef enum
 {
 	CONVERT_NONE = 0,
-	CONVERT_UTOPTR,
+	CONVERT_UINT64,
 	CONVERT_UVEC2,
+	CONVERT_U64CMP,
+	CONVERT_UVEC2CMP,
+	CONVERT_UVEC2TOU64,
+	CONVERT_U64TOUVEC2,
 } Convert;
 
 struct CaseDef
@@ -190,9 +194,19 @@ void BufferAddressTestCase::checkSupport (Context& context) const
 	}
 #endif
 
-	if (m_data.convertUToPtr == CONVERT_UTOPTR && !context.getDeviceFeatures().shaderInt64)
+	const bool needsInt64	= (	m_data.convertUToPtr == CONVERT_UINT64		||
+								m_data.convertUToPtr == CONVERT_U64CMP		||
+								m_data.convertUToPtr == CONVERT_U64TOUVEC2	||
+								m_data.convertUToPtr == CONVERT_UVEC2TOU64	);
+
+	const bool needsKHR		= (	m_data.convertUToPtr == CONVERT_UVEC2		||
+								m_data.convertUToPtr == CONVERT_UVEC2CMP	||
+								m_data.convertUToPtr == CONVERT_U64TOUVEC2	||
+								m_data.convertUToPtr == CONVERT_UVEC2TOU64	);
+
+	if (needsInt64 && !context.getDeviceFeatures().shaderInt64)
 		TCU_THROW(NotSupportedError, "Int64 not supported");
-	if (m_data.convertUToPtr == CONVERT_UVEC2 && !context.isDeviceFunctionalitySupported("VK_KHR_buffer_device_address"))
+	if (needsKHR && !context.isDeviceFunctionalitySupported("VK_KHR_buffer_device_address"))
 		TCU_THROW(NotSupportedError, "VK_KHR_buffer_device_address not supported");
 }
 
@@ -201,9 +215,9 @@ void BufferAddressTestCase::checkBuffer (std::stringstream& checks, deUint32 buf
 	string newPrefix = prefix;
 	if (curDepth > 0)
 	{
-		if (m_data.convertUToPtr == CONVERT_UTOPTR)
+		if (m_data.convertUToPtr == CONVERT_UINT64 || m_data.convertUToPtr == CONVERT_UVEC2TOU64)
 			newPrefix = "T1(uint64_t(T1(" + newPrefix + ")))";
-		else if (m_data.convertUToPtr == CONVERT_UVEC2)
+		else if (m_data.convertUToPtr == CONVERT_UVEC2 || m_data.convertUToPtr == CONVERT_U64TOUVEC2)
 			newPrefix = "T1(uvec2(T1(" + newPrefix + ")))";
 	}
 
@@ -230,17 +244,72 @@ void BufferAddressTestCase::checkBuffer (std::stringstream& checks, deUint32 buf
 		checks << "   accum |= f.z - " << bufNum*3+9 << ";\n";
 	}
 
+	const std::string localPrefix = "l" + de::toString(bufNum);
+
+	if (m_data.convertUToPtr == CONVERT_U64CMP || m_data.convertUToPtr == CONVERT_UVEC2CMP)
+	{
+		const std::string type = ((m_data.convertUToPtr == CONVERT_U64CMP) ? "uint64_t" : "uvec2");
+
+		checks << "   " << type << " " << localPrefix << "c0 = " << type << "(" << newPrefix << ".c[0]);\n";
+		checks << "   " << type << " " << localPrefix << "c1 = " << type << "(" << newPrefix << ".c[pc.identity[1]]);\n";
+		checks << "   " << type << " " << localPrefix << "d  = " << type << "(" << newPrefix << ".d);\n";
+	}
+
 	if (curDepth != m_data.depth)
 	{
+		// Check non-null pointers and inequality among them.
+		if (m_data.convertUToPtr == CONVERT_U64CMP)
+		{
+			checks << "   if (" << localPrefix << "c0 == zero ||\n"
+				   << "       " << localPrefix << "c1 == zero ||\n"
+				   << "       " << localPrefix << "d  == zero ||\n"
+				   << "       " << localPrefix << "c0 == " << localPrefix << "c1 ||\n"
+				   << "       " << localPrefix << "c1 == " << localPrefix << "d  ||\n"
+				   << "       " << localPrefix << "c0 == " << localPrefix << "d  ) {\n"
+				   << "     accum |= 1;\n"
+				   << "   }\n";
+		}
+		else if (m_data.convertUToPtr == CONVERT_UVEC2CMP)
+		{
+			checks << "   if (all(equal(" << localPrefix << "c0, zero)) ||\n"
+				   << "       all(equal(" << localPrefix << "c1, zero)) ||\n"
+				   << "       all(equal(" << localPrefix << "d , zero)) ||\n"
+				   << "       all(equal(" << localPrefix << "c0, " << localPrefix << "c1)) ||\n"
+				   << "       all(equal(" << localPrefix << "c1, " << localPrefix << "d )) ||\n"
+				   << "       all(equal(" << localPrefix << "c0, " << localPrefix << "d )) ) {\n"
+				   << "     accum |= 1;\n"
+				   << "   }\n";
+		}
+
 		checkBuffer(checks, bufNum*3+1, curDepth+1, newPrefix + ".c[0]");
 		checkBuffer(checks, bufNum*3+2, curDepth+1, newPrefix + ".c[pc.identity[1]]");
 		checkBuffer(checks, bufNum*3+3, curDepth+1, newPrefix + ".d");
 	}
+	else
+	{
+		// Check null pointers nonexplicitly.
+		if (m_data.convertUToPtr == CONVERT_U64CMP)
+		{
+			checks << "   if (!(" << localPrefix << "c0 == " << localPrefix << "c1 &&\n"
+				   << "         " << localPrefix << "c1 == " << localPrefix << "d  &&\n"
+				   << "         " << localPrefix << "c0 == " << localPrefix << "d  )) {\n"
+				   << "     accum |= 1;\n"
+				   << "   }\n";
+		}
+		else if (m_data.convertUToPtr == CONVERT_UVEC2CMP)
+		{
+			checks << "   if (!(all(equal(" << localPrefix << "c0, " << localPrefix << "c1)) &&\n"
+				   << "         all(equal(" << localPrefix << "c1, " << localPrefix << "d )) &&\n"
+				   << "         all(equal(" << localPrefix << "c0, " << localPrefix << "d )) )) {\n"
+				   << "     accum |= 1;\n"
+				   << "   }\n";
+		}
+	}
 }
 
 void BufferAddressTestInstance::fillBuffer (const std::vector<deUint8 *>& cpuAddrs,
-										  const std::vector<deUint64>& gpuAddrs,
-										  deUint32 bufNum, deUint32 curDepth) const
+											const std::vector<deUint64>& gpuAddrs,
+											deUint32 bufNum, deUint32 curDepth) const
 {
 	deUint8 *buf = cpuAddrs[bufNum];
 
@@ -278,19 +347,45 @@ void BufferAddressTestInstance::fillBuffer (const std::vector<deUint8 *>& cpuAdd
 		fillBuffer(cpuAddrs, gpuAddrs, bufNum*3+2, curDepth+1);
 		fillBuffer(cpuAddrs, gpuAddrs, bufNum*3+3, curDepth+1);
 	}
+	else
+	{
+		// c
+		((deUint64 *)(buf+48))[0] = 0ull;
+		((deUint64 *)(buf+48))[cStride] = 0ull;
+		// d
+		((deUint64 *)(buf+80))[0] = 0ull;
+	}
 }
 
 
 void BufferAddressTestCase::initPrograms (SourceCollections& programCollection) const
 {
-	std::stringstream decls, checks;
+	std::stringstream decls, checks, localDecls;
 
 	std::string baseStorage = m_data.base == BASE_UBO ? "uniform" : "buffer";
 	std::string memberStorage = "buffer";
 
 	decls << "layout(r32ui, set = " << m_data.set << ", binding = 0) uniform uimage2D image0_0;\n";
 	decls << "layout(buffer_reference) " << memberStorage << " T1;\n";
-	std::string refType = m_data.convertUToPtr == CONVERT_UTOPTR ? "uint64_t" : m_data.convertUToPtr == CONVERT_UVEC2 ? "uvec2" : "T1";
+
+	std::string refType;
+	switch (m_data.convertUToPtr)
+	{
+	case CONVERT_UINT64:
+	case CONVERT_U64TOUVEC2:
+		refType = "uint64_t";
+		break;
+
+	case CONVERT_UVEC2:
+	case CONVERT_UVEC2TOU64:
+		refType = "uvec2";
+		break;
+
+	default:
+		refType = "T1";
+		break;
+	}
+
 	std::string layout = m_data.layout == LAYOUT_SCALAR ? "scalar" : "std140";
 	decls <<
 			"layout(set = " << m_data.set << ", binding = 1, " << layout << ") " << baseStorage << " T2 {\n"
@@ -311,6 +406,11 @@ void BufferAddressTestCase::initPrograms (SourceCollections& programCollection) 
 			"   layout(offset = 96, row_major) mat2 e; // tightly packed for scalar, 16 byte matrix stride for std140\n"
 			"};\n";
 
+	if (m_data.convertUToPtr == CONVERT_U64CMP)
+		localDecls << "  uint64_t zero = uint64_t(0);\n";
+	else if (m_data.convertUToPtr == CONVERT_UVEC2CMP)
+		localDecls << "  uvec2 zero = uvec2(0, 0);\n";
+
 	checkBuffer(checks, 0, 0, "x");
 
 	std::stringstream pushdecl;
@@ -319,6 +419,9 @@ void BufferAddressTestCase::initPrograms (SourceCollections& programCollection) 
 	vk::ShaderBuildOptions::Flags flags = vk::ShaderBuildOptions::Flags(0);
 	if (m_data.layout == LAYOUT_SCALAR)
 		flags = vk::ShaderBuildOptions::FLAG_ALLOW_SCALAR_OFFSETS;
+
+	// The conversion and comparison in uvec2 form test needs SPIR-V 1.5 for OpBitcast.
+	const vk::SpirvVersion spirvVersion = ((m_data.convertUToPtr == CONVERT_UVEC2CMP) ? vk::SPIRV_VERSION_1_5 : vk::SPIRV_VERSION_1_0);
 
 	switch (m_data.stage)
 	{
@@ -339,13 +442,14 @@ void BufferAddressTestCase::initPrograms (SourceCollections& programCollection) 
 				"{\n"
 				"  int accum = 0, temp;\n"
 				"  ivec3 f;\n"
+				<< localDecls.str()
 				<< checks.str() <<
 				"  uvec4 color = (accum != 0) ? uvec4(0,0,0,0) : uvec4(1,0,0,1);\n"
 				"  imageStore(image0_0, ivec2(gl_GlobalInvocationID.xy), color);\n"
 				"}\n";
 
 			programCollection.glslSources.add("test") << glu::ComputeSource(css.str())
-				<< vk::ShaderBuildOptions(programCollection.usedVulkanVersion, vk::SPIRV_VERSION_1_0, flags);
+				<< vk::ShaderBuildOptions(programCollection.usedVulkanVersion, spirvVersion, flags);
 			break;
 		}
 #if ENABLE_RAYTRACING
@@ -365,13 +469,14 @@ void BufferAddressTestCase::initPrograms (SourceCollections& programCollection) 
 				"{\n"
 				"  int accum = 0, temp;\n"
 				"  ivec3 f;\n"
+				<< localDecls.str()
 				<< checks.str() <<
 				"  uvec4 color = (accum != 0) ? uvec4(0,0,0,0) : uvec4(1,0,0,1);\n"
 				"  imageStore(image0_0, ivec2(gl_LaunchIDNV.xy), color);\n"
 				"}\n";
 
 			programCollection.glslSources.add("test") << glu::RaygenSource(css.str())
-				<< vk::ShaderBuildOptions(programCollection.usedVulkanVersion, vk::SPIRV_VERSION_1_0, flags);
+				<< vk::ShaderBuildOptions(programCollection.usedVulkanVersion, spirvVersion, flags);
 			break;
 		}
 #endif
@@ -390,6 +495,7 @@ void BufferAddressTestCase::initPrograms (SourceCollections& programCollection) 
 				"{\n"
 				"  int accum = 0, temp;\n"
 				"  ivec3 f;\n"
+				<< localDecls.str()
 				<< checks.str() <<
 				"  uvec4 color = (accum != 0) ? uvec4(0,0,0,0) : uvec4(1,0,0,1);\n"
 				"  imageStore(image0_0, ivec2(gl_VertexIndex % " << DIM << ", gl_VertexIndex / " << DIM << "), color);\n"
@@ -397,7 +503,7 @@ void BufferAddressTestCase::initPrograms (SourceCollections& programCollection) 
 				"}\n";
 
 			programCollection.glslSources.add("test") << glu::VertexSource(vss.str())
-				<< vk::ShaderBuildOptions(programCollection.usedVulkanVersion, vk::SPIRV_VERSION_1_0, flags);
+				<< vk::ShaderBuildOptions(programCollection.usedVulkanVersion, spirvVersion, flags);
 			break;
 		}
 	case STAGE_FRAGMENT:
@@ -426,13 +532,14 @@ void BufferAddressTestCase::initPrograms (SourceCollections& programCollection) 
 				"{\n"
 				"  int accum = 0, temp;\n"
 				"  ivec3 f;\n"
+				<< localDecls.str()
 				<< checks.str() <<
 				"  uvec4 color = (accum != 0) ? uvec4(0,0,0,0) : uvec4(1,0,0,1);\n"
 				"  imageStore(image0_0, ivec2(gl_FragCoord.x, gl_FragCoord.y), color);\n"
 				"}\n";
 
 			programCollection.glslSources.add("test") << glu::FragmentSource(fss.str())
-				<< vk::ShaderBuildOptions(programCollection.usedVulkanVersion, vk::SPIRV_VERSION_1_0, flags);
+				<< vk::ShaderBuildOptions(programCollection.usedVulkanVersion, spirvVersion, flags);
 			break;
 		}
 	}
@@ -1253,9 +1360,13 @@ tcu::TestCaseGroup*	createBufferDeviceAddressTests (tcu::TestContext& testCtx)
 
 	TestGroupCase cvtCases[] =
 	{
-		{ CONVERT_NONE,		"load",			"load reference"						},
-		{ CONVERT_UTOPTR,	"convert",		"load and convert reference"			},
-		{ CONVERT_UVEC2,	"convertuvec2",	"load and convert reference to uvec2"	},
+		{ CONVERT_NONE,			"load",				"load reference"										},
+		{ CONVERT_UINT64,		"convert",			"load and convert reference"							},
+		{ CONVERT_UVEC2,		"convertuvec2",		"load and convert reference to uvec2"					},
+		{ CONVERT_U64CMP,		"convertchecku64",	"load, convert and compare references as uint64_t"		},
+		{ CONVERT_UVEC2CMP,		"convertcheckuv2",	"load, convert and compare references as uvec2"			},
+		{ CONVERT_UVEC2TOU64,	"crossconvertu2p",	"load reference as uint64_t and convert it to uvec2"	},
+		{ CONVERT_U64TOUVEC2,	"crossconvertp2u",	"load reference as uvec2 and convert it to uint64_t"	},
 	};
 
 	TestGroupCase storeCases[] =
