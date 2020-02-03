7cb1c48bbc43974d8311bcdb4c7ad3244c071088
Alexander Galazin <alexander.galazin@arm.com>
2020-02-03 10:49:48 +0100

    Fix compiler warning
    Components: OpenGL
    
    Change-Id: Ie61463fcd7d5ad006e364a8b18505af0a23ebf73

--- framework/opengl/gluContextInfo.cpp
+++ framework/opengl/gluContextInfo.cpp
@@ -251,15 +251,15 @@ bool ContextInfo::isES3Compatible() const
 {
 	// Detect compatible GLES context by querying GL_MAJOR_VERSION.
 	// This query does not exist on GLES2 so succeeding query implies GLES3+ context.
-	bool isES3Compatible = false;
+	bool _isES3Compatible = false;
 	glw::GLint majorVersion = 0;
 	const glw::Functions& gl = m_context.getFunctions();
 	gl.getError();
 	gl.getIntegerv(GL_MAJOR_VERSION, &majorVersion);
 	if (gl.getError() == GL_NO_ERROR)
-		isES3Compatible = true;
+		_isES3Compatible = true;
 
-	return isES3Compatible;
+	return _isES3Compatible;
 }
 
 ContextInfo* ContextInfo::create (const RenderContext& context)
