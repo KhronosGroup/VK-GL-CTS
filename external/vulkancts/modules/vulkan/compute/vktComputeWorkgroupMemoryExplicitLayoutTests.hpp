44b93f8ac8a2c2d85b89ef65d0c6c15b66ffe232
Caio Marcelo de Oliveira Filho <caio.oliveira@intel.com>
2020-08-03 21:34:34 -0700

    Add VK_KHR_workgroup_memory_explicit_layout type aliasing tests
    Component: Vulkan
    VK-GL-CTS Issue: 2524
    New Tests: dEQP-VK.compute.workgroup_memory_explicit_layout.alias.*
    
    Change-Id: I17ebda4ca8fd750aa5e64f750220c91f8c789dbc

---
+++ external/vulkancts/modules/vulkan/compute/vktComputeWorkgroupMemoryExplicitLayoutTests.hpp
@@ -0,0 +1,40 @@
+#ifndef _VKTCOMPUTEWORKGROUPMEMORYEXPLICITLAYOUTTESTS_HPP
+#define _VKTCOMPUTEWORKGROUPMEMORYEXPLICITLAYOUTTESTS_HPP
+/*------------------------------------------------------------------------
+ * Vulkan Conformance Tests
+ * ------------------------
+ *
+ * Copyright (c) 2020 The Khronos Group Inc.
+ * Copyright (c) 2020 Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *//*!
+ * \file
+ * \brief VK_KHR_workgroup_memory_explict_layout tests
+ *//*--------------------------------------------------------------------*/
+
+#include "tcuDefs.hpp"
+#include "vktTestCase.hpp"
+
+namespace vkt
+{
+namespace compute
+{
+
+tcu::TestCaseGroup* createWorkgroupMemoryExplicitLayoutTests (tcu::TestContext& testCtx);
+
+} // compute
+} // vkt
+
+#endif // _VKTCOMPUTEWORKGROUPMEMORYEXPLICITLAYOUTTESTS_HPP
