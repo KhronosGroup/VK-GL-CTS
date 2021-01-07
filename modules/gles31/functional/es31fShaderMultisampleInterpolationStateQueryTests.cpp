3fdca2864be98c85a54f6defb006f2661edc8362
Alexander Galazin <alexander.galazin@arm.com>
2021-01-07 11:45:19 +0100

    Support GL 4.5 in the multisample interpolation states tests
    Affects:
    dEQP-GLES31.functional.state_query.multisample_interpolation.*
    dEQP-GL45.functional.state_query.multisample_interpolation.*
    
    Components: AOSP
    
    Change-Id: I671f88d6eef70370c8fb273e8e442a33a9923f4b

--- modules/gles31/functional/es31fShaderMultisampleInterpolationStateQueryTests.cpp
+++ modules/gles31/functional/es31fShaderMultisampleInterpolationStateQueryTests.cpp
@@ -78,9 +78,11 @@ InterpolationOffsetCase::~InterpolationOffsetCase (void)
 
 void InterpolationOffsetCase::init (void)
 {
-	const bool supportsES32 = glu::contextSupports(m_context.getRenderContext().getType(), glu::ApiType::es(3, 2));
+	auto		ctxType			= m_context.getRenderContext().getType();
+	const bool	isES32orGL45	= glu::contextSupports(ctxType, glu::ApiType::es(3, 2)) ||
+								  glu::contextSupports(ctxType, glu::ApiType::core(4, 5));
 
-	if (!supportsES32 && !m_context.getContextInfo().isExtensionSupported("GL_OES_shader_multisample_interpolation"))
+	if (!isES32orGL45 && !m_context.getContextInfo().isExtensionSupported("GL_OES_shader_multisample_interpolation"))
 		throw tcu::NotSupportedError("Test requires GL_OES_shader_multisample_interpolation extension");
 }
 
@@ -126,9 +128,11 @@ FragmentInterpolationOffsetBitsCase::~FragmentInterpolationOffsetBitsCase (void)
 
 void FragmentInterpolationOffsetBitsCase::init (void)
 {
-	const bool supportsES32 = glu::contextSupports(m_context.getRenderContext().getType(), glu::ApiType::es(3, 2));
+	auto		ctxType			= m_context.getRenderContext().getType();
+	const bool	isES32orGL45	= glu::contextSupports(ctxType, glu::ApiType::es(3, 2)) ||
+								  glu::contextSupports(ctxType, glu::ApiType::core(4, 5));
 
-	if (!supportsES32 && !m_context.getContextInfo().isExtensionSupported("GL_OES_shader_multisample_interpolation"))
+	if (!isES32orGL45 && !m_context.getContextInfo().isExtensionSupported("GL_OES_shader_multisample_interpolation"))
 		throw tcu::NotSupportedError("Test requires GL_OES_shader_multisample_interpolation extension");
 }
 
