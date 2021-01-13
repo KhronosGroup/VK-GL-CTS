KhronosGroup/VK-GL-CTS

commit 2e2e79bdf32203793bf0921d9302e97e623f2608

Issues detected by synchronization validation layer fix
This commit fixes certain synchronization issues detected by synchronization validation layer.

Component: Vulkan

Affects:
dEQP-VK.api.image_clearing.core.clear_color_image.1d.optimal.remaining_array_layers_twostep.r4g4_unorm_pack8
dEQP-VK.pipeline.timestamp.transfer_tests.transfer_stage_with_copy_buffer_method
dEQP-VK.pipeline.dynamic_offset.compute.single_set.uniform_buffer.numcmdbuffers_1.sameorder.numdescriptorsetbindings_2.numdynamicbindings_1.numnondynamicbindings_0
dEQP-VK.spirv_assembly.instruction.graphics.cross_stage.basic_type.flat
dEQP-VK.glsl.builtin_var.fragdepth.point_list_d32_sfloat_multisample_2
dEQP-VK.synchronization.op.single_queue.event.write_fill_buffer_read_copy_buffer.buffer_16384
dEQP-VK.compute.basic.copy_ssbo_to_image_small
dEQP-VK.image.store.with_format.1d.r32g32b32a32_sfloat
dEQP-VK.image.qualifiers.coherent.1d.r32f
dEQP-VK.image.image_size.1d.readonly_32
dEQP-VK.multiview.depth.3_6_12_9_6_12_9_3_6_12_9_3
dEQP-VK.pipeline.push_descriptor.graphics.binding0_numcalls1_input_attachment
dEQP-VK.pipeline.cache.graphics_tests.vertex_stage_fragment_stage
dEQP-VK.ycbcr.format.g8_b8_r8_3plane_420_unorm.vertex_linear_disjoint
dEQP-VK.imageless_framebuffer.depth_stencil_resolve
dEQP-VK.renderpass.suballocation.attachment_allocation.input_output.*

Public issue: 217

Change-Id: Idaed4cad0a15e84ed3803566eb2f3e9493c2f7a1

external/vulkancts/modules/vulkan/synchronization/vktSynchronizationOperation.cpp
@@ -509,7 +509,12 @@ public:
 		const DeviceInterface&	vk	= m_context.getDeviceInterface();
 
 		if (m_bufferOp == BUFFER_OP_FILL)
+		{
 			vk.cmdFillBuffer(cmdBuffer, m_resource.getBuffer().handle, m_resource.getBuffer().offset, m_resource.getBuffer().size, m_fillValue);
+
+			const VkBufferMemoryBarrier	barrier = makeBufferMemoryBarrier(VK_ACCESS_TRANSFER_WRITE_BIT, VK_ACCESS_TRANSFER_READ_BIT, m_resource.getBuffer().handle, 0u, m_resource.getBuffer().size);
+			vk.cmdPipelineBarrier(cmdBuffer, VK_PIPELINE_STAGE_TRANSFER_BIT, VK_PIPELINE_STAGE_TRANSFER_BIT, (VkDependencyFlags)0, 0u, DE_NULL, 1u, &barrier, 0u, DE_NULL);
+		}
 		else if (m_bufferOp == BUFFER_OP_UPDATE)
 			vk.cmdUpdateBuffer(cmdBuffer, m_resource.getBuffer().handle, m_resource.getBuffer().offset, m_resource.getBuffer().size, reinterpret_cast<deUint32*>(&m_data[0]));
 		else

external/vulkancts/framework/vulkan/vkObjUtil.cpp
@@ -392,7 +392,7 @@ Move<VkRenderPass> makeRenderPass (const DeviceInterface&				vk,
 	const bool								hasColor							= colorFormat != VK_FORMAT_UNDEFINED;
 	const bool								hasDepthStencil						= depthStencilFormat != VK_FORMAT_UNDEFINED;
 	const VkImageLayout						initialLayoutColor					= loadOperation == VK_ATTACHMENT_LOAD_OP_LOAD ? VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL : VK_IMAGE_LAYOUT_UNDEFINED;
-	const VkImageLayout						initialLayoutDepthStencil			= loadOperation == VK_ATTACHMENT_LOAD_OP_LOAD ? VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL : VK_IMAGE_LAYOUT_UNDEFINED;
+	const VkImageLayout						initialLayoutDepthStencil			= VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
 
 	const VkAttachmentDescription			colorAttachmentDescription			=
 	{

external/vulkancts/modules/vulkan/api/vktApiImageClearingTests.cpp
@@ -1387,8 +1387,24 @@ TestStatus ClearColorImageTestInstance::iterate (void)
 	for (std::size_t i = 0u; i < subresourceRanges.size(); ++i)
 	{
 		m_vkd.cmdClearColorImage(*m_commandBuffer, *m_image, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, &m_params.clearValue[i].color, 1, &subresourceRanges[i]);
+
+		pipelineImageBarrier(VK_PIPELINE_STAGE_TRANSFER_BIT,			// VkPipelineStageFlags		srcStageMask
+							 VK_PIPELINE_STAGE_TRANSFER_BIT,			// VkPipelineStageFlags		dstStageMask
+							 VK_ACCESS_TRANSFER_WRITE_BIT,				// VkAccessFlags			srcAccessMask
+							 VK_ACCESS_TRANSFER_WRITE_BIT,				// VkAccessFlags			dstAccessMask
+							 VK_IMAGE_LAYOUT_UNDEFINED,					// VkImageLayout			oldLayout;
+							 VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL);		// VkImageLayout			newLayout;
 		if (m_twoStep)
+		{
 			m_vkd.cmdClearColorImage(*m_commandBuffer, *m_image, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, &m_params.clearValue[i].color, 1, &steptwoRanges[i]);
+
+			pipelineImageBarrier(VK_PIPELINE_STAGE_TRANSFER_BIT,		// VkPipelineStageFlags		srcStageMask
+								 VK_PIPELINE_STAGE_TRANSFER_BIT,		// VkPipelineStageFlags		dstStageMask
+								 VK_ACCESS_TRANSFER_WRITE_BIT,			// VkAccessFlags			srcAccessMask
+								 VK_ACCESS_TRANSFER_WRITE_BIT,			// VkAccessFlags			dstAccessMask
+								 VK_IMAGE_LAYOUT_UNDEFINED,				// VkImageLayout			oldLayout;
+								 VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL);	// VkImageLayout			newLayout;
+		}
 	}
 
 	pipelineImageBarrier(VK_PIPELINE_STAGE_TRANSFER_BIT,				// VkPipelineStageFlags		srcStageMask

external/vulkancts/modules/vulkan/compute/vktComputeBasicComputeShaderTests.cpp
@@ -972,7 +972,7 @@ tcu::TestStatus CopySSBOToImageTestInstance::iterate (void)
 		const VkBufferMemoryBarrier inputBufferPostHostWriteBarrier = makeBufferMemoryBarrier(VK_ACCESS_HOST_WRITE_BIT, VK_ACCESS_SHADER_READ_BIT, *inputBuffer, 0ull, bufferSizeBytes);
 
 		const VkImageMemoryBarrier imageLayoutBarrier = makeImageMemoryBarrier(
-			0u, 0u,
+			0u, VK_ACCESS_SHADER_WRITE_BIT,
 			VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_GENERAL,
 			*image, subresourceRange);
 

external/vulkancts/modules/vulkan/descriptor_indexing/vktDescriptorSetsIndexingTests.cpp
@@ -1038,7 +1038,7 @@ void CommonDescriptorInstance::copyBuffersToImages					(IterateCommonVariables&	
 	DE_ASSERT(variables.descriptorsImages.size() == infoCount);
 	const VkPipelineStageFlagBits dstStageMask = (m_testParams.stageFlags & VK_SHADER_STAGE_COMPUTE_BIT)
 		? VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT
-		: VK_PIPELINE_STAGE_VERTEX_INPUT_BIT;
+		: VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT;
 	for (deUint32 infoIdx = 0; infoIdx < infoCount; ++infoIdx)
 	{
 		ut::recordCopyBufferToImage(

external/vulkancts/modules/vulkan/image/vktImageLoadStoreTests.cpp
@@ -920,7 +920,7 @@ void ImageStoreTestInstance::commandBeforeCompute (const VkCommandBuffer cmdBuff
 
 	const VkImageSubresourceRange fullImageSubresourceRange = makeImageSubresourceRange(VK_IMAGE_ASPECT_COLOR_BIT, 0u, 1u, 0u, m_texture.numLayers());
 	const VkImageMemoryBarrier setImageLayoutBarrier = makeImageMemoryBarrier(
-		0u, 0u,
+		0u, VK_ACCESS_SHADER_WRITE_BIT,
 		VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_GENERAL,
 		m_image->get(), fullImageSubresourceRange);
 

external/vulkancts/modules/vulkan/image/vktImageQualifiersTests.cpp
@@ -572,7 +572,7 @@ void MemoryQualifierInstanceImage::commandsBeforeCompute (const VkCommandBuffer 
 
 	const VkImageMemoryBarrier imageLayoutBarrier
 		= makeImageMemoryBarrier(0u,
-								 VK_ACCESS_SHADER_READ_BIT,
+								 VK_ACCESS_SHADER_WRITE_BIT,
 								 VK_IMAGE_LAYOUT_UNDEFINED,
 								 VK_IMAGE_LAYOUT_GENERAL,
 								 m_image->get(),

external/vulkancts/modules/vulkan/image/vktImageSizeTests.cpp
@@ -411,7 +411,7 @@ void ImageSizeTestInstance::commandBeforeCompute (const VkCommandBuffer cmdBuffe
 
 	const VkImageSubresourceRange subresourceRange = makeImageSubresourceRange(VK_IMAGE_ASPECT_COLOR_BIT, 0u, 1u, 0u, m_texture.numLayers());
 	const VkImageMemoryBarrier barrierSetImageLayout = makeImageMemoryBarrier(
-		0u, 0u,
+		0u, VK_ACCESS_SHADER_READ_BIT,
 		VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_GENERAL,
 		m_image->get(), subresourceRange);
 

commit d2d3f54de446e22d0fa0e5d5cce32606a1c2e7c6

Move image to optimal layout before using it in renderpass in synchronization tests
This fixes 'write_clear_attachments' operation, where newly created image, while
still being in UNDEFINED layout was used in renderpass, that starts from
DEPTH_STENCIL_OPTIMAL layout.

This fixes regression introduced in 2e2e79bd.

Components: Vulkan

VK-GL-CTS Issue: 2709

Affects: dEQP-VK.synchronization.*write_clear_attachments*

Change-Id: I2e2f587ce5fbe928b6146f349cd37a645661645f

external/vulkancts/modules/vulkan/synchronization/vktSynchronizationOperation.cpp
@@ -3738,6 +3738,23 @@ public:
 	void recordCommands (const VkCommandBuffer cmdBuffer)
 	{
 		const DeviceInterface&		vk						= m_context.getDeviceInterface();
+		if ((m_resource.getImage().subresourceRange.aspectMask & (VK_IMAGE_ASPECT_DEPTH_BIT | VK_IMAGE_ASPECT_STENCIL_BIT)) != 0)
+		{
+			const VkImageMemoryBarrier imageBarrier =
+			{
+				VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER,				// sType
+				DE_NULL,											// pNext
+				0u,													// srcAccessMask
+				VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT,		// dstAccessMask
+				VK_IMAGE_LAYOUT_UNDEFINED,							// oldLayout
+				VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL,	// newLayout
+				VK_QUEUE_FAMILY_IGNORED,							// srcQueueFamilyIndex
+				VK_QUEUE_FAMILY_IGNORED,							// dstQueueFamilyIndex
+				m_resource.getImage().handle,						// image
+				m_resource.getImage().subresourceRange				// subresourceRange
+			};
+			vk.cmdPipelineBarrier(cmdBuffer, VK_PIPELINE_STAGE_HOST_BIT, VK_PIPELINE_STAGE_EARLY_FRAGMENT_TESTS_BIT | VK_PIPELINE_STAGE_LATE_FRAGMENT_TESTS_BIT, 0u, 0u, DE_NULL, 0u, DE_NULL, 1u, &imageBarrier);
+		}
 		beginRenderPass(vk, cmdBuffer, *m_renderPass, *m_frameBuffer, makeRect2D(0 ,0, m_resource.getImage().extent.width, m_resource.getImage().extent.height), m_clearValue);
 
 		const VkClearAttachment	clearAttachment	=