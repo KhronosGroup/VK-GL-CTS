fae4b78aafbb6b6b2d048ece0896cfb01221403b
Sławomir Cygan <slawomir.cygan@intel.com>
2021-02-10 14:40:11 +0100

    Fix extended dynamic state to not access out of bounds memory
    The test assumed const size (4096) of the storage backing the
    vectors of vertices. It looks like the vector of 'reversed' vertices
    was fully populated only when TRIANGLES topology was used,
    so the test may crash on other topologies.
    
    Components: Vulkan
    
    VK-GL-CTS Issue: 2786
    
    Affects: dEQP-VK.pipeline.extended_dynamic_state.*
    Change-Id: Ic56d3125335d2eef0c97028d855b27eae25da24f

--- external/vulkancts/modules/vulkan/pipeline/vktPipelineExtendedDynamicStateTests.cpp
+++ external/vulkancts/modules/vulkan/pipeline/vktPipelineExtendedDynamicStateTests.cpp
@@ -960,8 +960,8 @@ tcu::TestStatus ExtendedDynamicStateInstance::iterate (void)
 			fillWithPattern(*b, offset + dataSize, extraSize, &offScreenVertex, offScreenVertexSz);
 		}
 
-		copyAndFlush(vkd, device, vertBuffer, offset, vertices.data(), dataSize);
-		copyAndFlush(vkd, device, rvertBuffer, offset, reversedVertices.data(), dataSize);
+		copyAndFlush(vkd, device, vertBuffer, offset, vertices.data(), static_cast<size_t>(vertices.size() * sizeof(vertices[0])));
+		copyAndFlush(vkd, device, rvertBuffer, offset, reversedVertices.data(), static_cast<size_t>(reversedVertices.size() * sizeof(reversedVertices[0])));
 	}
 
 	// Descriptor set layout.
