KhronosGroup/VK-GL-CTS

commit 327ee5674a8855c18ba681d8b286042590af7026

Support GL 4.5 in various multisample tests
Affects:
dEQP-GLES31.functional.shaders.sample_variables.*
dEQP-GLES31.functional.sample_shading.*
dEQP-GLES31.functional.shaders.multisample_interpolation.*
dEQP-GL45.functional.shaders.sample_variables.*
dEQP-GL45.functional.sample_shading.*
dEQP-GL45.functional.shaders.multisample_interpolation.*

Components: AOSP

Change-Id: I4a7dce72401f32544ff6c4b93ae42733c9916c5c

framework/opengl/gluStateReset.cpp
@@ -1163,6 +1163,27 @@ void resetStateGLCore (const RenderContext& renderCtx, const ContextInfo& ctxInf
 		GLU_EXPECT_NO_ERROR(gl.getError(), "Buffer copy state reset failed");
 	}
 
+	// Images.
+	if (contextSupports(type, ApiType::core(4,4)))
+	{
+		int numImageUnits = 0;
+		gl.getIntegerv(GL_MAX_IMAGE_UNITS, &numImageUnits);
+
+		for (int ndx = 0; ndx < numImageUnits; ndx++)
+			gl.bindImageTexture(ndx, 0, 0, GL_FALSE, 0, GL_READ_ONLY, GL_R32UI);
+
+		GLU_EXPECT_NO_ERROR(gl.getError(), "Image state reset failed");
+	}
+
+	// Sample shading state.
+	if (contextSupports(type, ApiType::core(2,0)))
+	{
+		gl.minSampleShading(0.0f);
+		gl.disable(GL_SAMPLE_SHADING);
+
+		GLU_EXPECT_NO_ERROR(gl.getError(), "Sample shading state reset failed");
+	}
+
 	// Debug state
 	if (ctxInfo.isExtensionSupported("GL_KHR_debug"))
 	{

modules/gles31/functional/es31fMultisampleShaderRenderCase.cpp
@@ -26,6 +26,7 @@
 #include "tcuSurface.hpp"
 #include "tcuTestLog.hpp"
 #include "tcuStringTemplate.hpp"
+#include "gluContextInfo.hpp"
 #include "gluShaderProgram.hpp"
 #include "gluRenderContext.hpp"
 #include "gluPixelTransfer.hpp"
@@ -116,9 +117,7 @@ void MultisampleRenderCase::init (void)
 
 		case TARGET_TEXTURE:
 		{
-			deInt32 maxTextureSamples = 0;
-			gl.getInternalformativ(GL_TEXTURE_2D_MULTISAMPLE, GL_RGBA8, GL_SAMPLES, 1, &maxTextureSamples);
-
+			deInt32 maxTextureSamples = getMaxConformantSampleCount(GL_TEXTURE_2D_MULTISAMPLE, GL_RGBA8);
 			if (m_numRequestedSamples > maxTextureSamples)
 				throw tcu::NotSupportedError("Sample count not supported");
 			break;
@@ -126,9 +125,7 @@ void MultisampleRenderCase::init (void)
 
 		case TARGET_RENDERBUFFER:
 		{
-			deInt32 maxRboSamples = 0;
-			gl.getInternalformativ(GL_RENDERBUFFER, GL_RGBA8, GL_SAMPLES, 1, &maxRboSamples);
-
+			deInt32 maxRboSamples = getMaxConformantSampleCount(GL_RENDERBUFFER, GL_RGBA8);
 			if (m_numRequestedSamples > maxRboSamples)
 				throw tcu::NotSupportedError("Sample count not supported");
 			break;
@@ -781,6 +778,46 @@ void MultisampleRenderCase::setupRenderData (void)
 
 	gl.bindBuffer(GL_ARRAY_BUFFER, m_buffer);
 	gl.bufferData(GL_ARRAY_BUFFER, (int)sizeof(fullscreenQuad), fullscreenQuad, GL_STATIC_DRAW);
+}
+
+glw::GLint MultisampleRenderCase::getMaxConformantSampleCount(glw::GLenum target, glw::GLenum internalFormat)
+{
+	deInt32					maxTextureSamples	= 0;
+	const glw::Functions&	gl					= m_context.getRenderContext().getFunctions();
+
+	if (m_context.getContextInfo().isExtensionSupported("GL_NV_internalformat_sample_query"))
+	{
+		glw::GLint gl_sample_counts = 0;
+		gl.getInternalformativ(target, internalFormat, GL_NUM_SAMPLE_COUNTS, 1, &gl_sample_counts);
+		GLU_EXPECT_NO_ERROR(gl.getError(), "glGetInternalformativ() failed for GL_NUM_SAMPLE_COUNTS pname");
+
+		/* Check and return the first conformant sample count */
+		glw::GLint* gl_supported_samples = new glw::GLint[gl_sample_counts];
+		if (gl_supported_samples)
+		{
+			gl.getInternalformativ(target, internalFormat, GL_SAMPLES, gl_sample_counts, gl_supported_samples);
+
+			for (glw::GLint i = 0; i < gl_sample_counts; i++)
+			{
+				glw::GLint isConformant = 0;
+				gl.getInternalformatSampleivNV(target, internalFormat, gl_supported_samples[i], GL_CONFORMANT_NV, 1,
+					&isConformant);
+				GLU_EXPECT_NO_ERROR(gl.getError(), "glGetInternalformatSampleivNV() call(s) failed");
+
+				if (isConformant && gl_supported_samples[i] > maxTextureSamples)
+				{
+					maxTextureSamples = gl_supported_samples[i];
+				}
+			}
+			delete[] gl_supported_samples;
+		}
+	}
+	else
+	{
+		gl.getInternalformativ(target, internalFormat, GL_SAMPLES, 1, &maxTextureSamples);
+	}
+
+	return maxTextureSamples;
 }
 
 } // MultisampleShaderRenderUtil

modules/gles31/functional/es31fMultisampleShaderRenderCase.hpp
@@ -97,6 +97,8 @@ private:
 	virtual bool		verifySampleBuffers				(const std::vector<tcu::Surface>& resultBuffers);
 	virtual void		setupRenderData					(void);
 
+	glw::GLint			getMaxConformantSampleCount		(glw::GLenum target, glw::GLenum internalFormat);
+
 protected:
 	struct Attrib
 	{

modules/gles31/functional/es31fSampleShadingTests.cpp
@@ -47,6 +47,14 @@ namespace Functional
 namespace
 {
 
+static bool checkSupport(Context& ctx)
+{
+	auto contextType = ctx.getRenderContext().getType();
+	return contextSupports(contextType, glu::ApiType::es(3, 2)) ||
+		   contextSupports(contextType, glu::ApiType::core(4, 5)) ||
+		   ctx.getContextInfo().isExtensionSupported("GL_OES_sample_shading");
+}
+
 using namespace gls::StateQueryUtil;
 
 class SampleShadingStateCase : public TestCase
@@ -69,7 +77,7 @@ SampleShadingStateCase::SampleShadingStateCase (Context& ctx, const char* name, 
 
 void SampleShadingStateCase::init (void)
 {
-	if (!contextSupports(m_context.getRenderContext().getType(), glu::ApiType::es(3, 2)) && !m_context.getContextInfo().isExtensionSupported("GL_OES_sample_shading"))
+	if (!checkSupport(m_context))
 		throw tcu::NotSupportedError("Test requires GL_OES_sample_shading extension or a context version 3.2 or higher.");
 }
 
@@ -120,7 +128,7 @@ MinSampleShadingValueCase::MinSampleShadingValueCase (Context& ctx, const char* 
 
 void MinSampleShadingValueCase::init (void)
 {
-	if (!contextSupports(m_context.getRenderContext().getType(), glu::ApiType::es(3, 2)) && !m_context.getContextInfo().isExtensionSupported("GL_OES_sample_shading"))
+	if (!checkSupport(m_context))
 		throw tcu::NotSupportedError("Test requires GL_OES_sample_shading extension or a context version 3.2 or higher.");
 }
 
@@ -187,7 +195,7 @@ MinSampleShadingValueClampingCase::MinSampleShadingValueClampingCase (Context& c
 
 void MinSampleShadingValueClampingCase::init (void)
 {
-	if (!contextSupports(m_context.getRenderContext().getType(), glu::ApiType::es(3, 2)) && !m_context.getContextInfo().isExtensionSupported("GL_OES_sample_shading"))
+	if (!checkSupport(m_context))
 		throw tcu::NotSupportedError("Test requires GL_OES_sample_shading extension or a context version 3.2 or higher.");
 }
 
@@ -274,8 +282,7 @@ SampleShadingRenderingCase::~SampleShadingRenderingCase (void)
 void SampleShadingRenderingCase::init (void)
 {
 	// requirements
-
-	if (!contextSupports(m_context.getRenderContext().getType(), glu::ApiType::es(3, 2)) && !m_context.getContextInfo().isExtensionSupported("GL_OES_sample_shading"))
+	if (!checkSupport(m_context))
 		throw tcu::NotSupportedError("Test requires GL_OES_sample_shading extension or a context version 3.2 or higher.");
 	if (m_renderTarget == TARGET_DEFAULT && m_context.getRenderTarget().getNumSamples() <= 1)
 		throw tcu::NotSupportedError("Multisampled default framebuffer required");

modules/gles31/functional/es31fSampleVariableTests.cpp
@@ -276,8 +276,9 @@ MultisampleRenderCase::~MultisampleRenderCase (void)
 
 void MultisampleRenderCase::init (void)
 {
-	const bool	supportsES32 = glu::contextSupports(m_context.getRenderContext().getType(), glu::ApiType::es(3, 2));
-	if (!supportsES32 && !m_context.getContextInfo().isExtensionSupported("GL_OES_sample_variables"))
+	const bool supportsES32orGL45 = glu::contextSupports(m_context.getRenderContext().getType(), glu::ApiType::es(3, 2)) ||
+		contextSupports(m_context.getRenderContext().getType(), glu::ApiType::core(4, 5));
+	if (!supportsES32orGL45 && !m_context.getContextInfo().isExtensionSupported("GL_OES_sample_variables"))
 		TCU_THROW(NotSupportedError, "Test requires GL_OES_sample_variables extension or a context version 3.2 or higher.");
 
 	MultisampleShaderRenderUtil::MultisampleRenderCase::init();
@@ -920,9 +921,12 @@ SamplePosCorrectnessCase::~SamplePosCorrectnessCase (void)
 
 void SamplePosCorrectnessCase::init (void)
 {
-	const bool supportsES32 = glu::contextSupports(m_context.getRenderContext().getType(), glu::ApiType::es(3, 2));
+	auto		ctxType			= m_context.getRenderContext().getType();
+	const bool	isES32orGL45	= glu::contextSupports(ctxType, glu::ApiType::es(3, 2)) ||
+								  glu::contextSupports(ctxType, glu::ApiType::core(4, 5));
+
 	// requirements: per-invocation interpolation required
-	if (!supportsES32 && !m_context.getContextInfo().isExtensionSupported("GL_OES_shader_multisample_interpolation") &&
+	if (!isES32orGL45 && !m_context.getContextInfo().isExtensionSupported("GL_OES_shader_multisample_interpolation") &&
 		!m_context.getContextInfo().isExtensionSupported("GL_OES_sample_shading"))
 		TCU_THROW(NotSupportedError, "Test requires GL_OES_shader_multisample_interpolation or GL_OES_sample_shading extension or a context version 3.2 or higher.");
 
@@ -971,11 +975,14 @@ std::string SamplePosCorrectnessCase::genVertexSource (int numTargetSamples) con
 {
 	DE_UNREF(numTargetSamples);
 
+	auto		ctxType	= m_context.getRenderContext().getType();
+	const bool	isES32	= glu::contextSupports(ctxType, glu::ApiType::es(3, 2));
+	const bool	isGL45	= glu::contextSupports(ctxType, glu::ApiType::core(4, 5));
+
 	std::ostringstream	buf;
-	const bool			supportsES32	= glu::contextSupports(m_context.getRenderContext().getType(), glu::ApiType::es(3, 2));
 	map<string, string>	args;
-	args["GLSL_VERSION_DECL"]	= supportsES32 ? getGLSLVersionDeclaration(glu::GLSL_VERSION_320_ES) : getGLSLVersionDeclaration(glu::GLSL_VERSION_310_ES);
-	args["GLSL_EXTENSION"]		= supportsES32 ? "" : m_useSampleQualifier ? "#extension GL_OES_shader_multisample_interpolation : require" : "";
+	args["GLSL_VERSION_DECL"]	= isES32 ? getGLSLVersionDeclaration(glu::GLSL_VERSION_320_ES) : getGLSLVersionDeclaration(glu::GLSL_VERSION_310_ES);
+	args["GLSL_EXTENSION"]		= (isES32 || isGL45) ? "" : m_useSampleQualifier ? "#extension GL_OES_shader_multisample_interpolation : require" : "";
 
 	buf <<	"${GLSL_VERSION_DECL}\n"
 			"${GLSL_EXTENSION}\n"
@@ -1078,9 +1085,10 @@ SampleMaskBaseCase::~SampleMaskBaseCase (void)
 
 void SampleMaskBaseCase::init (void)
 {
-	const bool supportsES32 = glu::contextSupports(m_context.getRenderContext().getType(), glu::ApiType::es(3, 2));
+	const bool supportsES32orGL45 = glu::contextSupports(m_context.getRenderContext().getType(), glu::ApiType::es(3, 2)) ||
+		contextSupports(m_context.getRenderContext().getType(), glu::ApiType::core(4, 5));
 	// required extra extension
-	if (m_runMode == RUN_PER_TWO_SAMPLES && !supportsES32 && !m_context.getContextInfo().isExtensionSupported("GL_OES_sample_shading"))
+	if (m_runMode == RUN_PER_TWO_SAMPLES && !supportsES32orGL45 && !m_context.getContextInfo().isExtensionSupported("GL_OES_sample_shading"))
 			TCU_THROW(NotSupportedError, "Test requires GL_OES_sample_shading extension or a context version 3.2 or higher.");
 
 	MultisampleRenderCase::init();
@@ -1208,10 +1216,11 @@ std::string SampleMaskCase::genFragmentSource (int numTargetSamples) const
 
 	const bool			multisampleTarget	= (m_numRequestedSamples > 0) || (m_renderTarget == TARGET_DEFAULT && m_context.getRenderTarget().getNumSamples() > 1);
 	std::ostringstream	buf;
-	const bool			supportsES32		= glu::contextSupports(m_context.getRenderContext().getType(), glu::ApiType::es(3, 2));
+	const bool			supportsES32orGL45	= glu::contextSupports(m_context.getRenderContext().getType(), glu::ApiType::es(3, 2)) ||
+							contextSupports(m_context.getRenderContext().getType(), glu::ApiType::core(4, 5));
 	map<string, string>	args;
-	args["GLSL_VERSION_DECL"]				= supportsES32 ? getGLSLVersionDeclaration(glu::GLSL_VERSION_320_ES) : getGLSLVersionDeclaration(glu::GLSL_VERSION_310_ES);
-	args["GLSL_EXTENSION"]					= supportsES32 ? "" : "#extension GL_OES_sample_variables : require";
+	args["GLSL_VERSION_DECL"]				= supportsES32orGL45 ? getGLSLVersionDeclaration(glu::GLSL_VERSION_320_ES) : getGLSLVersionDeclaration(glu::GLSL_VERSION_310_ES);
+	args["GLSL_EXTENSION"]					= supportsES32orGL45 ? "" : "#extension GL_OES_sample_variables : require";
 
 	// test supports only one sample mask word
 	if (numTargetSamples > 32)
@@ -1367,10 +1376,11 @@ std::string SampleMaskCountCase::genFragmentSource (int numTargetSamples) const
 	DE_ASSERT(numTargetSamples != 0);
 
 	std::ostringstream	buf;
-	const bool			supportsES32	= glu::contextSupports(m_context.getRenderContext().getType(), glu::ApiType::es(3, 2));
+	const bool			supportsES32orGL45	= glu::contextSupports(m_context.getRenderContext().getType(), glu::ApiType::es(3, 2)) ||
+							contextSupports(m_context.getRenderContext().getType(), glu::ApiType::core(4, 5));
 	map<string, string>	args;
-	args["GLSL_VERSION_DECL"]	= supportsES32 ? getGLSLVersionDeclaration(glu::GLSL_VERSION_320_ES) : getGLSLVersionDeclaration(glu::GLSL_VERSION_310_ES);
-	args["GLSL_EXTENSION"]		= supportsES32 ? "" : "#extension GL_OES_sample_variables : require";
+	args["GLSL_VERSION_DECL"]	= supportsES32orGL45 ? getGLSLVersionDeclaration(glu::GLSL_VERSION_320_ES) : getGLSLVersionDeclaration(glu::GLSL_VERSION_310_ES);
+	args["GLSL_EXTENSION"]		= supportsES32orGL45 ? "" : "#extension GL_OES_sample_variables : require";
 
 	// test supports only one sample mask word
 	if (numTargetSamples > 32)
@@ -1468,10 +1478,11 @@ std::string SampleMaskUniqueCase::genFragmentSource (int numTargetSamples) const
 	DE_ASSERT(numTargetSamples != 0);
 
 	std::ostringstream	buf;
-	const bool			supportsES32	= glu::contextSupports(m_context.getRenderContext().getType(), glu::ApiType::es(3, 2));
+	const bool			supportsES32orGL45	= glu::contextSupports(m_context.getRenderContext().getType(), glu::ApiType::es(3, 2)) ||
+							contextSupports(m_context.getRenderContext().getType(), glu::ApiType::core(4, 5));
 	map<string, string>	args;
-	args["GLSL_VERSION_DECL"]	= supportsES32 ? getGLSLVersionDeclaration(glu::GLSL_VERSION_320_ES) : getGLSLVersionDeclaration(glu::GLSL_VERSION_310_ES);
-	args["GLSL_EXTENSION"]		= supportsES32 ? "" : "#extension GL_OES_sample_variables : require";
+	args["GLSL_VERSION_DECL"]	= supportsES32orGL45 ? getGLSLVersionDeclaration(glu::GLSL_VERSION_320_ES) : getGLSLVersionDeclaration(glu::GLSL_VERSION_310_ES);
+	args["GLSL_EXTENSION"]		= supportsES32orGL45 ? "" : "#extension GL_OES_sample_variables : require";
 
 	// test supports only one sample mask word
 	if (numTargetSamples > 32)
@@ -1711,10 +1722,11 @@ std::string SampleMaskUniqueSetCase::genFragmentSource (int numTargetSamples) co
 	DE_ASSERT(numTargetSamples != 0);
 
 	std::ostringstream	buf;
-	const bool			supportsES32	= glu::contextSupports(m_context.getRenderContext().getType(), glu::ApiType::es(3, 2));
+	const bool			supportsES32orGL45	= glu::contextSupports(m_context.getRenderContext().getType(), glu::ApiType::es(3, 2)) ||
+							contextSupports(m_context.getRenderContext().getType(), glu::ApiType::core(4, 5));
 	map<string, string>	args;
-	args["GLSL_VERSION_DECL"]	= supportsES32 ? getGLSLVersionDeclaration(glu::GLSL_VERSION_320_ES) : getGLSLVersionDeclaration(glu::GLSL_VERSION_310_ES);
-	args["GLSL_EXTENSION"]		= supportsES32 ? "" : "#extension GL_OES_sample_variables : require";
+	args["GLSL_VERSION_DECL"]	= supportsES32orGL45 ? getGLSLVersionDeclaration(glu::GLSL_VERSION_320_ES) : getGLSLVersionDeclaration(glu::GLSL_VERSION_310_ES);
+	args["GLSL_EXTENSION"]		= supportsES32orGL45 ? "" : "#extension GL_OES_sample_variables : require";
 
 	// test supports only one sample mask word
 	if (numTargetSamples > 32)
@@ -2081,10 +2093,11 @@ std::string SampleMaskWriteCase::genFragmentSource (int numTargetSamples) const
 	DE_UNREF(numTargetSamples);
 
 	std::ostringstream	buf;
-	const bool			supportsES32	= glu::contextSupports(m_context.getRenderContext().getType(), glu::ApiType::es(3, 2));
+	const bool			supportsES32orGL45	= glu::contextSupports(m_context.getRenderContext().getType(), glu::ApiType::es(3, 2)) ||
+							contextSupports(m_context.getRenderContext().getType(), glu::ApiType::core(4, 5));
 	map<string, string>	args;
-	args["GLSL_VERSION_DECL"]	= supportsES32 ? getGLSLVersionDeclaration(glu::GLSL_VERSION_320_ES) : getGLSLVersionDeclaration(glu::GLSL_VERSION_310_ES);
-	args["GLSL_EXTENSION"]		= supportsES32 ? "" : "#extension GL_OES_sample_variables : require";
+	args["GLSL_VERSION_DECL"]	= supportsES32orGL45 ? getGLSLVersionDeclaration(glu::GLSL_VERSION_320_ES) : getGLSLVersionDeclaration(glu::GLSL_VERSION_310_ES);
+	args["GLSL_EXTENSION"]		= supportsES32orGL45 ? "" : "#extension GL_OES_sample_variables : require";
 
 	if (m_testMode == TEST_DISCARD)
 	{

modules/gles31/functional/es31fShaderMultisampleInterpolationTests.cpp
@@ -50,14 +50,22 @@ namespace
 
 static std::string specializeShader(const std::string& shaderSource, const glu::ContextType& contextType)
 {
-	const bool supportsES32 = glu::contextSupports(contextType, glu::ApiType::es(3, 2));
+	const bool	isES32orGL45	= glu::contextSupports(contextType, glu::ApiType::es(3, 2)) ||
+								  glu::contextSupports(contextType, glu::ApiType::core(4, 5));
 
 	std::map<std::string, std::string> args;
 	args["GLSL_VERSION_DECL"]							= glu::getGLSLVersionDeclaration(glu::getContextTypeGLSLVersion(contextType));
-	args["GLSL_EXT_SHADER_MULTISAMPLE_INTERPOLATION"]	= supportsES32 ? "" : "#extension GL_OES_shader_multisample_interpolation : require\n";
-	args["GLSL_EXT_SAMPLE_VARIABLES"]					= supportsES32 ? "" : "#extension GL_OES_sample_variables : require\n";
+	args["GLSL_EXT_SHADER_MULTISAMPLE_INTERPOLATION"]	= isES32orGL45 ? "" : "#extension GL_OES_shader_multisample_interpolation : require\n";
+	args["GLSL_EXT_SAMPLE_VARIABLES"]					= isES32orGL45 ? "" : "#extension GL_OES_sample_variables : require\n";
 
 	return tcu::StringTemplate(shaderSource).specialize(args);
+}
+
+static bool checkSupport(Context& ctx)
+{
+	auto ctxType = ctx.getRenderContext().getType();
+	return glu::contextSupports(ctxType, glu::ApiType::es(3, 2)) ||
+		   glu::contextSupports(ctxType, glu::ApiType::core(4, 5));
 }
 
 static bool verifyGreenImage (const tcu::Surface& image, tcu::TestLog& log)
@@ -123,7 +131,7 @@ MultisampleShadeCountRenderCase::~MultisampleShadeCountRenderCase (void)
 void MultisampleShadeCountRenderCase::init (void)
 {
 	// requirements
-	if (!m_context.getContextInfo().isExtensionSupported("GL_OES_shader_multisample_interpolation") && !glu::contextSupports(m_context.getRenderContext().getType(), glu::ApiType::es(3, 2)))
+	if (!checkSupport(m_context) && !m_context.getContextInfo().isExtensionSupported("GL_OES_shader_multisample_interpolation"))
 		TCU_THROW(NotSupportedError, "Test requires GL_OES_shader_multisample_interpolation extension");
 
 	MultisampleShaderRenderUtil::MultisampleRenderCase::init();
@@ -664,7 +672,7 @@ SingleSampleInterpolateAtSampleCase::~SingleSampleInterpolateAtSampleCase (void)
 void SingleSampleInterpolateAtSampleCase::init (void)
 {
 	// requirements
-	if (!m_context.getContextInfo().isExtensionSupported("GL_OES_shader_multisample_interpolation") && !glu::contextSupports(m_context.getRenderContext().getType(), glu::ApiType::es(3, 2)))
+	if (!checkSupport(m_context) && !m_context.getContextInfo().isExtensionSupported("GL_OES_shader_multisample_interpolation"))
 		TCU_THROW(NotSupportedError, "Test requires GL_OES_shader_multisample_interpolation extension");
 	if (m_renderTarget == TARGET_DEFAULT && m_context.getRenderTarget().getNumSamples() > 1)
 		TCU_THROW(NotSupportedError, "Non-multisample framebuffer required");
@@ -775,7 +783,7 @@ CentroidRenderCase::~CentroidRenderCase (void)
 void CentroidRenderCase::init (void)
 {
 	// requirements
-	if (!m_context.getContextInfo().isExtensionSupported("GL_OES_shader_multisample_interpolation") && !glu::contextSupports(m_context.getRenderContext().getType(), glu::ApiType::es(3, 2)))
+	if (!checkSupport(m_context) && !m_context.getContextInfo().isExtensionSupported("GL_OES_shader_multisample_interpolation"))
 		TCU_THROW(NotSupportedError, "Test requires GL_OES_shader_multisample_interpolation extension");
 
 	MultisampleShaderRenderUtil::MultisampleRenderCase::init();
@@ -954,9 +962,9 @@ InterpolateAtSampleIDCase::~InterpolateAtSampleIDCase (void)
 void InterpolateAtSampleIDCase::init (void)
 {
 	// requirements
-	if (!m_context.getContextInfo().isExtensionSupported("GL_OES_shader_multisample_interpolation") && !glu::contextSupports(m_context.getRenderContext().getType(), glu::ApiType::es(3, 2)))
+	if (!checkSupport(m_context) && !m_context.getContextInfo().isExtensionSupported("GL_OES_shader_multisample_interpolation"))
 		TCU_THROW(NotSupportedError, "Test requires GL_OES_shader_multisample_interpolation extension");
-	if (!m_context.getContextInfo().isExtensionSupported("GL_OES_sample_variables") && !glu::contextSupports(m_context.getRenderContext().getType(), glu::ApiType::es(3, 2)))
+	if (!checkSupport(m_context) && !m_context.getContextInfo().isExtensionSupported("GL_OES_sample_variables"))
 		TCU_THROW(NotSupportedError, "Test requires GL_OES_sample_variables extension");
 
 	// test purpose and expectations
@@ -1227,7 +1235,7 @@ InterpolateAtOffsetCase::~InterpolateAtOffsetCase (void)
 void InterpolateAtOffsetCase::init (void)
 {
 	// requirements
-	if (!m_context.getContextInfo().isExtensionSupported("GL_OES_shader_multisample_interpolation") && !glu::contextSupports(m_context.getRenderContext().getType(), glu::ApiType::es(3, 2)))
+	if (!checkSupport(m_context) && !m_context.getContextInfo().isExtensionSupported("GL_OES_shader_multisample_interpolation"))
 		TCU_THROW(NotSupportedError, "Test requires GL_OES_shader_multisample_interpolation extension");
 
 	// test purpose and expectations
@@ -1358,9 +1366,9 @@ InterpolateAtSamplePositionCase::~InterpolateAtSamplePositionCase (void)
 void InterpolateAtSamplePositionCase::init (void)
 {
 	// requirements
-	if (!m_context.getContextInfo().isExtensionSupported("GL_OES_shader_multisample_interpolation") && !glu::contextSupports(m_context.getRenderContext().getType(), glu::ApiType::es(3, 2)))
+	if (!checkSupport(m_context) && !m_context.getContextInfo().isExtensionSupported("GL_OES_shader_multisample_interpolation"))
 		TCU_THROW(NotSupportedError, "Test requires GL_OES_shader_multisample_interpolation extension");
-	if (!m_context.getContextInfo().isExtensionSupported("GL_OES_sample_variables") && !glu::contextSupports(m_context.getRenderContext().getType(), glu::ApiType::es(3, 2)))
+	if (!checkSupport(m_context) && !m_context.getContextInfo().isExtensionSupported("GL_OES_sample_variables"))
 		TCU_THROW(NotSupportedError, "Test requires GL_OES_sample_variables extension");
 
 	// test purpose and expectations
@@ -1478,8 +1486,19 @@ NegativeCompileInterpolationCase::NegativeCompileInterpolationCase (Context& con
 
 void NegativeCompileInterpolationCase::init (void)
 {
-	if (!m_context.getContextInfo().isExtensionSupported("GL_OES_shader_multisample_interpolation") && !glu::contextSupports(m_context.getRenderContext().getType(), glu::ApiType::es(3, 2)))
+	if (!checkSupport(m_context) && !m_context.getContextInfo().isExtensionSupported("GL_OES_shader_multisample_interpolation"))
 		TCU_THROW(NotSupportedError, "Test requires GL_OES_shader_multisample_interpolation extension");
+
+	if(!glu::isContextTypeES(m_context.getRenderContext().getType()))
+	{
+		if(m_caseType == CASE_VEC4_IDENTITY_SWIZZLE
+		   || m_caseType == CASE_VEC4_CROP_SWIZZLE
+		   || m_caseType == CASE_VEC4_MIXED_SWIZZLE
+		   || m_caseType == CASE_INTERPOLATE_IVEC4
+		   || m_caseType == CASE_INTERPOLATE_UVEC4
+		   || m_caseType == CASE_INTERPOLATE_STRUCT_MEMBER)
+			TCU_THROW(NotSupportedError, "Test requires a GLES context");
+	}
 
 	m_testCtx.getLog() << tcu::TestLog::Message << "Trying to compile illegal shader, expecting compile to fail." << tcu::TestLog::EndMessage;
 }

commit 6f94fbcdbae05d67040ae7d31799f4aa8033f966

reset minSampleShading only with OpenGL Core 4.0+
This fixes crashes seen with many tests when context state is
reset and gl.minSampleShading is NULL.

Affects: KHR-GL*

Components: OpenGL

VK-GL-CTS issue: 2827

Fixes: 327ee5674 ("Support GL 4.5 in various multisample tests")
Signed-off-by: Tapani Pälli <tapani.palli@intel.com>
Change-Id: I4b67a30264d4bdb6de8a5f7e811ae8bb8ee8a696

framework/opengl/gluStateReset.cpp
@@ -1176,7 +1176,7 @@ void resetStateGLCore (const RenderContext& renderCtx, const ContextInfo& ctxInf
 	}
 
 	// Sample shading state.
-	if (contextSupports(type, ApiType::core(2,0)))
+	if (contextSupports(type, ApiType::core(4,0)))
 	{
 		gl.minSampleShading(0.0f);
 		gl.disable(GL_SAMPLE_SHADING);