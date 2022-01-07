KhronosGroup/VK-GL-CTS

commit a590d9ab4cd2b0c708b6619c744c13311c32cde1

Add check for ES compatibility extensions with Desktop OpenGL
Affects:
   dEQP-GL45.functional.shaders.functions.es31.*
   dEQP-GL45.functional.shaders.arrays.es31.*
   dEQP-GL45.functional.shaders.implicit_conversions.es31.*
   dEQP-GL45.functional.shaders.uniform_block.es31.*

Components: Framework, OpenGL

VK-GL-CTS issue: 3399

Signed-off-by: Tapani Pälli <tapani.palli@intel.com>
Change-Id: Ife97fee3d2e5c433050142e9ba9e330bfe3cbde9

modules/glshared/glsShaderLibraryCase.cpp
@@ -728,12 +728,35 @@ ShaderLibraryCase::~ShaderLibraryCase (void)
 {
 }
 
+static inline void requireExtension(const glu::ContextInfo& info, const ShaderCaseSpecification& spec, const char *extension)
+{
+	if (!info.isExtensionSupported(extension))
+		TCU_THROW(NotSupportedError, (string(getGLSLVersionName(spec.targetVersion)) + " is not supported").c_str());
+}
+
 void ShaderLibraryCase::init (void)
 {
 	DE_ASSERT(isValid(m_spec));
 
-	if (!isGLSLVersionSupported(m_renderCtx.getType(), m_spec.targetVersion))
-		TCU_THROW(NotSupportedError, (string(getGLSLVersionName(m_spec.targetVersion)) + " is not supported").c_str());
+	// Check for ES compatibility extensions, e.g. if we are on desktop context but require GLSL ES
+	if (!isContextTypeES(m_renderCtx.getType()) && glslVersionIsES(m_spec.targetVersion)) {
+		switch (m_spec.targetVersion) {
+		case GLSL_VERSION_300_ES:
+			requireExtension(m_contextInfo, m_spec, "GL_ARB_ES3_compatibility");
+			break;
+		case GLSL_VERSION_310_ES:
+			requireExtension(m_contextInfo, m_spec, "GL_ARB_ES3_1_compatibility");
+			break;
+		case GLSL_VERSION_320_ES:
+			requireExtension(m_contextInfo, m_spec, "GL_ARB_ES3_2_compatibility");
+			break;
+		default:
+			DE_ASSERT(!"Unhandled version check");
+		}
+	} else {
+		if (!isGLSLVersionSupported(m_renderCtx.getType(), m_spec.targetVersion))
+			TCU_THROW(NotSupportedError, (string(getGLSLVersionName(m_spec.targetVersion)) + " is not supported").c_str());
+	}
 
 	checkImplementationLimits(m_spec.requiredCaps, m_contextInfo);
 

commit 140a4c5ed5c715a1d14fda22823e1d7d80985059

Fix compilation issues with ShaderLibraryCase assert
This assert was causing issues with some compilers/options.

Components: Framework, OpenGL

VK-GL-CTS issue: 3399

Fixes: a590d9ab4 ("Add check for ES compatibility extensions with Desktop OpenGL")
Signed-off-by: Tapani Pälli <tapani.palli@intel.com>
Change-Id: I070d82e7e9c0a4aafbda21cabd18a2d37b5b2cf0

modules/glshared/glsShaderLibraryCase.cpp
@@ -751,7 +751,7 @@ void ShaderLibraryCase::init (void)
 			requireExtension(m_contextInfo, m_spec, "GL_ARB_ES3_2_compatibility");
 			break;
 		default:
-			DE_ASSERT(!"Unhandled version check");
+			DE_ASSERT(false);
 		}
 	} else {
 		if (!isGLSLVersionSupported(m_renderCtx.getType(), m_spec.targetVersion))