873238b0285241903dc937151f51975e5d46445e
Matthew Netsch <quic_mnetsch@quicinc.com>
2022-01-14 11:09:02 -0500

    Update KC-CTS for MR40
    Relaxes tolerance for texture lod bias
    
    Components: Opengl
    VK-GL-CTS Issue: 2229
    
    Affects:
    GL40.gtf21.GL3Tests.texture_lod_bias.*
    
    Change-Id: I4de59e342bc180238902548519ab5b70a6f92909

--- external/fetch_kc_cts.py
+++ external/fetch_kc_cts.py
@@ -32,7 +32,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "..", "scripts"))
 from build.common import *
 
 EXTERNAL_DIR	= os.path.realpath(os.path.normpath(os.path.dirname(__file__)))
-SHA1 = "0eb3b7e9d717bda5b9ccc1e32f6dfdc4a12b8b2e"
+SHA1 = "a30aef1aef9af1e4d557e1f3c55b46a308b24631"
 
 PACKAGES = [
 	GitRepo(
