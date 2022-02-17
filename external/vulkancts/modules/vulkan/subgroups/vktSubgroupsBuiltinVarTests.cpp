cd0f2a5992566c8cc61841a49b78c12f8823c697
Ricardo Garcia <rgarcia@igalia.com>
2022-02-17 11:09:07 +0100

    Do not require full subgroups when requiring a work group size
    Most subgroup tests that require a particular work group size use the
    VK_PIPELINE_SHADER_STAGE_CREATE_REQUIRE_FULL_SUBGROUPS_BIT_EXT flag when
    creating the shader stage, but that in turn forces the local size in the
    X dimension to have certain values as required by the following VUID:
    
      VUID-VkPipelineShaderStageCreateInfo-pNext-02757
    
    There are specific tests to check full subgroups. The flag can be safely
    omitted and these other tests should still work.
    
    Affected tests:
    dEQP-VK.subgroups.*compute*
    
    Components: Vulkan
    VK-GL-CTS issue: 3520
    
    Change-Id: Ic20d07d3a875156f817066d040ddf5813aef311f

--- external/vulkancts/modules/vulkan/subgroups/vktSubgroupsBuiltinVarTests.cpp
+++ external/vulkancts/modules/vulkan/subgroups/vktSubgroupsBuiltinVarTests.cpp
@@ -1749,8 +1749,7 @@ TestStatus test (Context& context, const CaseDefinition caseDef)
 				// According to the spec, requiredSubgroupSize must be a power-of-two integer.
 				for (deUint32 size = subgroupSizeControlProperties.minSubgroupSize; size <= subgroupSizeControlProperties.maxSubgroupSize; size *= 2)
 				{
-					TestStatus result = subgroups::makeComputeTest(context, VK_FORMAT_R32_UINT, DE_NULL, 0, DE_NULL, checkComputeSubgroupSize,
-																	size, VK_PIPELINE_SHADER_STAGE_CREATE_REQUIRE_FULL_SUBGROUPS_BIT_EXT);
+					TestStatus result = subgroups::makeComputeTest(context, VK_FORMAT_R32_UINT, DE_NULL, 0, DE_NULL, checkComputeSubgroupSize, size);
 
 					if (result.getCode() != QP_TEST_RESULT_PASS)
 					{
@@ -1774,8 +1773,7 @@ TestStatus test (Context& context, const CaseDefinition caseDef)
 				// According to the spec, requiredSubgroupSize must be a power-of-two integer.
 				for (deUint32 size = subgroupSizeControlProperties.minSubgroupSize; size <= subgroupSizeControlProperties.maxSubgroupSize; size *= 2)
 				{
-					TestStatus result = subgroups::makeComputeTest(context, VK_FORMAT_R32_UINT, DE_NULL, 0, DE_NULL, checkComputeSubgroupInvocationID,
-																	size, VK_PIPELINE_SHADER_STAGE_CREATE_REQUIRE_FULL_SUBGROUPS_BIT_EXT);
+					TestStatus result = subgroups::makeComputeTest(context, VK_FORMAT_R32_UINT, DE_NULL, 0, DE_NULL, checkComputeSubgroupInvocationID, size);
 
 					if (result.getCode() != QP_TEST_RESULT_PASS)
 					{
@@ -1799,8 +1797,7 @@ TestStatus test (Context& context, const CaseDefinition caseDef)
 				// According to the spec, requiredSubgroupSize must be a power-of-two integer.
 				for (deUint32 size = subgroupSizeControlProperties.minSubgroupSize; size <= subgroupSizeControlProperties.maxSubgroupSize; size *= 2)
 				{
-					TestStatus result = subgroups::makeComputeTest(context, VK_FORMAT_R32_UINT, DE_NULL, 0, DE_NULL, checkComputeNumSubgroups,
-																	size, VK_PIPELINE_SHADER_STAGE_CREATE_REQUIRE_FULL_SUBGROUPS_BIT_EXT);
+					TestStatus result = subgroups::makeComputeTest(context, VK_FORMAT_R32_UINT, DE_NULL, 0, DE_NULL, checkComputeNumSubgroups, size);
 
 					if (result.getCode() != QP_TEST_RESULT_PASS)
 					{
@@ -1824,8 +1821,7 @@ TestStatus test (Context& context, const CaseDefinition caseDef)
 				// According to the spec, requiredSubgroupSize must be a power-of-two integer.
 				for (deUint32 size = subgroupSizeControlProperties.minSubgroupSize; size <= subgroupSizeControlProperties.maxSubgroupSize; size *= 2)
 				{
-					TestStatus result = subgroups::makeComputeTest(context, VK_FORMAT_R32_UINT, DE_NULL, 0, DE_NULL, checkComputeSubgroupID,
-																	size, VK_PIPELINE_SHADER_STAGE_CREATE_REQUIRE_FULL_SUBGROUPS_BIT_EXT);
+					TestStatus result = subgroups::makeComputeTest(context, VK_FORMAT_R32_UINT, DE_NULL, 0, DE_NULL, checkComputeSubgroupID, size);
 
 					if (result.getCode() != QP_TEST_RESULT_PASS)
 					{
