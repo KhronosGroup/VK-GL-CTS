bc75df5fb3388ed2b2ab2f1ffc4c896810ba7a35
Nanley Chery <nanley.g.chery@intel.com>
2022-03-16 13:40:46 -0700

    Fix incorrect layout transition in modifier tests
    The queue family ownership acquire operation specifies a layout
    transition from the pre-initialized layout. The image isn't in the
    pre-initialized layout however, it's in the transfer dst optimal layout.
    Align with the Vulkan spec, which says:
    
    	When performing a layout transition on an image subresource, the
    	old layout value must either equal the current layout of the
    	image subresource (at the time the transition executes), or else
    	be VK_IMAGE_LAYOUT_UNDEFINED (implying that the contents of the
    	image subresource need not be preserved).
    
    Components: Vulkan
    
    VK-GL-CTS issue: 3575
    
    Affects: dEQP-VK.drm_format_modifiers.export_import.*
    Change-Id: I76dbb54c699ed7d34e964de17f71ef01bc9e98a5

--- external/vulkancts/modules/vulkan/modifiers/vktModifiersTests.cpp
+++ external/vulkancts/modules/vulkan/modifiers/vktModifiersTests.cpp
@@ -620,7 +620,7 @@ bool exportImportMemoryExplicitModifiersCase (Context& context, const VkFormat f
 		DE_NULL,									// const void*				pNext;
 		VK_ACCESS_TRANSFER_WRITE_BIT,				// VkAccessFlags			srcAccessMask;
 		VK_ACCESS_TRANSFER_WRITE_BIT,				// VkAccessFlags			dstAccessMask;
-		VK_IMAGE_LAYOUT_PREINITIALIZED,		// VkImageLayout			oldLayout;
+		VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,			// VkImageLayout			oldLayout;
 		VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL,			// VkImageLayout			newLayout;
 		VK_QUEUE_FAMILY_FOREIGN_EXT,					// deUint32					srcQueueFamilyIndex;
 		context.getUniversalQueueFamilyIndex(),			// deUint32					dstQueueFamilyIndex;
