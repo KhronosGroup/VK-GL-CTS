77264b96f8eafcbf872d30b35f35b022cf5befd0
Lujin Wang <luwang@nvidia.com>
2022-06-14 13:29:01 -0700

    Add back InvalidApiVersion test for Vulkan SC
    Vulkan SC may only fail with VK_ERROR_INCOMPATIBLE_DRIVER if an incompatible
    variant is requested.
    
    Affects tests:
    dEQP-VK.api.device_init.create_instance_invalid_api_version.basic
    dEQP-VKSC.api.device_init.create_instance_invalid_api_version.basic
    
    Components: Vulkan VulkanSC
    
    VK-GL-CTS Issue: 3753
    
    Change-Id: I9064cd814b3870d9514c964ed0e5e3b1c94a4708

--- external/vulkancts/modules/vulkan/api/vktApiDeviceInitializationTests.cpp
+++ external/vulkancts/modules/vulkan/api/vktApiDeviceInitializationTests.cpp
@@ -212,7 +212,6 @@ tcu::TestStatus createInstanceTest (Context& context)
 	return tcu::TestStatus(resultCollector.getResult(), resultCollector.getMessage());
 }
 
-#ifndef CTS_USES_VULKANSC
 tcu::TestStatus createInstanceWithInvalidApiVersionTest (Context& context)
 {
 	tcu::TestLog&				log						= context.getTestContext().getLog();
@@ -230,7 +229,12 @@ tcu::TestStatus createInstanceWithInvalidApiVersionTest (Context& context)
 	vector<ApiVersion>			invalidApiVersions;
 
 	invalidApiVersions.push_back(ApiVersion(invalidApiVariant, invalidMajorVersion, apiVersion.minorNum, apiVersion.patchNum));
+	invalidApiVersions.push_back(ApiVersion(apiVersion.variantNum, invalidMajorVersion, apiVersion.minorNum, apiVersion.patchNum));
 	invalidApiVersions.push_back(ApiVersion(apiVersion.variantNum, apiVersion.majorNum, invalidMinorVersion, apiVersion.patchNum));
+#ifdef CTS_USES_VULKANSC
+	invalidApiVersions.push_back(ApiVersion(invalidApiVariant, apiVersion.majorNum, apiVersion.minorNum, apiVersion.patchNum));
+	invalidApiVersions.push_back(ApiVersion(0, apiVersion.majorNum, apiVersion.minorNum, apiVersion.patchNum));
+#endif // CTS_USES_VULKANSC
 
 	for (size_t apiVersionNdx = 0; apiVersionNdx < invalidApiVersions.size(); apiVersionNdx++)
 	{
@@ -265,6 +269,9 @@ tcu::TestStatus createInstanceWithInvalidApiVersionTest (Context& context)
 			UncheckedInstance	instance;
 			const VkResult		result		= createUncheckedInstance(context, &instanceCreateInfo, DE_NULL, &instance);
 
+#ifdef CTS_USES_VULKANSC
+			if (invalidApiVersions[apiVersionNdx].variantNum == apiVersion.variantNum)
+#else
 			if (apiVersion.majorNum == 1 && apiVersion.minorNum == 0)
 			{
 				if (result == VK_ERROR_INCOMPATIBLE_DRIVER)
@@ -276,15 +283,20 @@ tcu::TestStatus createInstanceWithInvalidApiVersionTest (Context& context)
 					resultCollector.fail("Fail, instance creation with invalid apiVersion is not rejected");
 			}
 			else if (apiVersion.majorNum == 1 && apiVersion.minorNum >= 1)
+#endif // CTS_USES_VULKANSC
 			{
 				if (result == VK_SUCCESS)
 				{
 					TCU_CHECK(static_cast<bool>(instance));
-					log << TestLog::Message << "Pass, instance creation with nonstandard apiVersion succeeds for Vulkan 1.1" << TestLog::EndMessage;
+					log << TestLog::Message << "Pass, instance creation with nonstandard apiVersion succeeds for "
+						<< ((apiVersion.variantNum == 0) ? "Vulkan 1.1" : "Vulkan SC when api variant is correct") << TestLog::EndMessage;
 				}
 				else if (result == VK_ERROR_INCOMPATIBLE_DRIVER)
 				{
-					resultCollector.fail("Fail, In Vulkan 1.1 instance creation must not return VK_ERROR_INCOMPATIBLE_DRIVER.");
+					std::ostringstream message;
+					message << "Fail, instance creation must not return VK_ERROR_INCOMPATIBLE_DRIVER for "
+						<< ((apiVersion.variantNum == 0) ? "Vulkan 1.1" : "Vulkan SC when api variant is correct");
+					resultCollector.fail(message.str().c_str());
 				}
 				else
 				{
@@ -293,12 +305,20 @@ tcu::TestStatus createInstanceWithInvalidApiVersionTest (Context& context)
 					resultCollector.fail(message.str().c_str());
 				}
 			}
+#ifdef CTS_USES_VULKANSC
+			else if (result == VK_ERROR_INCOMPATIBLE_DRIVER)
+			{
+				TCU_CHECK(!static_cast<bool>(instance));
+				log << TestLog::Message << "Pass, instance creation with invalid apiVersion is rejected" << TestLog::EndMessage;
+			}
+			else
+				resultCollector.fail("Fail, instance creation with invalid apiVersion is not rejected");
+#endif // CTS_USES_VULKANSC
 		}
 	}
 
 	return tcu::TestStatus(resultCollector.getResult(), resultCollector.getMessage());
 }
-#endif // CTS_USES_VULKANSC
 
 tcu::TestStatus createInstanceWithNullApplicationInfoTest (Context& context)
 {
@@ -2712,9 +2732,7 @@ tcu::TestCaseGroup* createDeviceInitializationTests (tcu::TestContext& testCtx)
 	de::MovePtr<tcu::TestCaseGroup>	deviceInitializationTests (new tcu::TestCaseGroup(testCtx, "device_init", "Device Initialization Tests"));
 
 	addFunctionCaseInNewSubgroup(testCtx, deviceInitializationTests.get(), "create_instance_name_version",					"", createInstanceTest);
-#ifndef CTS_USES_VULKANSC
 	addFunctionCaseInNewSubgroup(testCtx, deviceInitializationTests.get(), "create_instance_invalid_api_version",			"", createInstanceWithInvalidApiVersionTest);
-#endif // CTS_USES_VULKANSC
 	addFunctionCaseInNewSubgroup(testCtx, deviceInitializationTests.get(), "create_instance_null_appinfo",					"", createInstanceWithNullApplicationInfoTest);
 	addFunctionCaseInNewSubgroup(testCtx, deviceInitializationTests.get(), "create_instance_unsupported_extensions",		"", createInstanceWithUnsupportedExtensionsTest);
 	addFunctionCaseInNewSubgroup(testCtx, deviceInitializationTests.get(), "create_instance_extension_name_abuse",			"", createInstanceWithExtensionNameAbuseTest);
