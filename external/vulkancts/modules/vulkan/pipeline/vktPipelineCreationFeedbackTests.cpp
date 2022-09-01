6610462bb337fbd49dc294dd53109f318b5665a2
Marcin Zając <marcin.zajac@mobica.com>
2022-09-01 12:13:31 +0200

    pipelineStageCreationFeedbackCount = 0 case
    Coverage of VkPipelineCreationFeedbackCreateInfo
    configuration with per stage feedback count
    set to 0 which should cause per-stage information
    to not being emitted
    
    Components: Vulkan
    
    VK-GL-CTS issue: 3736
    
    New test:
    dEQP-VK.pipeline.*.creation_feedback.graphics_tests.vertex_stage_fragment_stage_no_cache_zero_out_feedback_cout
    
    Change-Id: Idf656c53bd09ec00bee70e6ff08ebd73a95aa7e0

--- external/vulkancts/modules/vulkan/pipeline/vktPipelineCreationFeedbackTests.cpp
+++ external/vulkancts/modules/vulkan/pipeline/vktPipelineCreationFeedbackTests.cpp
@@ -113,7 +113,8 @@ public:
 								CacheTestParam				(const PipelineConstructionType	pipelineConstructionType,
 															 const VkShaderStageFlags		shaders,
 															 deBool							noCache,
-															 deBool							delayedDestroy);
+															 deBool							delayedDestroy,
+															 deBool							zeroOutFeedbackCount = VK_FALSE);
 	virtual						~CacheTestParam				(void) = default;
 	virtual const std::string	generateTestName			(void)	const;
 	virtual const std::string	generateTestDescription		(void)	const;
@@ -121,19 +122,22 @@ public:
 	VkShaderStageFlags			getShaderFlags				(void)	const	{ return m_shaders; }
 	deBool						isCacheDisabled				(void)	const	{ return m_noCache; }
 	deBool						isDelayedDestroy			(void)	const	{ return m_delayedDestroy; }
+	deBool						isZeroOutFeedbackCount		(void)	const	{ return m_zeroOutFeedbackCount; }
 
 protected:
 	PipelineConstructionType	m_pipelineConstructionType;
 	VkShaderStageFlags			m_shaders;
 	bool						m_noCache;
 	bool						m_delayedDestroy;
+	bool						m_zeroOutFeedbackCount;
 };
 
-CacheTestParam::CacheTestParam (const PipelineConstructionType pipelineConstructionType, const VkShaderStageFlags shaders, deBool noCache, deBool delayedDestroy)
+CacheTestParam::CacheTestParam (const PipelineConstructionType pipelineConstructionType, const VkShaderStageFlags shaders, deBool noCache, deBool delayedDestroy, deBool zeroOutFeedbackCount)
 	: m_pipelineConstructionType	(pipelineConstructionType)
 	, m_shaders						(shaders)
 	, m_noCache						(noCache)
 	, m_delayedDestroy				(delayedDestroy)
+	, m_zeroOutFeedbackCount		(zeroOutFeedbackCount)
 {
 }
 
@@ -141,8 +145,9 @@ const std::string CacheTestParam::generateTestName (void) const
 {
 	std::string cacheString [] = { "", "_no_cache" };
 	std::string delayedDestroyString [] = { "", "_delayed_destroy" };
+	std::string zeroOutFeedbackCoutString [] = { "", "_zero_out_feedback_cout" };
 
-	return getShaderFlagStr(m_shaders, false) + cacheString[m_noCache ? 1 : 0] + delayedDestroyString[m_delayedDestroy ? 1 : 0];
+	return getShaderFlagStr(m_shaders, false) + cacheString[m_noCache ? 1 : 0] + delayedDestroyString[m_delayedDestroy ? 1 : 0] + zeroOutFeedbackCoutString[m_zeroOutFeedbackCount ? 1 : 0];
 }
 
 const std::string CacheTestParam::generateTestDescription (void) const
@@ -261,7 +266,8 @@ protected:
 														 VkShaderModule					fragShaderModule,
 														 VkPipelineCreationFeedbackEXT*	pipelineCreationFeedback,
 														 VkPipelineCreationFeedbackEXT*	pipelineStageCreationFeedbacks,
-														 VkPipeline						basePipelineHandle);
+														 VkPipeline						basePipelineHandle,
+														 VkBool32						zeroOutFeedbackCount);
 	virtual tcu::TestStatus verifyTestResult			(void);
 
 protected:
@@ -469,7 +475,8 @@ GraphicsCacheTestInstance::GraphicsCacheTestInstance (Context&				context,
 		preparePipelineWrapper(m_pipeline[ndx], vertShaderModule, *tescShaderModule, *teseShaderModule, *geomShaderModule, *fragShaderModule,
 							   &m_pipelineCreationFeedback[VK_MAX_PIPELINE_PARTS * ndx],
 							   &m_pipelineStageCreationFeedbacks[VK_MAX_SHADER_STAGES * ndx],
-							   ndx == PIPELINE_CACHE_NDX_DERIVATIVE ? m_pipeline[PIPELINE_CACHE_NDX_NO_CACHE].getPipeline() : DE_NULL);
+							   ndx == PIPELINE_CACHE_NDX_DERIVATIVE ? m_pipeline[PIPELINE_CACHE_NDX_NO_CACHE].getPipeline() : DE_NULL,
+							   param->isZeroOutFeedbackCount());
 
 		if (ndx != PIPELINE_CACHE_NDX_NO_CACHE)
 		{
@@ -498,7 +505,8 @@ void GraphicsCacheTestInstance::preparePipelineWrapper (GraphicsPipelineWrapper&
 														VkShaderModule					fragShaderModule,
 														VkPipelineCreationFeedbackEXT*	pipelineCreationFeedback,
 														VkPipelineCreationFeedbackEXT*	pipelineStageCreationFeedbacks,
-														VkPipeline						basePipelineHandle)
+														VkPipeline						basePipelineHandle,
+														VkBool32						zeroOutFeedbackCount)
 {
 	const VkVertexInputBindingDescription vertexInputBindingDescription
 	{
@@ -610,7 +618,7 @@ void GraphicsCacheTestInstance::preparePipelineWrapper (GraphicsPipelineWrapper&
 	deUint32 geometryStages = 1u + (geomShaderModule != DE_NULL) + (tescShaderModule != DE_NULL) + (teseShaderModule != DE_NULL);
 	if (m_param->getPipelineConstructionType() == PIPELINE_CONSTRUCTION_TYPE_MONOLITHIC)
 	{
-		pipelineCreationFeedbackCreateInfo[4].pipelineStageCreationFeedbackCount	= 1u + geometryStages;
+		pipelineCreationFeedbackCreateInfo[4].pipelineStageCreationFeedbackCount	= zeroOutFeedbackCount ? 0u : (1u + geometryStages);
 		pipelineCreationFeedbackCreateInfo[4].pPipelineStageCreationFeedbacks		= pipelineStageCreationFeedbacks;
 	}
 	else
@@ -670,6 +678,7 @@ tcu::TestStatus GraphicsCacheTestInstance::verifyTestResult (void)
 	bool			durationZeroWarning		= DE_FALSE;
 	bool			cachedPipelineWarning	= DE_FALSE;
 	bool			isMonolithic			= m_param->getPipelineConstructionType() == PIPELINE_CONSTRUCTION_TYPE_MONOLITHIC;
+	bool			isZeroOutFeedbackCout	= m_param->isZeroOutFeedbackCount();
 	deUint32		finalPipelineIndex		= deUint32(VK_MAX_PIPELINE_PARTS) - 1u;
 	deUint32		start					= isMonolithic ? finalPipelineIndex : 0u;
 	deUint32		step					= start + 1u;
@@ -684,7 +693,6 @@ tcu::TestStatus GraphicsCacheTestInstance::verifyTestResult (void)
 
 		std::ostringstream message;
 		message << caseString;
-
 		// Check first that the no cached pipeline was missed in the pipeline cache
 
 		// According to the spec:
@@ -760,6 +768,13 @@ tcu::TestStatus GraphicsCacheTestInstance::verifyTestResult (void)
 				// According to the spec:
 				// "An implementation should write pipeline creation feedback to pPipelineCreationFeedback and
 				//      may write pipeline stage creation feedback to pPipelineStageCreationFeedbacks."
+				if (m_pipelineStageCreationFeedbacks[index].flags & isZeroOutFeedbackCout)
+				{
+					std::ostringstream			errorMsg;
+					errorMsg << caseString << ": feedback indicates pipeline " << (shader + 1) << " shader stage feedback was generated despite setting feedback count to zero";
+					return tcu::TestStatus::fail(errorMsg.str());
+				}
+
 				if (!(m_pipelineStageCreationFeedbacks[index].flags & VK_PIPELINE_CREATION_FEEDBACK_VALID_BIT_EXT))
 				{
 					// According to the spec:
@@ -933,8 +948,9 @@ void ComputeCacheTestInstance::buildShader (deUint32 ndx)
 
 void ComputeCacheTestInstance::buildPipeline (const CacheTestParam*	param, deUint32 ndx)
 {
-	const DeviceInterface&	vk				 = m_context.getDeviceInterface();
-	const VkDevice			vkDevice		 = m_context.getDevice();
+	const DeviceInterface&	vk					 = m_context.getDeviceInterface();
+	const VkDevice			vkDevice			 = m_context.getDevice();
+	const VkBool32			zeroOutFeedbackCount = param->isZeroOutFeedbackCount();
 
 	deMemset(&m_pipelineCreationFeedback[ndx], 0, sizeof(VkPipelineCreationFeedbackEXT));
 	deMemset(&m_pipelineStageCreationFeedback[ndx], 0, sizeof(VkPipelineCreationFeedbackEXT));
@@ -944,7 +960,7 @@ void ComputeCacheTestInstance::buildPipeline (const CacheTestParam*	param, deUin
 		VK_STRUCTURE_TYPE_PIPELINE_CREATION_FEEDBACK_CREATE_INFO_EXT,	// VkStructureType					sType;
 		DE_NULL,														// const void *						pNext;
 		&m_pipelineCreationFeedback[ndx],								// VkPipelineCreationFeedbackEXT*	pPipelineCreationFeedback;
-		1u,																// deUint32							pipelineStageCreationFeedbackCount;
+		zeroOutFeedbackCount ? 0u : 1u,									// deUint32							pipelineStageCreationFeedbackCount;
 		&m_pipelineStageCreationFeedback[ndx]							// VkPipelineCreationFeedbackEXT*	pPipelineStageCreationFeedbacks;
 	};
 
@@ -1183,6 +1199,7 @@ tcu::TestCaseGroup* createCreationFeedbackTests (tcu::TestContext& testCtx, Pipe
 			{ pipelineConstructionType, vertGeomFragStages,	DE_FALSE, DE_FALSE },
 			{ pipelineConstructionType, vertTessFragStages,	DE_FALSE, DE_FALSE },
 			{ pipelineConstructionType, vertFragStages,		DE_TRUE,  DE_FALSE },
+			{ pipelineConstructionType, vertFragStages,		DE_TRUE,  DE_FALSE, DE_TRUE },
 			{ pipelineConstructionType, vertGeomFragStages,	DE_TRUE,  DE_FALSE },
 			{ pipelineConstructionType, vertTessFragStages,	DE_TRUE,  DE_FALSE },
 			{ pipelineConstructionType, vertFragStages,		DE_FALSE, DE_TRUE },
