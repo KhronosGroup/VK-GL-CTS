7ba7c7de8d3658c4bea670c1dde25e3f6c8566e1
ziga-lunarg <ziga@lunarg.com>
2022-09-24 23:00:25 +0200

    Fix samples in VkCoarseSampleLocationNV tests
    VkCoarseSampleLocationNV::sample must be less than the number of
    coverage samples in each pixel belongning to the fragment
    
    Components: Vulkan
    
    VK-GL-CTS issue: 3995
    
    Affected tests:
    dEQP-VK.dynamic_state.*.viewport_coarse_sample_order_nv.*
    
    Change-Id: Ia44906d0129d0af9374b44ca1f6a423b31285311

--- external/vulkancts/modules/vulkan/dynamic_state/vktDynamicStateComputeTests.cpp
+++ external/vulkancts/modules/vulkan/dynamic_state/vktDynamicStateComputeTests.cpp
@@ -394,7 +394,7 @@ void setCoarseSamplingOrder (const DeviceInterface* vkd, VkCommandBuffer cmdBuff
 		{
 			0u,	//	deUint32	pixelX;
 			1u,	//	deUint32	pixelY;
-			1u,	//	deUint32	sample;
+			0u,	//	deUint32	sample;
 		},
 	};
 	const VkCoarseSampleOrderCustomNV order =
