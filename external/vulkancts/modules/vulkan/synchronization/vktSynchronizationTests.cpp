c1ebeb5013f976a3dfafd532c28aa9112f90f32c
Piotr Byszewski <piotr.byszewski@mobica.com>
2022-12-21 17:00:46 +0100

    Fix missing global_priority_transition tests
    After recent vk video merge global_priority_transition tests
    were removed from test tree. This issue is probably a result
    of mistake that was done during video CL rebase 5 months ago.
    This CL adds global_priority_transition tests back to test tree.
    
    VK-GL-CTS issue: 4186
    
    Components: Vulkan
    
    Affects:
    dEQP-VK.synchronization.*
    
    Change-Id: I7d3ad9c6648ef560a0ce142574f7ec2ca3c0a504

--- external/vulkancts/modules/vulkan/synchronization/vktSynchronizationTests.cpp
+++ external/vulkancts/modules/vulkan/synchronization/vktSynchronizationTests.cpp
@@ -142,6 +142,7 @@ tcu::TestCaseGroup* createTestsInternal (tcu::TestContext& testCtx, Synchronizat
 			testGroup->addChild(createInternallySynchronizedObjects(testCtx));
 #ifndef CTS_USES_VULKANSC
 			testGroup->addChild(createWin32KeyedMutexTest(testCtx));
+			testGroup->addChild(createGlobalPriorityQueueTests(testCtx));
 #endif // CTS_USES_VULKANSC
 		}
 	}
