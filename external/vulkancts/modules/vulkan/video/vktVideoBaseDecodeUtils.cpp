a7a2ce442db51ca058ce051de7e09d62db44ae81
Charlie Turner <cturner@igalia.com>
2023-03-13 09:44:01 +0000

    Fix VUID-vkCmdDecodeVideoKHR-pDecodeInfo-07267 validation error.
    The top and bottom field flags are getting set in the H264 picture
    reference flags. These are mutually exclusive and only useful for
    interlacing. Currently there are no tests for interlaced sources, so
    it's unclear how the field handling code has ever been tested so far.
    
    Only set the masks if the current picture is really a field reference.
    
    Affects: dEQP-VK.video.decode.*
    
    Components: Vulkan
    
    VK-GL-CTS issue: 4333
    
    Change-Id: I91f4a6334df121c8c7170808e213e1e7c855d022

--- external/vulkancts/modules/vulkan/video/vktVideoBaseDecodeUtils.cpp
+++ external/vulkancts/modules/vulkan/video/vktVideoBaseDecodeUtils.cpp
@@ -553,15 +553,15 @@ typedef struct dpbEntry
 
 	NvidiaVulkanPictureBase* m_picBuff; // internal picture reference
 
-	void setReferenceAndTopBoottomField (bool						isReference,
-										 bool						nonExisting,
-										 bool						isLongTerm,
-										 bool						isFieldRef,
-										 bool						topFieldIsReference,
-										 bool						bottomFieldIsReference,
-										 int16_t					frameIdx,
-										 const int16_t				fieldOrderCntList[2],
-										 NvidiaVulkanPictureBase*	picBuff)
+	void setReferenceAndTopBottomField (bool						isReference,
+										bool						nonExisting,
+										bool						isLongTerm,
+										bool						isFieldRef,
+										bool						topFieldIsReference,
+										bool						bottomFieldIsReference,
+										int16_t						frameIdx,
+										const int16_t				fieldOrderCntList[2],
+										NvidiaVulkanPictureBase*	picBuff)
 	{
 		is_non_existing = nonExisting;
 		is_long_term = isLongTerm;
@@ -628,15 +628,17 @@ typedef struct dpbEntry
 			// picFlags.field_pic_flag = true;
 		}
 
-		if (used_for_reference & topFieldMask)
+		if ((used_for_reference & topFieldMask) && is_field_ref)
 		{
 			picFlags.top_field_flag = true;
 		}
 
-		if (used_for_reference & bottomFieldMask)
+		if ((used_for_reference & bottomFieldMask) && is_field_ref)
 		{
 			picFlags.bottom_field_flag = true;
 		}
+
+		DE_ASSERT(!(picFlags.top_field_flag && picFlags.bottom_field_flag));
 
 		return picFlags;
 	}
@@ -658,6 +660,7 @@ typedef struct dpbEntry
 
 		pRefPicInfo->FrameNum = FrameIdx;
 		pRefPicInfo->flags = getPictureFlag();
+
 		pRefPicInfo->PicOrderCnt[0] = FieldOrderCnt[0];
 		pRefPicInfo->PicOrderCnt[1] = FieldOrderCnt[1];
 	}
@@ -1527,7 +1530,7 @@ uint32_t VideoBaseDecoder::FillDpbH264State (const NvidiaVulkanParserPictureData
 				(int16_t)dpbIn[inIdx].FieldOrderCnt[1]
 			};
 
-			refOnlyDpbIn[numUsedRef].setReferenceAndTopBoottomField(
+			refOnlyDpbIn[numUsedRef].setReferenceAndTopBottomField(
 				!!used_for_reference,
 				(picIdx < 0), /* not_existing is frame inferred by the decoding process for gaps in frame_num */
 				!!dpbIn[inIdx].is_long_term,
