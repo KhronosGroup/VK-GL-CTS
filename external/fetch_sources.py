f09518b56fbf27e3eb2256b13b6f8914ec9830d9
Marcin Zając <marcin.zajac@mobica.com>
2023-03-29 16:29:49 +0200

    fetch_sources.py for cygwin and mingw
    After ffmpeg being removed we no longer need fetch sources customization
    depending on the results of platform.system()
    
    Affect tests:
     * dEQP-*
    
    Components: Framework
    VK-GL-CTS issue: 4358
    
    Change-Id: Idfb0c6d17e6b66d8d84a1ad9472691e75faaf011

--- external/fetch_sources.py
+++ external/fetch_sources.py
@@ -71,28 +71,18 @@ class SourcePackage (Source):
 		self.archiveDir		= "packages"
 		self.postExtract	= postExtract
 
-		if SYSTEM_NAME == 'Windows' or SYSTEM_NAME.startswith('CYGWIN') or SYSTEM_NAME.startswith('MINGW'):
-			self.sysNdx = 0
-		elif SYSTEM_NAME == 'Linux':
-			self.sysNdx = 1
-		elif SYSTEM_NAME == 'Darwin':
-			self.sysNdx = 2
-		else:
-			self.sysNdx = -1  # unknown system
-
 	def clean (self):
 		Source.clean(self)
 		self.removeArchives()
 
 	def update (self, cmdProtocol = None, force = False):
-		if self.sysNdx != 2:
-			if not self.isArchiveUpToDate():
-				self.fetchAndVerifyArchive()
+		if not self.isArchiveUpToDate():
+			self.fetchAndVerifyArchive()
 
-			if self.getExtractedChecksum() != self.checksum:
-				Source.clean(self)
-				self.extract()
-				self.storeExtractedChecksum(self.checksum)
+		if self.getExtractedChecksum() != self.checksum:
+			Source.clean(self)
+			self.extract()
+			self.storeExtractedChecksum(self.checksum)
 
 	def removeArchives (self):
 		archiveDir = os.path.join(EXTERNAL_DIR, pkg.baseDir, pkg.archiveDir)
@@ -298,19 +288,6 @@ def postExtractLibpng (path):
 	shutil.copy(os.path.join(path, "scripts", "pnglibconf.h.prebuilt"),
 				os.path.join(path, "pnglibconf.h"))
 
-if SYSTEM_NAME == 'Windows' or SYSTEM_NAME.startswith('CYGWIN') or SYSTEM_NAME.startswith('MINGW'):
-    ffmpeg_url = "https://github.com/BtbN/FFmpeg-Builds/releases/download/autobuild-2022-05-31-12-34/ffmpeg-n4.4.2-1-g8e98dfc57f-win64-lgpl-shared-4.4.zip"
-    ffmpeg_hash_value = "670df8e9d2ddd5e761459b3538f64b8826566270ef1ed13bcbfc63e73aab3fd9"
-elif SYSTEM_NAME == 'Linux':
-    ffmpeg_url = "https://github.com/BtbN/FFmpeg-Builds/releases/download/autobuild-2022-05-31-12-34/ffmpeg-n4.4.2-1-g8e98dfc57f-linux64-gpl-shared-4.4.tar.xz"
-    ffmpeg_hash_value = "817f8c93ff1ef7ede3dad15b20415d5e366bcd6848844d55046111fd3de827d0"
-elif SYSTEM_NAME == 'Darwin':
-    ffmpeg_url = ""
-    ffmpeg_hash_value = ""
-else:
-    ffmpeg_url = None
-    ffmpeg_hash_value = None
-
 PACKAGES = [
 	SourcePackage(
 		"http://zlib.net/fossils/zlib-1.2.13.tar.gz",
@@ -414,24 +391,10 @@ def run(*popenargs, **kwargs):
 	return retcode, stdout, stderr
 
 if __name__ == "__main__":
-	# Rerun script with python3 as python2 does not have lzma (xz) decompression support
-	if sys.version_info < (3, 0):
-		if SYSTEM_NAME == 'Windows' or SYSTEM_NAME.startswith('CYGWIN') or SYSTEM_NAME.startswith('MINGW'):
-			cmd = ['py', '-3']
-		elif SYSTEM_NAME == 'Linux':
-			cmd = ['python3']
-		elif SYSTEM_NAME == 'Darwin':
-			cmd = ['python3']
+	args = parseArgs()
+
+	for pkg in PACKAGES:
+		if args.clean:
+			pkg.clean()
 		else:
-			cmd = None  # unknown system
-
-		cmd = cmd + sys.argv
-		run(cmd)
-	else:
-		args = parseArgs()
-
-		for pkg in PACKAGES:
-			if args.clean:
-				pkg.clean()
-			else:
-				pkg.update(args.protocol, args.force)
+			pkg.update(args.protocol, args.force)
