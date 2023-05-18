73439003b767311ba324ae86ffc6201e756eeace
Piotr Byszewski <piotr.byszewski@mobica.com>
2023-05-18 18:28:52 +0200

    maintenance5: detect device loss
    Add test that executes a shader with a simple infinite loop. Then
    test runs set of functions that can return DEVICE_LOST. If one
    returns DEVICE_LOST all should. If any function after the
    initial one does not return DEVICE_LOST test fails.
    
    Components: Vulkan
    
    VK-GL-CTS issue: 4176
    
    Affects:
    dEQP-VK-experimental.postmortem.*
    
    New test:
    dEQP-VK-experimental.postmortem.device_loss.maintenance5
    
    Change-Id: I893c9a62b64e1746fc3923a2c498a6f50e8bc99c

--- AndroidGen.mk
+++ AndroidGen.mk
@@ -321,6 +321,7 @@ LOCAL_SRC_FILES := \
 	external/vulkancts/modules/vulkan/pipeline/vktPipelineVertexInputTests.cpp \
 	external/vulkancts/modules/vulkan/pipeline/vktPipelineVertexUtil.cpp \
 	external/vulkancts/modules/vulkan/postmortem/vktPostmortemDeviceFaultTests.cpp \
+	external/vulkancts/modules/vulkan/postmortem/vktPostmortemDeviceLossTests.cpp \
 	external/vulkancts/modules/vulkan/postmortem/vktPostmortemShaderTimeoutTests.cpp \
 	external/vulkancts/modules/vulkan/postmortem/vktPostmortemTests.cpp \
 	external/vulkancts/modules/vulkan/postmortem/vktPostmortemUseAfterFreeTests.cpp \
