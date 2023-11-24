KhronosGroup/VK-GL-CTS

commit a8b1e1bd006e8b76d35f3fce72efb9ce27acb7ea

Add verbose option for logging build information
This helps debugging issues and understanding which exact commands are being run during the build.

This CL does not affect any test.

Change-Id: I75083b5889ca9ff3012f2f300a35f90f11c36d0c

scripts/check_build_sanity.py
@@ -31,9 +31,10 @@ from ctsbuild.build import *
 pythonExecutable = sys.executable or "python"
 
 class Environment:
-	def __init__ (self, srcDir, tmpDir):
+	def __init__ (self, srcDir, tmpDir, verbose):
 		self.srcDir	= srcDir
 		self.tmpDir	= tmpDir
+		self.verbose = verbose
 
 class BuildTestStep:
 	def getName (self):
@@ -199,11 +200,11 @@ EARLY_SPECIAL_RECIPES	= [
 LATE_SPECIAL_RECIPES	= [
 	('android-mustpass', [
 			RunScript(os.path.join("scripts", "build_android_mustpass.py"),
-					  lambda env: ["--build-dir", os.path.join(env.tmpDir, "android-mustpass")]),
+					  lambda env: ["--build-dir", os.path.join(env.tmpDir, "android-mustpass")] + ["--verbose"] if env.verbose else []),
 		]),
 	('vulkan-mustpass', [
 			RunScript(os.path.join("external", "vulkancts", "scripts", "build_mustpass.py"),
-					  lambda env: ["--build-dir", os.path.join(env.tmpDir, "vulkan-mustpass")]),
+					  lambda env: ["--build-dir", os.path.join(env.tmpDir, "vulkan-mustpass")] + ["--verbose"] if env.verbose else []),
 		]),
 	('spirv-binaries', [
 			RunScript(os.path.join("external", "vulkancts", "scripts", "build_spirv_binaries.py"),
@@ -275,12 +276,17 @@ def parseArgs ():
 						dest="skipPostCheck",
 						action="store_true",
 						help="Skip post recipe checks")
+	parser.add_argument("-v", "--verbose",
+						dest="verbose",
+						action="store_true",
+						help="Enable verbose logging")
 
 	return parser.parse_args()
 
 if __name__ == "__main__":
 	args	= parseArgs()
-	env		= Environment(args.srcDir, args.tmpDir)
+	env		= Environment(args.srcDir, args.tmpDir, args.verbose)
+	initializeLogger(args.verbose)
 
 	if args.dumpRecipes:
 		for name, steps in RECIPES:

scripts/ctsbuild/common.py
@@ -25,6 +25,7 @@ import sys
 import shlex
 import platform
 import subprocess
+import logging
 
 DEQP_DIR = os.path.realpath(os.path.normpath(os.path.join(os.path.dirname(__file__), "..", "..")))
 
@@ -74,6 +75,7 @@ g_workDirStack = []
 def pushWorkingDir (path):
 	oldDir = os.getcwd()
 	os.chdir(path)
+	logging.debug("Current working directory: " + path)
 	g_workDirStack.append(oldDir)
 
 def popWorkingDir ():
@@ -81,8 +83,10 @@ def popWorkingDir ():
 	newDir = g_workDirStack[-1]
 	g_workDirStack.pop()
 	os.chdir(newDir)
+	logging.debug("Current working directory: " + newDir)
 
 def execute (args):
+	logging.debug("Running: " + " ".join(args))
 	retcode	= subprocess.call(args)
 	if retcode != 0:
 		raise Exception("Failed to execute '%s', got %d" % (str(args), retcode))
@@ -100,11 +104,13 @@ def readFile (filename):
 	return data
 
 def writeBinaryFile (filename, data):
+	logging.debug("Writing to binary file: " + filename)
 	f = open(filename, 'wb')
 	f.write(data)
 	f.close()
 
 def writeFile (filename, data):
+	logging.debug("Writing to file: " + filename)
 	if (sys.version_info < (3, 0)):
 		f = open(filename, 'wt')
 	else:
@@ -132,6 +138,14 @@ def which (binName, paths = None):
 	for extension in extensions:
 		extResult = whichImpl(binName + extension)
 		if extResult != None:
+			logging.debug("which: found " + binName + " as " + extResult)
 			return extResult
 
 	return None
+
+def initializeLogger(verbose):
+	log_level = logging.INFO
+	if verbose:
+		log_level = logging.DEBUG
+	logging.root.setLevel(level=log_level)
+	logging.debug("Logging is set to " + str(log_level))

scripts/mustpass.py
@@ -423,8 +423,13 @@ def parseCmdLineArgs ():
 						dest="targetName",
 						default=DEFAULT_TARGET,
 						help="dEQP build target")
+	parser.add_argument("-v", "--verbose",
+						dest="verbose",
+						action="store_true",
+						help="Enable verbose logging")
 	return parser.parse_args()
 
 def parseBuildConfigFromCmdLineArgs ():
 	args = parseCmdLineArgs()
+	initializeLogger(args.verbose)
 	return getBuildConfig(args.buildDir, args.targetName, args.buildType)

commit 29ded10d935e677894ce7d2313b32ef76875b74a

Fix for bug introduced in a8b1e1b (verbose option)
When --verbose was not passed, the scripts would ignore the other arguments.

Change-Id: Iefba054b21551f7b819812eaa050fc0e06c3b530

scripts/check_build_sanity.py
@@ -200,11 +200,11 @@ EARLY_SPECIAL_RECIPES	= [
 LATE_SPECIAL_RECIPES	= [
 	('android-mustpass', [
 			RunScript(os.path.join("scripts", "build_android_mustpass.py"),
-					  lambda env: ["--build-dir", os.path.join(env.tmpDir, "android-mustpass")] + ["--verbose"] if env.verbose else []),
+					  lambda env: ["--build-dir", os.path.join(env.tmpDir, "android-mustpass")] + (["--verbose"] if env.verbose else [])),
 		]),
 	('vulkan-mustpass', [
 			RunScript(os.path.join("external", "vulkancts", "scripts", "build_mustpass.py"),
-					  lambda env: ["--build-dir", os.path.join(env.tmpDir, "vulkan-mustpass")] + ["--verbose"] if env.verbose else []),
+					  lambda env: ["--build-dir", os.path.join(env.tmpDir, "vulkan-mustpass")] + (["--verbose"] if env.verbose else [])),
 		]),
 	('spirv-binaries', [
 			RunScript(os.path.join("external", "vulkancts", "scripts", "build_spirv_binaries.py"),