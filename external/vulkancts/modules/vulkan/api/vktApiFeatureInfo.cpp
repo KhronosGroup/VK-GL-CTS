628a1fbee4dfa65b4587895e9925e9f624f2cc7d
Trevor David Black <vantablack@google.com>
2023-12-05 19:32:17 +0000

    Add user_type extension to no_unknown_extensions
    The android.no_unknown_extensions helps to make sure the device doesn't
    expose any unknown extensions. We have a device (cuttlefish) that
    exposes VK_GOOGLE_user_type and is causing problems.
    
    Affects:
    
    dEQP-VK.api.info.android.no_unknown_extensions
    
    Components: Vulkan, AOSP
    
    Google bug: 304204466
    VK-GL-CTS issue: 4838
    
    Change-Id: I91411b58fcd705c489cb6d447496ab2124c651bc

--- external/vulkancts/modules/vulkan/api/vktApiFeatureInfo.cpp
+++ external/vulkancts/modules/vulkan/api/vktApiFeatureInfo.cpp
@@ -6750,6 +6750,7 @@ tcu::TestStatus testNoUnknownExtensions (Context& context)
 	allowedDeviceExtensions.insert("VK_GOOGLE_display_timing");
 	allowedDeviceExtensions.insert("VK_GOOGLE_decorate_string");
 	allowedDeviceExtensions.insert("VK_GOOGLE_hlsl_functionality1");
+	allowedDeviceExtensions.insert("VK_GOOGLE_user_type");
 	allowedInstanceExtensions.insert("VK_GOOGLE_surfaceless_query");
 
 	// Instance extensions
