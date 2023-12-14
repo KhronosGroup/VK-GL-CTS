0393a61b564fbfbfb0a4ac40a88aefe217dd3740
Lorenzo Dal Col <lorenzo@khronosgroup.org>
2023-12-14 15:20:19 +0100

    Add pass/fail results for each session in cts-run-summary.xml
    This does not affect any test.
    
    VK-GL-CTS Issue: 867
    
    Change-Id: I825f62c164f2e0ce7ad70ed2b3fcb35daeb625cb

--- external/openglcts/modules/runner/glcTestRunner.hpp
+++ external/openglcts/modules/runner/glcTestRunner.hpp
@@ -26,6 +26,7 @@
 
 #include "gluPlatform.hpp"
 #include "tcuDefs.hpp"
+#include "tcuTestPackage.hpp"
 
 #include <string>
 #include <vector>
@@ -67,6 +68,7 @@ struct TestRunSummary
 	bool					   isConformant;
 	std::string				   configLogFilename;
 	std::vector<TestRunParams> runParams;
+	std::vector<tcu::TestRunStatus> results;
 
 	TestRunSummary(void) : isConformant(false)
 	{
@@ -78,6 +80,7 @@ struct TestRunSummary
 		isConformant = false;
 		configLogFilename.clear();
 		runParams.clear();
+		results.clear();
 	}
 };
 
