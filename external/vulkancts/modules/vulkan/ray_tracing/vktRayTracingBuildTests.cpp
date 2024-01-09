baf39acc306a26a904fb51c07f2601aebb7f5774
Ricardo Garcia <rgarcia@igalia.com>
2024-01-09 15:34:59 +0100

    Use consistent ray payload declarations
    Several tests were using inconsistent ray payload declarations for some
    variables. Even if unused, this goes against the spec, which reads:
    
    > The ray payload for a shader invoked by a ray tracing command must be
    > the same structure for all shader stages using the payload for that
    > ray.
    
    In addition, this commit also renames some of the ray tracing variables
    from "dummy" to a variant of "unused", which is more explicit and does
    not go against the inclusive language agreement:
    
    https://www.khronos.org/about/inclusive-language
    
    Affects:
    dEQP-VK.ray_tracing_pipeline.build.*
    dEQP-VK.ray_tracing_pipeline.indirect.*
    dEQP-VK.ray_tracing_pipeline.misc.*
    dEQP-VK.ray_tracing_pipeline.null_as.*
    dEQP-VK.ray_tracing_pipeline.watertightness.*
    
    Components: Vulkan
    VK-GL-CTS issue: 4877
    
    Change-Id: If1c2a03650b760dfbbf9ac720b9f10378b852527

--- external/vulkancts/modules/vulkan/ray_tracing/vktRayTracingBuildTests.cpp
+++ external/vulkancts/modules/vulkan/ray_tracing/vktRayTracingBuildTests.cpp
@@ -229,7 +229,7 @@ void RayTracingTestCase::initPrograms (SourceCollections& programCollection) con
 		css <<
 			"#version 460 core\n"
 			"#extension GL_EXT_ray_tracing : require\n"
-			"layout(location = 0) rayPayloadInEXT dummyPayload { vec4 dummy; };\n"
+			"layout(location = 0) rayPayloadInEXT vec3 unusedPayload;\n"
 			"layout(r32ui, set = 0, binding = 0) uniform uimage2D result;\n"
 			"void main()\n"
 			"{\n"
