de53a57350a728bc3319d182fcf3eec2766c2658
Ricardo Garcia <rgarcia@igalia.com>
2024-01-10 11:38:38 +0100

    Test frag shader invocations with non-flat colors
    Implementations are allowed to reuse the same output for multiple
    fragments, but they can't do so if the color is not known to be constant
    at runtime or if the frag shader has side effects.
    
    This commit adds non-flat color shading variants and verify the number
    of frag shader invocations matches the pixel count.
    
    New and affected tests:
    dEQP-VK.query_pool.frag_invocations.*
    
    Components: Vulkan
    VK-GL-CTS issue: 4851
    
    Change-Id: I9712a8cf82f5c1b65f267c6a0460c27c47ac48da

--- external/vulkancts/modules/vulkan/query_pool/vktQueryPoolFragInvocationTests.cpp
+++ external/vulkancts/modules/vulkan/query_pool/vktQueryPoolFragInvocationTests.cpp
@@ -29,6 +29,7 @@
 #include "vkObjUtil.hpp"
 #include "vkTypeUtil.hpp"
 #include "vktTestCaseUtil.hpp"
+#include "vkBuilderUtil.hpp"
 
 #include <sstream>
 
@@ -57,13 +58,21 @@ std::string getQueryTypeName (const QueryType qType)
 	return "";
 }
 
-struct TestParams
+enum class FragShaderVariant
 {
-	const QueryType	queryType;
-	const bool		secondary;
+	FLAT			= 0,
+	VERTEX_COLOR	= 1,
+	ATOMIC_COUNTER	= 2,
 };
 
-tcu::Vec4 getFlatColor (void)
+struct TestParams
+{
+	const QueryType			queryType;
+	const bool				secondary;
+	const FragShaderVariant	fragShaderVariant;
+};
+
+tcu::Vec4 getGeometryColor (void)
 {
 	return tcu::Vec4(0.0f, 0.0f, 1.0f, 1.0f);
 }
@@ -82,20 +91,25 @@ void checkSupport (Context& context, TestParams params)
 		context.requireDeviceCoreFeature(DEVICE_CORE_FEATURE_OCCLUSION_QUERY_PRECISE);
 	else if (params.queryType == QueryType::INVOCATIONS)
 		context.requireDeviceCoreFeature(DEVICE_CORE_FEATURE_PIPELINE_STATISTICS_QUERY);
+
+	if (params.fragShaderVariant == FragShaderVariant::ATOMIC_COUNTER)
+		context.requireDeviceCoreFeature(DEVICE_CORE_FEATURE_FRAGMENT_STORES_AND_ATOMICS);
 }
 
-void initPrograms (vk::SourceCollections& programCollection, TestParams)
+void initPrograms (vk::SourceCollections& programCollection, TestParams params)
 {
+	const bool isAtomic			= (params.fragShaderVariant == FragShaderVariant::ATOMIC_COUNTER);
+	const bool isVertexColor	= (params.fragShaderVariant == FragShaderVariant::VERTEX_COLOR);
+
 	std::ostringstream vert;
 	vert
 		<< "#version 460\n"
-		<< "vec2 positions[3] = vec2[](\n"
-		<< "    vec2(-1.0, -1.0),"
-		<< "    vec2(3.0, -1.0),"
-		<< "    vec2(-1.0, 3.0)"
-		<< ");\n"
+		<< "layout (location=0) in vec4 inPos;\n"
+		<< (isVertexColor ? "layout (location=1) in vec4 inColor;\n" : "")
+		<< (isVertexColor ? "layout (location=0) out vec4 outColor;\n" : "")
 		<< "void main() {\n"
-		<< "    gl_Position = vec4(positions[gl_VertexIndex % 3], 0.0, 1.0);\n"
+		<< "    gl_Position = inPos;\n"
+		<< (isVertexColor ? "    outColor = inColor;\n" : "")
 		<< "}"
 		;
 	programCollection.glslSources.add("vert") << glu::VertexSource(vert.str());
@@ -104,16 +118,32 @@ void initPrograms (vk::SourceCollections& programCollection, TestParams)
 	frag
 		<< "#version 460\n"
 		<< "layout (location=0) out vec4 outColor;\n"
+		<< (isVertexColor ? "layout (location=0) in vec4 vtxColor;\n" : "")
+		<< (isAtomic ? "layout (set=0, binding=0) buffer CounterBlock { uint counter; } cb;\n" : "")
 		<< "void main() {\n"
-		<< "    outColor = vec4" << getFlatColor() << ";\n"
+		<< (isAtomic ? "    atomicAdd(cb.counter, 1u);\n" : "")
+		<< "    outColor = " << (isVertexColor ? "vtxColor" : ("vec4" + de::toString(getGeometryColor()))) << ";\n"
 		<< "}";
 		;
 	programCollection.glslSources.add("frag") << glu::FragmentSource(frag.str());
 }
 
-void recordRenderPassCommands (const DeviceInterface& vkd, const VkCommandBuffer cmdBuffer, const VkPipelineBindPoint bindPoint, const VkPipeline pipeline)
+// Records the render pass commands (bind pipeline and draw) and optionally binds descriptor sets.
+void recordRenderPassCommands	(const DeviceInterface&		vkd,
+								 const VkCommandBuffer		cmdBuffer,
+								 const VkPipelineBindPoint	bindPoint,
+								 const VkPipeline			pipeline,
+								 const VkPipelineLayout		pipelineLayout,
+								 const VkDescriptorSet		descriptorSet,	// Can be made VK_NULL_HANDLE to avoid binding.
+								 const VkBuffer				vertexBuffer,
+								 const VkDeviceSize			vertexBufferOffset)
 {
 	vkd.cmdBindPipeline(cmdBuffer, bindPoint, pipeline);
+
+	if (descriptorSet != VK_NULL_HANDLE)
+		vkd.cmdBindDescriptorSets(cmdBuffer, bindPoint, pipelineLayout, 0u, 1u, &descriptorSet, 0u, nullptr);
+
+	vkd.cmdBindVertexBuffers(cmdBuffer, 0u, 1u, &vertexBuffer, &vertexBufferOffset);
 	vkd.cmdDraw(cmdBuffer, 3u, 1u, 0u, 0u);
 }
 
@@ -128,6 +158,9 @@ tcu::TestStatus testInvocations (Context& context, const TestParams params)
 	const auto			colorUsage			= (VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT | VK_IMAGE_USAGE_TRANSFER_SRC_BIT);
 	const auto			imageType			= VK_IMAGE_TYPE_2D;
 	const auto			bindPoint			= VK_PIPELINE_BIND_POINT_GRAPHICS;
+	const auto			isAtomic			= (params.fragShaderVariant == FragShaderVariant::ATOMIC_COUNTER);
+	const auto			resShaderStages		= VK_SHADER_STAGE_FRAGMENT_BIT; // Shader stages for the resources.
+	const auto			geometryColor		= getGeometryColor();
 
 	ImageWithBuffer colorBuffer (ctx.vkd, ctx.device, ctx.allocator, vkExtent, colorFormat, colorUsage, imageType, colorSRR);
 
@@ -135,7 +168,67 @@ tcu::TestStatus testInvocations (Context& context, const TestParams params)
 	const auto	vertModule	= createShaderModule(ctx.vkd, ctx.device, binaries.get("vert"));
 	const auto	fragModule	= createShaderModule(ctx.vkd, ctx.device, binaries.get("frag"));
 
-	const auto pipelineLayout	= makePipelineLayout(ctx.vkd, ctx.device);
+	// Vertex buffer, which is going to contain interleaved pairs of positions and colors.
+	const std::vector<tcu::Vec4> vertices =
+	{
+		// position								// color
+		tcu::Vec4(-1.0f, -1.0f, 0.0f, 1.0f),	geometryColor,
+		tcu::Vec4( 3.0f, -1.0f, 0.0f, 1.0f),	geometryColor,
+		tcu::Vec4(-1.0f,  3.0f, 0.0f, 1.0f),	geometryColor,
+	};
+
+	const uint32_t kVec4Sz			= static_cast<uint32_t>(sizeof(tcu::Vec4));
+	const uint32_t vertexStride		= (kVec4Sz * 2u);
+	const uint32_t positionOffset	= 0u;
+	const uint32_t colorOffset		= kVec4Sz;
+
+	// Vertex buffer
+	const auto			vbSize			= static_cast<VkDeviceSize>(de::dataSize(vertices));
+	const auto			vbInfo			= makeBufferCreateInfo(vbSize, VK_BUFFER_USAGE_VERTEX_BUFFER_BIT);
+	BufferWithMemory	vertexBuffer	(ctx.vkd, ctx.device, ctx.allocator, vbInfo, MemoryRequirement::HostVisible);
+	const auto			vbAlloc			= vertexBuffer.getAllocation();
+	void*				vbData			= vbAlloc.getHostPtr();
+	const auto			vbOffset		= static_cast<VkDeviceSize>(0);
+
+	deMemcpy(vbData, de::dataOrNull(vertices), de::dataSize(vertices));
+	flushAlloc(ctx.vkd, ctx.device, vbAlloc); // strictly speaking, not needed.
+
+	using BufferWithMemoryPtr = std::unique_ptr<BufferWithMemory>;
+
+	// Used in the atomic buffer case.
+	Move<VkDescriptorSetLayout>	setLayout;
+	Move<VkDescriptorPool>		descriptorPool;
+	Move<VkDescriptorSet>		descriptorSet;
+	BufferWithMemoryPtr			atomicBuffer;
+
+	const auto atomicBufferType	= VK_DESCRIPTOR_TYPE_STORAGE_BUFFER;
+	const auto atomicBufferSize	= static_cast<VkDeviceSize>(sizeof(uint32_t));
+
+	if (isAtomic)
+	{
+		// Zero-out atomic counter.
+		const auto atomicBufferInfo = makeBufferCreateInfo(atomicBufferSize, VK_BUFFER_USAGE_STORAGE_BUFFER_BIT);
+		atomicBuffer.reset(new BufferWithMemory(ctx.vkd, ctx.device, ctx.allocator, atomicBufferInfo, MemoryRequirement::HostVisible));
+		deMemset(atomicBuffer->getAllocation().getHostPtr(), 0, static_cast<size_t>(atomicBufferSize));
+
+		DescriptorSetLayoutBuilder layoutBuilder;
+		layoutBuilder.addSingleBinding(atomicBufferType, resShaderStages);
+		setLayout = layoutBuilder.build(ctx.vkd, ctx.device);
+
+		DescriptorPoolBuilder descPoolBuilder;
+		descPoolBuilder.addType(atomicBufferType);
+		descriptorPool = descPoolBuilder.build(ctx.vkd, ctx.device, VK_DESCRIPTOR_POOL_CREATE_FREE_DESCRIPTOR_SET_BIT, 1u);
+
+		descriptorSet = makeDescriptorSet(ctx.vkd, ctx.device, *descriptorPool, *setLayout);
+
+		DescriptorSetUpdateBuilder updateBuilder;
+		const auto atomicBufferDescriptorInfo = makeDescriptorBufferInfo(atomicBuffer->get(), 0ull, atomicBufferSize);
+		updateBuilder.writeSingle(*descriptorSet, DescriptorSetUpdateBuilder::Location::binding(0u), atomicBufferType, &atomicBufferDescriptorInfo);
+		updateBuilder.update(ctx.vkd, ctx.device);
+	}
+
+	// Pipeline layout, render pass and framebuffer.
+	const auto pipelineLayout	= makePipelineLayout(ctx.vkd, ctx.device, *setLayout);
 	const auto renderPass		= makeRenderPass(ctx.vkd, ctx.device, colorFormat);
 	const auto framebuffer		= makeFramebuffer(ctx.vkd, ctx.device, renderPass.get(), colorBuffer.getImageView(), vkExtent.width, vkExtent.height);
 
@@ -158,7 +251,23 @@ tcu::TestStatus testInvocations (Context& context, const TestParams params)
 	const std::vector<VkViewport>	viewports	(1u, makeViewport(vkExtent));
 	const std::vector<VkRect2D>		scissors	(1u, makeRect2D(fbExtent));
 
-	const VkPipelineVertexInputStateCreateInfo inputStateCreateInfo = initVulkanStructure();
+	// Vertex buffer description.
+	const auto vtxBinding = makeVertexInputBindingDescription(0u, vertexStride, VK_VERTEX_INPUT_RATE_VERTEX);
+	const std::vector<VkVertexInputAttributeDescription> vtxAttribs
+	{
+		makeVertexInputAttributeDescription(0u, 0u, VK_FORMAT_R32G32B32A32_SFLOAT, positionOffset),
+		makeVertexInputAttributeDescription(1u, 0u, VK_FORMAT_R32G32B32A32_SFLOAT, colorOffset),
+	};
+	const VkPipelineVertexInputStateCreateInfo inputStateCreateInfo =
+	{
+		VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO,	//	VkStructureType								sType;
+		nullptr,													//	const void*									pNext;
+		0u,															//	VkPipelineVertexInputStateCreateFlags		flags;
+		1u,															//	uint32_t									vertexBindingDescriptionCount;
+		&vtxBinding,												//	const VkVertexInputBindingDescription*		pVertexBindingDescriptions;
+		de::sizeU32(vtxAttribs),									//	uint32_t									vertexAttributeDescriptionCount;
+		de::dataOrNull(vtxAttribs),									//	const VkVertexInputAttributeDescription*	pVertexAttributeDescriptions;
+	};
 
 	const auto pipeline = makeGraphicsPipeline(ctx.vkd, ctx.device, pipelineLayout.get(),
 		vertModule.get(), VK_NULL_HANDLE, VK_NULL_HANDLE, VK_NULL_HANDLE, fragModule.get(), renderPass.get(),
@@ -196,7 +305,7 @@ tcu::TestStatus testInvocations (Context& context, const TestParams params)
 		};
 
 		VK_CHECK(ctx.vkd.beginCommandBuffer(secCmdBuffer, &beginInfo));
-		recordRenderPassCommands(ctx.vkd, secCmdBuffer, bindPoint, pipeline.get());
+		recordRenderPassCommands(ctx.vkd, secCmdBuffer, bindPoint, pipeline.get(), *pipelineLayout, *descriptorSet, *vertexBuffer, vbOffset);
 		endCommandBuffer(ctx.vkd, secCmdBuffer);
 	}
 
@@ -208,7 +317,7 @@ tcu::TestStatus testInvocations (Context& context, const TestParams params)
 	ctx.vkd.cmdBeginQuery(primaryCmdBuffer, queryPool.get(), 0u, controlFlags);
 	beginRenderPass(ctx.vkd, primaryCmdBuffer, renderPass.get(), framebuffer.get(), scissors.at(0), clearColor, subpassContents);
 	if (!params.secondary)
-		recordRenderPassCommands(ctx.vkd, primaryCmdBuffer, bindPoint, pipeline.get());
+		recordRenderPassCommands(ctx.vkd, primaryCmdBuffer, bindPoint, pipeline.get(), *pipelineLayout, *descriptorSet, *vertexBuffer, vbOffset);
 	else
 		ctx.vkd.cmdExecuteCommands(primaryCmdBuffer, 1u, &secCmdBufferPtr.get());
 	endRenderPass(ctx.vkd, primaryCmdBuffer);
@@ -225,8 +334,20 @@ tcu::TestStatus testInvocations (Context& context, const TestParams params)
 		const auto copyRegion = makeBufferImageCopy(vkExtent, colorSRL);
 		ctx.vkd.cmdCopyImageToBuffer(primaryCmdBuffer, colorBuffer.getImage(), VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL, colorBuffer.getBuffer(), 1u, &copyRegion);
 
-		const auto preHostBarrier = makeMemoryBarrier(VK_ACCESS_TRANSFER_WRITE_BIT, VK_ACCESS_HOST_READ_BIT);
-		cmdPipelineMemoryBarrier(ctx.vkd, primaryCmdBuffer, VK_PIPELINE_STAGE_TRANSFER_BIT, VK_PIPELINE_STAGE_HOST_BIT, &preHostBarrier);
+		// Synchronize color buffer copy, and possible atomic writes from the frag shader, to the host.
+		{
+			VkAccessFlags			srcAccess = VK_ACCESS_TRANSFER_WRITE_BIT;
+			VkPipelineStageFlags	srcStages = VK_PIPELINE_STAGE_TRANSFER_BIT;
+
+			if (isAtomic)
+			{
+				srcAccess |= VK_ACCESS_SHADER_WRITE_BIT;
+				srcStages |= VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT;
+			}
+
+			const auto preHostBarrier = makeMemoryBarrier(srcAccess, VK_ACCESS_HOST_READ_BIT);
+			cmdPipelineMemoryBarrier(ctx.vkd, primaryCmdBuffer, srcStages, VK_PIPELINE_STAGE_HOST_BIT, &preHostBarrier);
+		}
 	}
 	endCommandBuffer(ctx.vkd, primaryCmdBuffer);
 	submitCommandsAndWait(ctx.vkd, ctx.device, ctx.queue, primaryCmdBuffer);
@@ -237,24 +358,34 @@ tcu::TestStatus testInvocations (Context& context, const TestParams params)
 	uint32_t queryResult = 0u;
 	VK_CHECK(ctx.vkd.getQueryPoolResults(ctx.device, queryPool.get(), 0u, 1u, sizeof(queryResult), &queryResult, static_cast<VkDeviceSize>(sizeof(queryResult)), VK_QUERY_RESULT_WAIT_BIT));
 
+	const auto pixelCount = vkExtent.width * vkExtent.height * vkExtent.depth;
+
+	// Check query results.
 	if (isInvQuery)
 	{
-		// Implementations are allowed to reuse fragment shader invocations to shade different fragments under some circumstances:
-		// - The frag shader statically computes the same value for different framebuffer locations, and
-		// - It does not write to any storage resources.
-		//
-		// The spec does not mention a minimum number of invocations, but in practice we're tying this to enabling fragment shading
-		// rate support automatically. We'll suppose implementations not supporting fragment shading rate will not do this, and
-		// those supporting it will not run less invocations than the whole framebuffer divided into areas of maxFramentSize pixels.
-		// If this proves problematic, we can relax the check later.
-		const auto	maxFragmentSize		= (context.isDeviceFunctionalitySupported("VK_KHR_fragment_shading_rate")
-										? context.getFragmentShadingRateProperties().maxFragmentSize
-										: makeExtent2D(1u, 1u));
-		const auto	maxFragmentWidth	= std::max(maxFragmentSize.width, 1u);	// In case an implementation reports zero.
-		const auto	maxFragmentHeight	= std::max(maxFragmentSize.height, 1u);	// Ditto.
-		const auto	minCols				= vkExtent.width / maxFragmentWidth;
-		const auto	minRows				= vkExtent.height / maxFragmentHeight;
-		const auto	minCount			= minCols * minRows * vkExtent.depth;
+		uint32_t minCount = std::numeric_limits<uint32_t>::max();
+
+		if (params.fragShaderVariant == FragShaderVariant::FLAT)
+		{
+			// Implementations are allowed to reuse fragment shader invocations to shade different fragments under some circumstances:
+			// - The frag shader statically computes the same value for different framebuffer locations, and
+			// - It does not write to any storage resources.
+			// The spec does not mention a minimum number of invocations, but in practice we're tying this to enabling fragment shading
+			// rate support automatically. We'll suppose implementations not supporting fragment shading rate will not do this, and
+			// those supporting it will not run less invocations than the whole framebuffer divided into areas of maxFramentSize pixels.
+			// If this proves problematic, we can relax the check later.
+			const auto	maxFragmentSize		= (context.isDeviceFunctionalitySupported("VK_KHR_fragment_shading_rate")
+											? context.getFragmentShadingRateProperties().maxFragmentSize
+											: makeExtent2D(1u, 1u));
+			const auto	maxFragmentWidth	= std::max(maxFragmentSize.width, 1u);	// In case an implementation reports zero.
+			const auto	maxFragmentHeight	= std::max(maxFragmentSize.height, 1u);	// Ditto.
+			const auto	minCols				= vkExtent.width / maxFragmentWidth;
+			const auto	minRows				= vkExtent.height / maxFragmentHeight;
+
+			minCount						= minCols * minRows * vkExtent.depth;
+		}
+		else
+			minCount						= pixelCount;
 
 		if (queryResult < minCount)
 		{
@@ -265,7 +396,6 @@ tcu::TestStatus testInvocations (Context& context, const TestParams params)
 	}
 	else
 	{
-		const auto pixelCount = vkExtent.width * vkExtent.height * vkExtent.depth;
 		if (pixelCount != queryResult)
 		{
 			std::ostringstream msg;
@@ -274,12 +404,30 @@ tcu::TestStatus testInvocations (Context& context, const TestParams params)
 		}
 	}
 
+	if (isAtomic)
+	{
+		// Verify atomic counter.
+		auto& atomicBufferAlloc = atomicBuffer->getAllocation();
+		invalidateAlloc(ctx.vkd, ctx.device, atomicBufferAlloc);
+
+		uint32_t atomicCounter = 0u;
+		deMemcpy(&atomicCounter, atomicBufferAlloc.getHostPtr(), sizeof(atomicCounter));
+
+		if (atomicCounter != pixelCount)
+		{
+			std::ostringstream msg;
+			msg << "Framebuffer size: " << vkExtent.width << "x" << vkExtent.height << "; expected atomic counter to be " << pixelCount << " but found " << atomicCounter;
+			return tcu::TestStatus::fail(msg.str());
+		}
+	}
+
+	// Check color buffer.
 	const auto					tcuFormat		= mapVkFormat(colorFormat);
 	auto&						log				= context.getTestContext().getLog();
 	const tcu::Vec4				colorThreshold	(0.0f, 0.0f, 0.0f, 0.0f); // Expect exact color result.
 	tcu::ConstPixelBufferAccess	resultAccess	(tcuFormat, fbExtent, resultAllocation.getHostPtr());
 
-	if (!tcu::floatThresholdCompare(log, "Result", "", getFlatColor(), resultAccess, colorThreshold, tcu::COMPARE_LOG_ON_ERROR))
+	if (!tcu::floatThresholdCompare(log, "Result", "", geometryColor, resultAccess, colorThreshold, tcu::COMPARE_LOG_ON_ERROR))
 		return tcu::TestStatus::fail("Unexpected results in color buffer -- check log for details");
 
 	return tcu::TestStatus::pass("Pass");
@@ -294,6 +442,17 @@ tcu::TestCaseGroup* createFragInvocationTests (tcu::TestContext& testContext)
 	// Test implementations do not optimize out fragment shader invocations
 	GroupPtr mainGroup (new tcu::TestCaseGroup(testContext, "frag_invocations"));
 
+	const struct
+	{
+		FragShaderVariant	fragShaderVariant;
+		const std::string	suffix;
+	} fragShaderVariantCases[] =
+	{
+		{ FragShaderVariant::FLAT,				""						},
+		{ FragShaderVariant::VERTEX_COLOR,		"_with_vertex_color"	},
+		{ FragShaderVariant::ATOMIC_COUNTER,	"_with_atomic_counter"	},
+	};
+
 	for (const auto queryType : { QueryType::OCCLUSION, QueryType::INVOCATIONS })
 	{
 		const auto groupName = getQueryTypeName(queryType);
@@ -301,9 +460,12 @@ tcu::TestCaseGroup* createFragInvocationTests (tcu::TestContext& testContext)
 
 		for (const auto secondaryCase : { false, true })
 		{
-			const auto testName = (secondaryCase ? "secondary" : "primary");
-			const TestParams params { queryType, secondaryCase };
-			addFunctionCaseWithPrograms(queryTypeGroup.get(), testName, checkSupport, initPrograms, testInvocations, params);
+			for (const auto& fragShaderVariantCase : fragShaderVariantCases)
+			{
+				const auto testName = (secondaryCase ? "secondary" : "primary") + fragShaderVariantCase.suffix;
+				const TestParams params { queryType, secondaryCase, fragShaderVariantCase.fragShaderVariant };
+				addFunctionCaseWithPrograms(queryTypeGroup.get(), testName, checkSupport, initPrograms, testInvocations, params);
+			}
 		}
 
 		mainGroup->addChild(queryTypeGroup.release());
