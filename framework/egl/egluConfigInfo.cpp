KhronosGroup/VK-GL-CTS

commit 88ba9ac270db5be600b1ecacbc6d9db0c55d5be4

Implement support for the EGL_EXT_config_select_group extension
Affects:
dEQP-EGL.functional.choose_config.*

Ref: https://registry.khronos.org/EGL/extensions/EXT/EGL_EXT_config_select_group.txt

Components: Framework, Modules, Scripts

Closes: https://github.com/KhronosGroup/VK-GL-CTS/issues/392

Co-authored-by: Erico Nunes <nunes.erico@gmail.com>
Co-authored-by: Robert Mader <robert.mader@posteo.de>
Change-Id: I5e749cf9a51c119713d25801ec15da9bb78acdd8
Signed-off-by: David Heidelberg <david.heidelberg@collabora.com>

framework/egl/egluConfigInfo.cpp
@@ -83,6 +83,9 @@ deInt32 ConfigInfo::getAttribute (deUint32 attribute) const
 		// EGL_ANDROID_recordable
 		case EGL_RECORDABLE_ANDROID:		return recordableAndroid;
 
+		// EGL_EXT_config_select_group
+		case EGL_CONFIG_SELECT_GROUP_EXT:	return groupId;
+
 		default:							TCU_THROW(InternalError, "Unknown attribute");
 	}
 }
@@ -125,7 +128,8 @@ void queryCoreConfigInfo (const Library& egl, EGLDisplay display, EGLConfig conf
 
 void queryExtConfigInfo (const eglw::Library& egl, eglw::EGLDisplay display, eglw::EGLConfig config, ConfigInfo* dst)
 {
-	const std::vector<std::string>	extensions	= getDisplayExtensions(egl, display);
+	const std::vector<std::string>	extensions		= getDisplayExtensions(egl, display);
+	const std::vector<std::string>	clientExtensions	= getClientExtensions(egl);
 
 	if (de::contains(extensions.begin(), extensions.end(), "EGL_EXT_yuv_surface"))
 	{
@@ -154,6 +158,13 @@ void queryExtConfigInfo (const eglw::Library& egl, eglw::EGLDisplay display, egl
 	}
 	else
 		dst->colorComponentType = EGL_COLOR_COMPONENT_TYPE_FIXED_EXT;
+
+	if (de::contains(clientExtensions.begin(), clientExtensions.end(), "EGL_EXT_config_select_group"))
+	{
+		egl.getConfigAttrib(display, config, EGL_CONFIG_SELECT_GROUP_EXT,	(EGLint*)&dst->groupId);
+
+		EGLU_CHECK_MSG(egl, "Failed to query EGL_EXT_config_select_group config attribs");
+	}
 }
 
 } // eglu

framework/egl/egluConfigInfo.hpp
@@ -87,6 +87,9 @@ public:
 	// EGL_ANDROID_recordable
 	deUint32		recordableAndroid;
 
+	// EGL_EXT_config_select_group
+	deInt32			groupId;
+
 	ConfigInfo (void)
 		: bufferSize			(0)
 		, redSize				(0)
@@ -127,6 +130,7 @@ public:
 		, yuvPlaneBpp			(EGL_YUV_PLANE_BPP_0_EXT)
 		, colorComponentType	(EGL_NONE)
 		, recordableAndroid		(0)
+		, groupId			(0)
 	{
 	}
 

framework/egl/wrapper/eglwEnums.inl
@@ -292,4 +292,5 @@
 #define EGL_GL_COLORSPACE_DISPLAY_P3_LINEAR_EXT				0x3362
 #define EGL_GL_COLORSPACE_DISPLAY_P3_EXT					0x3363
 #define EGL_GL_COLORSPACE_DISPLAY_P3_PASSTHROUGH_EXT		0x3490
+#define EGL_CONFIG_SELECT_GROUP_EXT							0x34C0
 #define EGL_GL_COLORSPACE_BT2020_HLG_EXT					0x3540

framework/egl/wrapper/eglwEnumsC.inl
@@ -292,4 +292,5 @@
 #define EGL_GL_COLORSPACE_DISPLAY_P3_LINEAR_EXT				0x3362
 #define EGL_GL_COLORSPACE_DISPLAY_P3_EXT					0x3363
 #define EGL_GL_COLORSPACE_DISPLAY_P3_PASSTHROUGH_EXT		0x3490
+#define EGL_CONFIG_SELECT_GROUP_EXT							0x34C0
 #define EGL_GL_COLORSPACE_BT2020_HLG_EXT					0x3540

modules/egl/teglChooseConfigReference.cpp
@@ -243,6 +243,7 @@ public:
 	{
 		static const SurfaceConfig::CompareFunc compareFuncs[] =
 		{
+			SurfaceConfig::compareAttributeSmaller<EGL_CONFIG_SELECT_GROUP_EXT>,
 			SurfaceConfig::compareCaveat,
 			SurfaceConfig::compareColorComponentType,
 			SurfaceConfig::compareColorBufferType,
@@ -329,6 +330,9 @@ public:
 
 		// EGL_ANDROID_recordable
 		rules[EGL_RECORDABLE_ANDROID]		= AttribRule(EGL_RECORDABLE_ANDROID,		EGL_DONT_CARE,							CRITERIA_EXACT,		SORTORDER_NONE);
+
+		// EGL_EXT_config_select_group
+		rules[EGL_CONFIG_SELECT_GROUP_EXT]	= AttribRule(EGL_CONFIG_SELECT_GROUP_EXT,	EGL_DONT_CARE,		CRITERIA_SPECIAL,		SORTORDER_SMALLER); // 0
 
 		return rules;
 	}

scripts/egl/common.py
@@ -68,6 +68,7 @@ EXTENSIONS			= [
 	"EGL_KHR_partial_update",
 	"EGL_KHR_swap_buffers_with_damage",
 	"EGL_KHR_mutable_render_buffer",
+	"EGL_EXT_config_select_group",
 	"EGL_EXT_pixel_format_float",
 	"EGL_KHR_gl_colorspace",
 	"EGL_EXT_gl_colorspace_bt2020_linear",

commit 3bdb369640e3811f042261b9935dbeadc60e09b9

Correct EGL_EXT_config_select_group extension query
EGL_EXT_config_select_group is a display extension,
not a client extension.

Affects:
dEQP-EGL.functional.choose_config.simple.selection_and_sort.*

Ref: https://github.com/KhronosGroup/EGL-Registry/pull/199

Fixes: 88ba9ac270db ("Implement support for the EGL_EXT_config_select_group extension")

Change-Id: I38956511bdcb8e99d585ea9b99aeab53da0457e2
Signed-off-by: David Heidelberg <david.heidelberg@collabora.com>

framework/egl/egluConfigInfo.cpp
@@ -169,8 +169,7 @@ void queryCoreConfigInfo(const Library &egl, EGLDisplay display, EGLConfig confi
 
 void queryExtConfigInfo(const eglw::Library &egl, eglw::EGLDisplay display, eglw::EGLConfig config, ConfigInfo *dst)
 {
-    const std::vector<std::string> extensions       = getDisplayExtensions(egl, display);
-    const std::vector<std::string> clientExtensions = getClientExtensions(egl);
+    const std::vector<std::string> extensions = getDisplayExtensions(egl, display);
 
     if (de::contains(extensions.begin(), extensions.end(), "EGL_EXT_yuv_surface"))
     {
@@ -200,7 +199,7 @@ void queryExtConfigInfo(const eglw::Library &egl, eglw::EGLDisplay display, eglw
     else
         dst->colorComponentType = EGL_COLOR_COMPONENT_TYPE_FIXED_EXT;
 
-    if (de::contains(clientExtensions.begin(), clientExtensions.end(), "EGL_EXT_config_select_group"))
+    if (hasExtension(egl, display, "EGL_EXT_config_select_group"))
     {
         egl.getConfigAttrib(display, config, EGL_CONFIG_SELECT_GROUP_EXT, (EGLint *)&dst->groupId);
 