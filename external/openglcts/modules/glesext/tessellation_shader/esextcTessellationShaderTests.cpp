bb2b218475f99c61e1eb606a0ba911444cd53859
Kamil Goras <kamil.goras@mobica.com>
2024-09-13 13:30:30 +0200

    Split KHR-GLES32.core.tessellation_shader.vertex.vertex_spacing
    Affects:
    KHR-GLES32.core.tessellation_shader.vertex.vertex_spacing
    
    VK-GL-CTS issue: 3467
    
    Change-Id: Ib9f9e3ea3e824f04df27758ea02f223e91bd6bd2

--- external/openglcts/modules/glesext/tessellation_shader/esextcTessellationShaderTests.cpp
+++ external/openglcts/modules/glesext/tessellation_shader/esextcTessellationShaderTests.cpp
@@ -61,7 +61,7 @@ void TessellationShaderTests::init(void)
 {
     TestCaseGroupBase *vertexGroup = new TestCaseGroupBase(m_context, m_extParams, "vertex", "");
     vertexGroup->addChild(new TessellationShaderVertexOrdering(m_context, m_extParams));
-    vertexGroup->addChild(new TessellationShaderVertexSpacing(m_context, m_extParams));
+    addTessellationShaderVertexSpacingTest(vertexGroup);
     addChild(vertexGroup);
 
     TestCaseGroupBase *singleGroup = new TestCaseGroupBase(m_context, m_extParams, "single", "");
@@ -87,6 +87,70 @@ void TessellationShaderTests::init(void)
     addChild(new TessellationShaderErrors(m_context, m_extParams));
     addChild(new TessellationShaderInvarianceTests(m_context, m_extParams));
     addChild(new TesselationShaderWindingTests(m_context, m_extParams));
+}
+
+void TessellationShaderTests::addTessellationShaderVertexSpacingTest(TestCaseGroupBase *vertexGroup)
+{
+    if (vertexGroup == nullptr)
+    {
+        return;
+    }
+
+    deqp::GLExtTokens m_glExtTokens;
+    m_glExtTokens.init(m_context.getRenderContext().getType());
+
+    const glw::Functions &gl = m_context.getRenderContext().getFunctions();
+
+    /* Retrieve GL_MAX_TESS_GEN_LEVEL_EXT value */
+    glw::GLint gl_max_tess_gen_level_value;
+    gl.getIntegerv(m_glExtTokens.MAX_TESS_GEN_LEVEL, &gl_max_tess_gen_level_value);
+    GLU_EXPECT_NO_ERROR(gl.getError(), "glGetIntegerv() call failed for GL_MAX_TESS_GEN_LEVEL_EXT pname");
+
+    const _tessellation_shader_vertex_spacing vs_modes[] = {
+        TESSELLATION_SHADER_VERTEX_SPACING_EQUAL, TESSELLATION_SHADER_VERTEX_SPACING_FRACTIONAL_EVEN,
+        TESSELLATION_SHADER_VERTEX_SPACING_FRACTIONAL_ODD, TESSELLATION_SHADER_VERTEX_SPACING_DEFAULT};
+    const unsigned int n_vs_modes = sizeof(vs_modes) / sizeof(vs_modes[0]);
+
+    const _tessellation_primitive_mode primitive_modes[] = {TESSELLATION_SHADER_PRIMITIVE_MODE_ISOLINES,
+                                                            TESSELLATION_SHADER_PRIMITIVE_MODE_TRIANGLES,
+                                                            TESSELLATION_SHADER_PRIMITIVE_MODE_QUADS};
+    const unsigned int n_primitive_modes                 = sizeof(primitive_modes) / sizeof(primitive_modes[0]);
+
+    size_t test_index = 0;
+    /* Iterate through all primitive modes */
+    for (unsigned int n_primitive_mode = 0; n_primitive_mode < n_primitive_modes; ++n_primitive_mode)
+    {
+        _tessellation_primitive_mode primitive_mode = primitive_modes[n_primitive_mode];
+
+        /* Generate tessellation level set for current primitive mode */
+        _tessellation_levels_set tess_levels_set;
+
+        tess_levels_set = TessellationShaderUtils::getTessellationLevelSetForPrimitiveMode(
+            primitive_mode, gl_max_tess_gen_level_value,
+            TESSELLATION_LEVEL_SET_FILTER_INNER_AND_OUTER_LEVELS_USE_DIFFERENT_VALUES);
+
+        /* Iterate through all vertex spacing modes */
+        for (unsigned int n_vs_mode = 0; n_vs_mode < n_vs_modes; ++n_vs_mode)
+        {
+            _tessellation_shader_vertex_spacing vs_mode = vs_modes[n_vs_mode];
+
+            /* Iterate through all tessellation level combinations */
+            for (_tessellation_levels_set_const_iterator tess_levels_set_iterator = tess_levels_set.begin();
+                 tess_levels_set_iterator != tess_levels_set.end(); tess_levels_set_iterator++)
+            {
+                if (primitive_mode == TESSELLATION_SHADER_PRIMITIVE_MODE_QUADS &&
+                    vs_mode == TESSELLATION_SHADER_VERTEX_SPACING_FRACTIONAL_ODD &&
+                    ((*tess_levels_set_iterator).inner[0] <= 1 || (*tess_levels_set_iterator).inner[1] <= 1))
+                {
+                    continue;
+                }
+
+                vertexGroup->addChild(new TessellationShaderVertexSpacing(
+                    m_context, m_extParams, primitive_mode, vs_mode, *tess_levels_set_iterator, test_index));
+                test_index++;
+            }
+        }
+    }
 }
 
 } // namespace glcts
