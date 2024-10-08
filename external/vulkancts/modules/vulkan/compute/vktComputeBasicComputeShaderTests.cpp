c0031de8f6f8fbb00e6208a3570c845f1de412ef
Peter McNeeley <petermcneeley@google.com>
2024-10-08 11:07:18 -0400

    Adding amber test for barriered atomics in vulkan
    On some mobile devices barriers are not being respected when the
    workgroup size is less than or equal to the subgroup size. This issue
    becomes visible when doing atomic operations on workgroup memory. This
    tests highlights this edge case.
    
    Affects:
    
    dEQP-VK.compute.pipeline.basic.*
    
    Components: Vulkan
    
    Google bug: 42241448
    VK-GL-CTS issue: 5380
    
    Change-Id: I52cc371128856edc8dd5531c94c03e8e1c3c86b2

--- external/vulkancts/modules/vulkan/compute/vktComputeBasicComputeShaderTests.cpp
+++ external/vulkancts/modules/vulkan/compute/vktComputeBasicComputeShaderTests.cpp
@@ -5310,6 +5310,8 @@ tcu::TestCaseGroup *createBasicComputeShaderTests(tcu::TestContext &testCtx,
     {
         basicComputeTests->addChild(
             cts_amber::createAmberTestCase(testCtx, "write_ssbo_array", "", "compute", "write_ssbo_array.amber"));
+        basicComputeTests->addChild(cts_amber::createAmberTestCase(testCtx, "atomic_barrier_sum_small", "", "compute",
+                                                                   "atomic_barrier_sum_small.amber"));
         basicComputeTests->addChild(
             cts_amber::createAmberTestCase(testCtx, "branch_past_barrier", "", "compute", "branch_past_barrier.amber"));
         basicComputeTests->addChild(cts_amber::createAmberTestCase(
