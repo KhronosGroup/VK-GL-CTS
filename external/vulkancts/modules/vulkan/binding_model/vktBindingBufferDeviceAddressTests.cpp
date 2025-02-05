046343f46f7d39d53b47842d7fd8ed3279528046
Graeme Leese <gleese@broadcom.com>
2025-02-05 11:41:07 +0000

    Stop querying device address from unbound buffers
    The spec says that it is not valid to query the device address for a
    buffer which has not had memory bound unless it has been created with
    the CAPTURE_REPLAY bit (VUID-VkBufferDeviceAddressInfo-buffer-02600)
    
    Components: Vulkan
    Affects: dEQP-VK.binding_model.buffer_device_address.*
    
    Change-Id: I76abdc4dd77fa4c4dcaf785b78dfc767e16a8141

--- external/vulkancts/modules/vulkan/binding_model/vktBindingBufferDeviceAddressTests.cpp
+++ external/vulkancts/modules/vulkan/binding_model/vktBindingBufferDeviceAddressTests.cpp
@@ -770,7 +770,7 @@ tcu::TestStatus BufferAddressTestInstance::iterate(void)
         buffers[i] = VkBufferSp(new Unique<VkBuffer>(createBuffer(vk, device, &bufferCreateInfo)));
 
         // query opaque capture address before binding memory
-        if (useKHR)
+        if (useKHR && m_data.bufType == BT_REPLAY)
         {
             bufferDeviceAddressInfo.buffer = **buffers[i];
             opaqueBufferAddrs[i]           = vk.getBufferOpaqueCaptureAddress(device, &bufferDeviceAddressInfo);
