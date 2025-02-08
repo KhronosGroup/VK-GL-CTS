20328e033582a926d9584d3900bc69546c64a2e9
ziga-lunarg <ziga@lunarg.com>
2025-02-09 01:43:02 +0200

    Fix barrier in wsi maintenance1 scaling tests
    The barriers use the wrong dst access mask and src stage
    
    Components: Vulkan
    
    VK-GL-CTS issue: 5594
    
    Affected tests:
    dEQP-VK.wsi.*.maintenance1.scaling.*
    
    Change-Id: Ia5f64c8739a2425ad118100b0b6d33e660fb71f1

--- external/vulkancts/modules/vulkan/wsi/vktWsiMaintenance1Tests.cpp
+++ external/vulkancts/modules/vulkan/wsi/vktWsiMaintenance1Tests.cpp
@@ -1623,7 +1623,7 @@ tcu::TestStatus scalingTest(Context &context, const ScalingTestConfig testParams
                 VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER,
                 DE_NULL,
                 0,
-                0,
+                VK_ACCESS_TRANSFER_WRITE_BIT,
                 VK_IMAGE_LAYOUT_UNDEFINED,
                 VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
                 VK_QUEUE_FAMILY_IGNORED,
@@ -1632,8 +1632,8 @@ tcu::TestStatus scalingTest(Context &context, const ScalingTestConfig testParams
                 range,
             };
 
-            vkd.cmdPipelineBarrier(**commandBuffers[i], VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT,
-                                   VK_PIPELINE_STAGE_TRANSFER_BIT, 0u, 0, DE_NULL, 0, DE_NULL, 1, &barrier);
+            vkd.cmdPipelineBarrier(**commandBuffers[i], VK_PIPELINE_STAGE_BOTTOM_OF_PIPE_BIT,
+                                   VK_PIPELINE_STAGE_TRANSFER_BIT, 0u, 0, nullptr, 0, nullptr, 1, &barrier);
 
             const tcu::UVec2 halfSwapchainSize = swapchainSize / 2u;
             copyBufferToImage(vkd, **commandBuffers[i], *redBuffer, swapchainImages[imageIndex], tcu::UVec2(0, 0),
@@ -1651,6 +1651,7 @@ tcu::TestStatus scalingTest(Context &context, const ScalingTestConfig testParams
             barrier.oldLayout     = VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL;
             barrier.newLayout     = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
             barrier.srcAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
+            barrier.dstAccessMask = 0;
 
             vkd.cmdPipelineBarrier(**commandBuffers[i], VK_PIPELINE_STAGE_TRANSFER_BIT,
                                    VK_PIPELINE_STAGE_BOTTOM_OF_PIPE_BIT, 0u, 0, DE_NULL, 0, DE_NULL, 1, &barrier);
