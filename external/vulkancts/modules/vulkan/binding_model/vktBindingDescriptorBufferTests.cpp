4d17fe362433cb77fb9d1681431518ce001cf50f
Lionel Landwerlin <lionel.g.landwerlin@intel.com>
2025-03-06 11:33:51 +0200

    Test non buffer aligned descriptor buffer bindings
    New tests:
        dEQP-VK.binding_model.descriptor_buffer.*non_buffer_aligned
    
    Components: Vulkan
    VK-GL-CTS issue: 5631
    
    Change-Id: Ib039c2eff4a4d5ff206a6f2ab7c98b5d900b53fa

--- external/vulkancts/modules/vulkan/binding_model/vktBindingDescriptorBufferTests.cpp
+++ external/vulkancts/modules/vulkan/binding_model/vktBindingDescriptorBufferTests.cpp
@@ -355,6 +355,7 @@ struct TestParams
     uint32_t bufferBindingCount; // number of buffer bindings to create
     uint32_t setsPerBuffer;      // how may sets to put in one buffer binding
     bool useMaintenance5;        // should we use VkPipelineCreateFlagBits2KHR
+    bool nonBufferAligned;       // bind descriptor buffer at a non buffer aligned offset
 
     // Basic, null descriptor, capture/replay, or ycbcr sampler test
     VkDescriptorType descriptor; // descriptor type under test
@@ -3355,6 +3356,9 @@ void DescriptorBufferTestInstance::createDescriptorBuffers()
 
     currentBuffer = {};
 
+    if (m_params.nonBufferAligned)
+        currentBuffer.setOffset = m_descriptorBufferProperties.descriptorBufferOffsetAlignment;
+
     for (uint32_t setIndex = 0; setIndex < u32(m_descriptorSetLayouts.size()); ++setIndex)
     {
         auto &dsl = **m_descriptorSetLayouts[setIndex];
@@ -3653,8 +3657,10 @@ void DescriptorBufferTestInstance::bindDescriptorBuffers(VkCommandBuffer cmdBuf,
     for (const auto &buffer : m_descriptorBuffers)
     {
         VkDescriptorBufferBindingInfoEXT info = initVulkanStructure();
+        const VkDeviceSize bindOffset =
+            m_params.nonBufferAligned ? m_descriptorBufferProperties.descriptorBufferOffsetAlignment : 0;
 
-        info.address = buffer->deviceAddress;
+        info.address = buffer->deviceAddress + bindOffset;
         info.usage   = buffer->usage;
 
         if (!m_descriptorBufferProperties.bufferlessPushDescriptors &&
@@ -3686,11 +3692,13 @@ void DescriptorBufferTestInstance::bindDescriptorBuffers(VkCommandBuffer cmdBuf,
         const auto &dsl       = **m_descriptorSetLayouts[setIndex];
         const bool isBoundSet = (dsl.bufferIndex != INDEX_INVALID);
         const bool isLastSet  = ((setIndex + 1) == u32(m_descriptorSetLayouts.size()));
+        const VkDeviceSize bindOffset =
+            m_params.nonBufferAligned ? m_descriptorBufferProperties.descriptorBufferOffsetAlignment : 0;
 
         if (isBoundSet)
         {
             bufferIndices.emplace_back(dsl.bufferIndex);
-            bufferOffsets.emplace_back(dsl.bufferOffset);
+            bufferOffsets.emplace_back(dsl.bufferOffset - bindOffset);
         }
 
         if ((!isBoundSet || isLastSet) && !bufferIndices.empty())
@@ -6309,6 +6317,11 @@ void populateDescriptorBufferTestGroup(tcu::TestCaseGroup *topGroup, ResourceRes
         params.useMaintenance5 = true;
 
         subGroup->addChild(new DescriptorBufferTestCase(testCtx, "compute_maintenance5", params));
+
+        params.nonBufferAligned = true;
+
+        subGroup->addChild(new DescriptorBufferTestCase(testCtx, "non_buffer_aligned", params));
+
         topGroup->addChild(subGroup.release());
     }
 
