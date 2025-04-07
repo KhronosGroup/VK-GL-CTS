64a83266a7d41048f03506cc52b0bf2135b241a1
Ricardo Garcia <rgarcia@igalia.com>
2025-04-07 16:00:19 +0200

    Code generating tests is reading beyond the end of an array
    This is due to the wrong array being used in an iteration loop.
    Unfortunately, using the right array means we no longer hit the
    "continue" statement in the same cases and additional tests are
    generated.
    
    New tests:
    dEQP-VK.query_pool.statistics_query.multiple_queries.input_assembly_vertex_cmdcopy_dstoffset_stride_zero
    dEQP-VK.query_pool.statistics_query.multiple_queries.input_assembly_vertex_fragment_cmdcopy_dstoffset_stride_zero
    dEQP-VK.query_pool.statistics_query.multiple_queries.input_assembly_vertex_fragment_wait_cmdcopy_dstoffset_stride_zero
    dEQP-VK.query_pool.statistics_query.multiple_queries.input_assembly_vertex_wait_cmdcopy_dstoffset_stride_zero
    
    Affects:
    dEQP-VK.query_pool.statistics_query.*
    
    Components: Vulkan
    VK-GL-CTS issue: 5690
    
    Change-Id: Ifd823156e37bd29c6bc5a05f58322dfa38c14b91

--- external/vulkancts/modules/vulkan/query_pool/vktQueryPoolStatisticsTests.cpp
+++ external/vulkancts/modules/vulkan/query_pool/vktQueryPoolStatisticsTests.cpp
@@ -7611,7 +7611,7 @@ void QueryPoolStatisticsTests::init(void)
                         if (partialFlags[partialFlagsIdx] && strideTypes[strideTypesIdx] == STRIDE_TYPE_ZERO)
                             continue;
 
-                        if (strideTypes[strideTypesIdx] == STRIDE_TYPE_ZERO && copyType[copyTypesIdx] != COPY_TYPE_CMD)
+                        if (strideTypes[strideTypesIdx] == STRIDE_TYPE_ZERO && copyTypes[copyTypesIdx] != COPY_TYPE_CMD)
                             continue;
 
                         VkQueryResultFlags queryFlags = VK_QUERY_RESULT_64_BIT | VK_QUERY_RESULT_WITH_AVAILABILITY_BIT |
