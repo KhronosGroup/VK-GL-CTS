KhronosGroup/VK-GL-CTS

commit f49b4f0277b62863a774475ae076c1d0b74b2c77

Only fetch external sources when strictly required
Much of the time, fetch_sources can simply do git checkout and move on.
Calling git fetch every time can be quite costly: during the Mesa CI
setup where we need to repeatedly build dEQP, it looks like just
fetching the refs from SPIRV-Tools costs us 3 minutes, even if we
already have everything cached.

Rework the update process so that we optimistically try a checkout
first, and only try a fetch if required.

Change-Id: Iba7b9475eed442fc519c3d27d76ba54655653987
Signed-off-by: Daniel Stone <daniels@collabora.com>

external/fetch_sources.py
@@ -251,27 +251,25 @@ class GitRepo (Source):
         self.patch = patch
 
     def checkout(self, url, fullDstPath, force):
-        if not os.path.exists(os.path.join(fullDstPath, '.git')):
-            execute(["git", "clone", "--no-checkout", url, fullDstPath])
-
-        pushWorkingDir(fullDstPath)
         print("Directory: " + fullDstPath)
+        for tag in self.removeTags:
+            proc = subprocess.Popen(['git', 'tag', '-l', tag], stdout=subprocess.PIPE)
+            (stdout, stderr) = proc.communicate()
+            if len(stdout) > 0:
+                execute(["git", "tag", "-d",tag])
+
+        force_arg = ['--force'] if force else []
         try:
-            for tag in self.removeTags:
-                proc = subprocess.Popen(['git', 'tag', '-l', tag], stdout=subprocess.PIPE)
-                (stdout, stderr) = proc.communicate()
-                if len(stdout) > 0:
-                    execute(["git", "tag", "-d",tag])
-            force_arg = ['--force'] if force else []
+            execute(["git", "checkout"] + force_arg + [self.revision])
+        except:
+            logging.debug("couldn't find revision " + self.revision + " locally; fetching")
             execute(["git", "fetch"] + force_arg + ["--tags", url, "+refs/heads/*:refs/remotes/origin/*"])
             execute(["git", "checkout"] + force_arg + [self.revision])
 
-            if(self.patch != ""):
-                patchFile = os.path.join(EXTERNAL_DIR, self.patch)
-                execute(["git", "reset", "--hard", "HEAD"])
-                execute(["git", "apply", patchFile])
-        finally:
-            popWorkingDir()
+        if(self.patch != ""):
+            patchFile = os.path.join(EXTERNAL_DIR, self.patch)
+            execute(["git", "reset", "--hard", "HEAD"])
+            execute(["git", "apply", patchFile])
 
     def update (self, cmdProtocol, force = False):
         fullDstPath = os.path.join(EXTERNAL_DIR, self.baseDir, self.extractDir)
@@ -283,11 +281,20 @@ class GitRepo (Source):
             url       = self.sshUrl
             backupUrl = self.httpsUrl
 
+        if not os.path.exists(os.path.join(fullDstPath, '.git')):
+            logging.debug("git repository does not exist; performing full clone")
+            try:
+                execute(["git", "clone", "--no-checkout", url, fullDstPath])
+            except:
+                if backupUrl != None:
+                    execute(["git", "clone", "--no-checkout", url, backupPath])
+
+        pushWorkingDir(fullDstPath)
+
         try:
             self.checkout(url, fullDstPath, force)
-        except:
-            if backupUrl != None:
-                self.checkout(backupUrl, fullDstPath, force)
+        finally:
+            popWorkingDir()
 
 def postExtractLibpng (path):
     shutil.copy(os.path.join(path, "scripts", "pnglibconf.h.prebuilt"),

commit aeedc692a79cf31073cdb377b51d1ee01dbdb682

fetch_sources: Fix fallback to backup url
In commit f49b4f0277b, there was a mixup between the backup url and
clone destination.

Fixes: f49b4f0277b ("Only fetch external sources when strictly required")
Change-Id: I445bb2568ea8f79b261562cb72ffb00d81243e86

external/fetch_sources.py
@@ -290,7 +290,7 @@ class GitRepo (Source):
                 run(["git", "clone", "--no-checkout", url, fullDstPath])
             except:
                 if backupUrl != None:
-                    run(["git", "clone", "--no-checkout", url, backupPath])
+                    run(["git", "clone", "--no-checkout", backupUrl, fullDstPath])
 
         pushWorkingDir(fullDstPath)
 