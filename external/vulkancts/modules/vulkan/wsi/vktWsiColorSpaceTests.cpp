5c949f1c08bdf52bb11dccae8be05d5f2d1ef4aa
ziga-lunarg <ziga@lunarg.com>
2025-10-04 17:44:01 +0300

    Fix using semaphores in colorspace tests
    Use correct index for rendering complete semaphore and make sure they
    are not reused while still in use
    
    Components: Vulkan
    
    VK-GL-CTS issue: 6042
    
    Affected tests:
    dEQP-VK.wsi.*.colorspace.*
    
    Change-Id: I79b29a1f6957a746795a326ae7e50b18b0185028

--- external/vulkancts/modules/vulkan/wsi/vktWsiColorSpaceTests.cpp
+++ external/vulkancts/modules/vulkan/wsi/vktWsiColorSpaceTests.cpp
@@ -632,27 +632,26 @@ tcu::TestStatus surfaceFormatRenderTest(Context &context, Type wsiType, const In
             TCU_CHECK((size_t)imageNdx < swapchainImages.size());
 
             {
-                const VkSemaphore renderingCompleteSemaphore =
-                    **renderingCompleteSemaphores[frameNdx % renderingCompleteSemaphores.size()];
-                const VkCommandBuffer commandBuffer     = **commandBuffers[frameNdx % commandBuffers.size()];
-                const VkPipelineStageFlags waitDstStage = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
-                const VkSubmitInfo submitInfo           = {VK_STRUCTURE_TYPE_SUBMIT_INFO,
-                                                           DE_NULL,
-                                                           1u,
-                                                           &imageReadySemaphore,
-                                                           &waitDstStage,
-                                                           1u,
-                                                           &commandBuffer,
-                                                           1u,
-                                                           &renderingCompleteSemaphore};
-                const VkPresentInfoKHR presentInfo      = {VK_STRUCTURE_TYPE_PRESENT_INFO_KHR,
-                                                           DE_NULL,
-                                                           1u,
-                                                           &renderingCompleteSemaphore,
-                                                           1u,
-                                                           &*swapchain,
-                                                           &imageNdx,
-                                                           (VkResult *)DE_NULL};
+                const VkSemaphore renderingCompleteSemaphore = **renderingCompleteSemaphores[imageNdx];
+                const VkCommandBuffer commandBuffer          = **commandBuffers[frameNdx % commandBuffers.size()];
+                const VkPipelineStageFlags waitDstStage      = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
+                const VkSubmitInfo submitInfo                = {VK_STRUCTURE_TYPE_SUBMIT_INFO,
+                                                                DE_NULL,
+                                                                1u,
+                                                                &imageReadySemaphore,
+                                                                &waitDstStage,
+                                                                1u,
+                                                                &commandBuffer,
+                                                                1u,
+                                                                &renderingCompleteSemaphore};
+                const VkPresentInfoKHR presentInfo           = {VK_STRUCTURE_TYPE_PRESENT_INFO_KHR,
+                                                                DE_NULL,
+                                                                1u,
+                                                                &renderingCompleteSemaphore,
+                                                                1u,
+                                                                &*swapchain,
+                                                                &imageNdx,
+                                                                (VkResult *)DE_NULL};
 
                 if (checkHdr)
                 {
