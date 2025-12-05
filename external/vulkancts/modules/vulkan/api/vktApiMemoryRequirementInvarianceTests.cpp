KhronosGroup/VK-GL-CTS

commit 4c7e4e98f9e577ecd5bf2d922cd756aadc02701a

Fix api invariance test using pvrtc formats
A check if format is supported is missing before using it

Components: Vulkan

VK-GL-CTS issue: 4430

Affected test:
dEQP-VK.api.invariance.random

Change-Id: I8fb1c3f7e4325e3f9d86fd454e886591a77dd1a4

external/vulkancts/framework/vulkan/vkImageUtil.cpp
@@ -357,6 +357,26 @@ bool isYCbCr422Format (VkFormat format)
 	}
 }
 
+bool isPvrtcFormat (VkFormat format)
+{
+	switch (format)
+	{
+#ifndef CTS_USES_VULKANSC
+		case VK_FORMAT_PVRTC1_2BPP_UNORM_BLOCK_IMG:
+		case VK_FORMAT_PVRTC1_4BPP_UNORM_BLOCK_IMG:
+		case VK_FORMAT_PVRTC2_2BPP_UNORM_BLOCK_IMG:
+		case VK_FORMAT_PVRTC2_4BPP_UNORM_BLOCK_IMG:
+		case VK_FORMAT_PVRTC1_2BPP_SRGB_BLOCK_IMG:
+		case VK_FORMAT_PVRTC1_4BPP_SRGB_BLOCK_IMG:
+		case VK_FORMAT_PVRTC2_2BPP_SRGB_BLOCK_IMG:
+		case VK_FORMAT_PVRTC2_4BPP_SRGB_BLOCK_IMG:
+			return true;
+#endif
+		default:
+			return false;
+	}
+}
+
 const std::map<VkFormat, std::string> spirvFormats = {
 	{ VK_FORMAT_R32G32B32A32_SFLOAT,		"Rgba32f"		},
 	{ VK_FORMAT_R32G32_SFLOAT,				"Rg32f"			},

external/vulkancts/framework/vulkan/vkImageUtil.hpp
@@ -137,6 +137,7 @@ struct PlanarFormatDescription
 bool							isYCbCrFormat					(VkFormat						format);
 bool							isYCbCrExtensionFormat			(VkFormat						format);
 bool							isYCbCrConversionFormat			(VkFormat						format);
+bool							isPvrtcFormat					(VkFormat						format);
 PlanarFormatDescription			getPlanarFormatDescription		(VkFormat						format);
 int								getPlaneCount					(VkFormat						format);
 deUint32						getMipmapCount					(VkFormat						format,

external/vulkancts/modules/vulkan/api/vktApiMemoryRequirementInvarianceTests.cpp
@@ -259,6 +259,7 @@ tcu::TestStatus InvarianceInstance::iterate (void)
 	const deBool							isDedicatedAllocationSupported	= m_context.isDeviceFunctionalitySupported("VK_KHR_dedicated_allocation");
 	const deBool							isYcbcrSupported				= m_context.isDeviceFunctionalitySupported("VK_KHR_sampler_ycbcr_conversion");
 	const deBool							isYcbcrExtensionSupported		= m_context.isDeviceFunctionalitySupported("VK_EXT_ycbcr_2plane_444_formats");
+	const deBool							isPvrtcSupported				= m_context.isDeviceFunctionalitySupported("VK_IMG_format_pvrtc");
 	std::vector<int>						optimalFormats;
 	std::vector<int>						linearFormats;
 	std::vector<int>						memoryTypes;
@@ -509,6 +510,9 @@ tcu::TestStatus InvarianceInstance::iterate (void)
 			continue;
 
 		if (isYCbCrExtensionFormat((VkFormat)formatlist[i]) && !isYcbcrExtensionSupported)
+			continue;
+
+		if (isPvrtcFormat((VkFormat)formatlist[i]) && !isPvrtcSupported)
 			continue;
 
 		vk::VkImageFormatProperties imageformatprops;

commit 6ccf49e025f7e77bcac3ead5a717f6aabf9ea7d3

Fix: Test PVRTC extension and always use power-of-two dimensions
Public PR: https://github.com/KhronosGroup/VK-GL-CTS/pull/451

The CTS tests always use random image sizes for testing images, which was not valid for PVRTC1 textures. With 1.3.279 a VUID was added to require power-of-two dimensions for PVRTC1 textures, as discussed in KhronosGroup/Vulkan-Docs#1991.

Also, this fixes a bug introduced with 4c7e4e9, as it required the extension to be present for the PVRTC formats to be tested, but it was never added as a extension to be enabled, so PVRTC textures were never tested.

Components: Vulkan

Affects:
dEQP-VK.api.invariance.random

Change-Id: I89884bed683b796ccc9c793ba419455f842e71ba

external/vulkancts/framework/vulkan/vkImageUtil.cpp
@@ -378,6 +378,25 @@ bool isPvrtcFormat(VkFormat format)
     return false;
 }
 
+bool isPvrtc1Format(VkFormat format)
+{
+#ifndef CTS_USES_VULKANSC
+    switch (format)
+    {
+    case VK_FORMAT_PVRTC1_2BPP_UNORM_BLOCK_IMG:
+    case VK_FORMAT_PVRTC1_4BPP_UNORM_BLOCK_IMG:
+    case VK_FORMAT_PVRTC1_2BPP_SRGB_BLOCK_IMG:
+    case VK_FORMAT_PVRTC1_4BPP_SRGB_BLOCK_IMG:
+        return true;
+    default:
+        return false;
+    }
+#else
+    DE_UNREF(format);
+#endif
+    return false;
+}
+
 const std::map<VkFormat, std::string> spirvFormats = {
     {VK_FORMAT_R32G32B32A32_SFLOAT, "Rgba32f"},
     {VK_FORMAT_R32G32_SFLOAT, "Rg32f"},

external/vulkancts/framework/vulkan/vkImageUtil.hpp
@@ -214,6 +214,7 @@ bool isYCbCrFormat(VkFormat format);
 bool isYCbCrExtensionFormat(VkFormat format);
 bool isYCbCrConversionFormat(VkFormat format);
 bool isPvrtcFormat(VkFormat format);
+bool isPvrtc1Format(VkFormat format);
 PlanarFormatDescription getPlanarFormatDescription(VkFormat format);
 int getPlaneCount(VkFormat format);
 uint32_t getMipmapCount(VkFormat format, const vk::PlanarFormatDescription &formatDescription,

external/vulkancts/modules/vulkan/vktTestCase.cpp
@@ -110,6 +110,7 @@ vector<string> filterExtensions(const vector<VkExtensionProperties> &extensions)
         "VK_QCOM_fragment_density_map_offset",
         "VK_QCOM_image_processing",
         "VK_ARM_performance_counters_by_region",
+        "VK_IMG_format_pvrtc",
     };
 
     const char *exclusions[] = {"VK_EXT_device_address_binding_report", "VK_EXT_device_memory_report"};

framework/delibs/debase/deInt32.h
@@ -251,7 +251,7 @@ static inline bool deIsPowerOfTwoSize(size_t a)
 }
 
 /*--------------------------------------------------------------------*//*!
- * \brief Roud a value up to a power-of-two.
+ * \brief Round a value up to a power-of-two.
  * \param a Input value.
  * \return Smallest power-of-two value that is greater or equal to an input value.
  *//*--------------------------------------------------------------------*/
@@ -267,7 +267,7 @@ static inline uint32_t deSmallestGreaterOrEquallPowerOfTwoU32(uint32_t a)
 }
 
 /*--------------------------------------------------------------------*//*!
- * \brief Roud a value up to a power-of-two.
+ * \brief Round a value up to a power-of-two.
  * \param a Input value.
  * \return Smallest power-of-two value that is greater or equal to an input value.
  *//*--------------------------------------------------------------------*/
@@ -284,7 +284,7 @@ static inline uint64_t deSmallestGreaterOrEquallPowerOfTwoU64(uint64_t a)
 }
 
 /*--------------------------------------------------------------------*//*!
- * \brief Roud a value up to a power-of-two.
+ * \brief Round a value up to a power-of-two.
  * \param a Input value.
  * \return Smallest power-of-two value that is greater or equal to an input value.
  *//*--------------------------------------------------------------------*/

external/vulkancts/modules/vulkan/api/vktApiMemoryRequirementInvarianceTests.cpp
@@ -175,12 +175,22 @@ ImageAllocator::ImageAllocator(deRandom &random, bool dedicated, std::vector<int
     else
         m_colorFormat = (VkFormat)optimalformats[deRandom_getUint32(&random) % optimalformats.size()];
 
-    int widthAlignment  = (isYCbCr420Format(m_colorFormat) || isYCbCr422Format(m_colorFormat)) ? 2 : 1;
-    int heightAlignment = isYCbCr420Format(m_colorFormat) ? 2 : 1;
+    if (isPvrtc1Format(m_colorFormat))
+    {
+        // See VUID-VkImageCreateInfo-format-09583 and VUID-VkImageCreateInfo-format-09584
+        // PVRTC1 textures have to have both a width and height that is a power of 2.
+        m_size = tcu::IVec2(1 << (deRandom_getUint32(&random) % 4 + 1), 1 << (deRandom_getUint32(&random) % 4 + 1));
+    }
+    else
+    {
+        int widthAlignment  = (isYCbCr420Format(m_colorFormat) || isYCbCr422Format(m_colorFormat)) ? 2 : 1;
+        int heightAlignment = isYCbCr420Format(m_colorFormat) ? 2 : 1;
 
-    // Random small size for causing potential alignment issues
-    m_size = tcu::IVec2((deRandom_getUint32(&random) % 16 + 3) & ~(widthAlignment - 1),
-                        (deRandom_getUint32(&random) % 16 + 3) & ~(heightAlignment - 1));
+        // Random small size for causing potential alignment issues
+        m_size = tcu::IVec2((deRandom_getUint32(&random) % 16 + 3) & ~(widthAlignment - 1),
+                            (deRandom_getUint32(&random) % 16 + 3) & ~(heightAlignment - 1));
+    }
+
     // Pick random memory type from the supported set
     m_memoryType = memoryTypes[deRandom_getUint32(&random) % memoryTypes.size()];
 }

external/vulkancts/modules/vulkan/pipeline/vktPipelineMultisampleImageTests.cpp
@@ -1738,20 +1738,20 @@ tcu::TestStatus test(Context &context, const CaseDef3d caseDef)
     const auto resolveRegionOffset    = makeOffset3D(0u, 0u, 0u);
 
     const VkImageResolve resolveRegion = {
-        colorSubresourceLayers, // VkImageSubresourceLayers	srcSubresource
-        resolveRegionOffset,    // VkOffset3D					srcOffset
-        colorSubresourceLayers, // VkImageSubresourceLayers	dstSubresource
-        resolveRegionOffset,    // VkOffset3D					dstOffset
-        msImageExtent           // VkExtent3D					extent
+        colorSubresourceLayers, // VkImageSubresourceLayers    srcSubresource
+        resolveRegionOffset,    // VkOffset3D                    srcOffset
+        colorSubresourceLayers, // VkImageSubresourceLayers    dstSubresource
+        resolveRegionOffset,    // VkOffset3D                    dstOffset
+        msImageExtent           // VkExtent3D                    extent
     };
 
     const VkBufferImageCopy copyRegion = {
-        0u,                                        // VkDeviceSize				bufferOffset
-        0u,                                        // uint32_t					bufferRowLength
-        0u,                                        // uint32_t					bufferImageHeight
-        colorSubresourceLayers,                    // VkImageSubresourceLayers	imageSubresource
-        resolveRegionOffset,                       // VkOffset3D					imageOffset
-        makeExtent3D(size.x(), size.y(), size.z()) // VkExtent3D					imageExtent
+        0u,                                        // VkDeviceSize                bufferOffset
+        0u,                                        // uint32_t                    bufferRowLength
+        0u,                                        // uint32_t                    bufferImageHeight
+        colorSubresourceLayers,                    // VkImageSubresourceLayers    imageSubresource
+        resolveRegionOffset,                       // VkOffset3D                    imageOffset
+        makeExtent3D(size.x(), size.y(), size.z()) // VkExtent3D                    imageExtent
     };
 
     // Output buffer