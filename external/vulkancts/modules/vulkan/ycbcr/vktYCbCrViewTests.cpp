bluestacks/external-deqp-a16

Fix YCbCr view tests assertion
Some YCbCr tests create views of the same image plane in different
formats which are compatible in size, and they attempt to see they can
be read in the compatible format and all non-padding bits are preserved
in the result. For this, they evaluated the read operation on a common
ground: the format with padding bits so that their values would be
ignored.

The code was supposing that only one of the two formats would include
padding bits, or both would be the same format, and was refusing to
compare two formats if they both had padding bits and were not the same
format, which resulted in an assertion failure.

In these compatible-format tests, single-plane YCbCr formats are ignored
and only multiplane YCbCr formats are used, comparing one plane each
time and using its plane-compatible format as one of two formats. Due to
this, we can see that we would only be using the following
plane-compatible formats in practice:

VK_FORMAT_R8_UNORM
VK_FORMAT_R8G8_UNORM
VK_FORMAT_R16_UNORM
VK_FORMAT_R16G16_UNORM
VK_FORMAT_R10X6_UNORM_PACK16 (*)
VK_FORMAT_R10X6G10X6_UNORM_2PACK16 (*)
VK_FORMAT_R12X4_UNORM_PACK16 (*)
VK_FORMAT_R12X4G12X4_UNORM_2PACK16 (*)

(*) Indicates the format includes padding bits.

Then, the code checks if this plane-compatible format can be read as any
other format in a small predefined list of compatible formats called
s_compatible_formats, which includes:

    // 8-bit compatibility class
    VK_FORMAT_R4G4_UNORM_PACK8,
    VK_FORMAT_R8_UINT,
    VK_FORMAT_R8_SINT,

    // 16-bit compatibility class
    VK_FORMAT_R8G8_UNORM,
    VK_FORMAT_R8G8_UINT,
    VK_FORMAT_R10X6_UNORM_PACK16,
    VK_FORMAT_R12X4_UNORM_PACK16,
    VK_FORMAT_R16_UNORM,
    VK_FORMAT_R16_UINT,
    VK_FORMAT_R16_SINT,
    VK_FORMAT_R4G4B4A4_UNORM_PACK16,

    // 32-bit compatibility class
    VK_FORMAT_R10X6G10X6_UNORM_2PACK16,
    VK_FORMAT_R12X4G12X4_UNORM_2PACK16,
    VK_FORMAT_R16G16_UNORM,
    VK_FORMAT_R8G8B8A8_UNORM,
    VK_FORMAT_R8G8B8A8_UINT,
    

external/vulkancts/modules/vulkan/ycbcr/vktYCbCrViewTests.cpp
@@ -472,9 +472,24 @@ VkFormat chooseComparisonFormat (VkFormat planeOriginalFormat, VkFormat planeCom
 	const bool isOriginalPadded		= isPaddedFormat(planeOriginalFormat);
 	const bool isCompatiblePadded	= isPaddedFormat(planeCompatibleFormat);
 
-	// We can't have padded formats on both sides unless they're the exact same formats.
 	if (isOriginalPadded && isCompatiblePadded)
-		DE_ASSERT(planeOriginalFormat == planeCompatibleFormat);
+	{
+		if (planeOriginalFormat == planeCompatibleFormat)
+			return planeOriginalFormat;
+
+		// Try to see if they're a known format pair that can be compared.
+		const auto& fmt1 = (planeOriginalFormat < planeCompatibleFormat ? planeOriginalFormat : planeCompatibleFormat);
+		const auto& fmt2 = (planeOriginalFormat < planeCompatibleFormat ? planeCompatibleFormat : planeOriginalFormat);
+
+		if (fmt1 == VK_FORMAT_R10X6_UNORM_PACK16 && fmt2 == VK_FORMAT_R12X4_UNORM_PACK16)
+			return fmt1;
+
+		if (fmt1 == VK_FORMAT_R10X6G10X6_UNORM_2PACK16 && fmt2 == VK_FORMAT_R12X4G12X4_UNORM_2PACK16)
+			return fmt1;
+
+		// We can't have padded formats on both sides unless they're the exact same formats or we know how to compare them.
+		DE_ASSERT(false);
+	}
 
 	if (isCompatiblePadded)
 		return planeCompatibleFormat;

KhronosGroup/VK-GL-CTS

dEQP-VKSC.ycbcr.plane_view.image_view.*
(cherry picked from commit c6c4fa243ebd8af373de29c00f5fb3b89c0e34b3)

external/vulkancts/modules/vulkan/ycbcr/vktYCbCrViewTests.cpp
@@ -464,9 +464,24 @@ VkFormat chooseComparisonFormat(VkFormat planeOriginalFormat, VkFormat planeComp
     const bool isOriginalPadded   = isPaddedFormat(planeOriginalFormat);
     const bool isCompatiblePadded = isPaddedFormat(planeCompatibleFormat);
 
-    // We can't have padded formats on both sides unless they're the exact same formats.
     if (isOriginalPadded && isCompatiblePadded)
-        DE_ASSERT(planeOriginalFormat == planeCompatibleFormat);
+    {
+        if (planeOriginalFormat == planeCompatibleFormat)
+            return planeOriginalFormat;
+
+        // Try to see if they're a known format pair that can be compared.
+        const auto &fmt1 = (planeOriginalFormat < planeCompatibleFormat ? planeOriginalFormat : planeCompatibleFormat);
+        const auto &fmt2 = (planeOriginalFormat < planeCompatibleFormat ? planeCompatibleFormat : planeOriginalFormat);
+
+        if (fmt1 == VK_FORMAT_R10X6_UNORM_PACK16 && fmt2 == VK_FORMAT_R12X4_UNORM_PACK16)
+            return fmt1;
+
+        if (fmt1 == VK_FORMAT_R10X6G10X6_UNORM_2PACK16 && fmt2 == VK_FORMAT_R12X4G12X4_UNORM_2PACK16)
+            return fmt1;
+
+        // We can't have padded formats on both sides unless they're the exact same formats or we know how to compare them.
+        DE_ASSERT(false);
+    }
 
     if (isCompatiblePadded)
         return planeCompatibleFormat;